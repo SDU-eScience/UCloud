a0b81bb527ecc692dfc7430f15af716961d7dfa2
Dan Sebastian Thrane <dthrane@gmail.com>
2017-10-30 13:03:42 +0100

    Gateway to storage

--- gateway/src/main/kotlin/org/esciencecloud/transactions/GatewayServer.kt
+++ gateway/src/main/kotlin/org/esciencecloud/transactions/GatewayServer.kt
@@ -1,35 +1,40 @@
 package org.esciencecloud.transactions
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import org.esciencecloud.kafka.examples.MetadataKey
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.common.serialization.Serdes
+import org.esciencecloud.kafka.JsonHttpClient
+import org.esciencecloud.kafka.JsonSerde.jsonSerde
+import org.esciencecloud.kafka.StreamDescription
 import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.cio.copyTo
+import org.jetbrains.ktor.cio.toInputStream
+import org.jetbrains.ktor.client.DefaultHttpClient
+import org.jetbrains.ktor.client.readText
 import org.jetbrains.ktor.gson.GsonSupport
 import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.http.HttpHeaders
+import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.request.receive
+import org.jetbrains.ktor.pipeline.PipelineContext
+import org.jetbrains.ktor.request.*
+import org.jetbrains.ktor.response.header
 import org.jetbrains.ktor.response.respond
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.response.respondWrite
+import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.post
-import org.jetbrains.ktor.routing.put
 import org.jetbrains.ktor.routing.route
 import org.jetbrains.ktor.routing.routing
-import org.slf4j.LoggerFactory
+import java.net.URL
 import java.util.*
 
-typealias StoragePath = String
-data class User(val username: String, val password: String)
-data class PermissionChange(val right: String, val to: String)
-data class BulkPermissionChange(val on: StoragePath, val changes: List<PermissionChange>)
-data class PermissionRequest(val user: User, val permissions: List<BulkPermissionChange>)
-
-data class MetadataUpdateRequest(val path: String, val key: String, val value: String)
-
 fun main(args: Array<String>) {
-    val log = LoggerFactory.getLogger("Server")!!
     val producer = KafkaProducer<String, JsonNode>(mapOf(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringSerializer",
@@ -42,40 +47,189 @@ fun main(args: Array<String>) {
         install(GsonSupport)
 
         routing {
-            route("/api/") {
-                put("permissions") {
-                    val request = call.receive<PermissionRequest>()
-                    val jobId = UUID.randomUUID()
-                    log.info(request.toString())
-                    producer.send(ProducerRecord(
-                            "bulk-permission-request",
-                            jobId.toString(),
-                            mapper.valueToTree(request))
-                    )
+            route("api") {
+                route("files") {
+                    get {
+                        val queryString = call.request.queryString()
+                        // Practically anything past the endpoint should be generic enough to refactor
+                        val endpoint = URL(resolveStorageService(), "/api/files?$queryString")
 
-                    call.respond(mapOf(
-                            "status" to "started",
-                            "jobId" to jobId)
-                    )
+                        val header = validateRequestAndPrepareKafkaHeader() ?: return@get
+                        val auth = with (header.performedFor) {
+                            Base64.getEncoder().encodeToString("$username:$password".toByteArray())
+                        }
+
+                        val proxyResponse = DefaultHttpClient.request(endpoint) {
+                            header(HttpHeaders.Accept, ContentType.Application.Json.toString())
+                            header(HttpHeaders.Authorization, "Basic $auth")
+                            header("Job-Id", header.uuid)
+                        }
+
+                        // TODO We need more general proxying here
+                        val proxyType = proxyResponse.headers[HttpHeaders.ContentType]
+                        if (proxyType != null) {
+                            call.response.header(HttpHeaders.ContentType, proxyType)
+                        }
+
+                        call.response.status(proxyResponse.status)
+                        call.respondText(proxyResponse.readText())
+                    }
                 }
 
-                post("metadata") {
-                    val request = call.receive<MetadataUpdateRequest>()
-                    val key = mapper.writeValueAsString(MetadataKey(request.path, request.key))
-                    producer.send(ProducerRecord(
-                            "requests.metadata.update",
-                            key,
-                            mapper.valueToTree(request.value)
-                    ))
+                route("users") {
+                    post {
+                        val header = validateRequestAndPrepareKafkaHeader() ?: return@post
+                        val request = call.receive<RestCreateUserRequest>()
 
-                    call.respond(mapOf(
-                            "status" to "started"
-                    ))
+                        val kafkaRequest = CreateUserRequest(
+                                header,
+                                request.username,
+                                request.password,
+                                request.userType
+                        )
+
+                        // Last part should be possible to refactor out. As long as we're given a correct request.
+                        // However, this would also require a common request scheme across all services.
+                        producer.send(ProducerRecord(
+                                UserGroupsProcessor.CreateUser.requestStream.name,
+                                header.uuid,
+                                mapper.valueToTree(kafkaRequest)
+                        ))
+
+                        call.respond(GatewayJobResponse(header.uuid, JobStatus.STARTED))
+                    }
                 }
             }
         }
     }.start(wait = true)
 
     producer.close()
+}
+
+private fun resolveStorageService() = URL("http://localhost:42100") // TODO Do something slightly better ;-)
+
+private suspend fun PipelineContext<Unit>.validateRequestAndPrepareKafkaHeader(): RequestHeader? {
+    val jobId = UUID.randomUUID().toString()
+    val (username, password) = call.request.basicAuth() ?: return run {
+        call.respond(HttpStatusCode.Unauthorized)
+        null
+    }
+    return RequestHeader(jobId, ProxyClient(username, password))
+}
+
+private fun ApplicationRequest.basicAuth(): Pair<String, String>? {
+    val auth = authorization() ?: return null
+    if (!auth.startsWith("Basic ")) return null
+    val decoded = String(Base64.getDecoder().decode(auth.substringAfter("Basic ")))
+    if (decoded.indexOf(':') == -1) return null
+
+    return Pair(decoded.substringBefore(':'), decoded.substringAfter(':'))
+}
+
+// -------------------------------------------
+// These should be exported to clients
+// -------------------------------------------
+
+enum class JobStatus {
+    STARTED,
+    COMPLETE,
+    ERROR
+}
+
+data class GatewayJobResponse(val jobId: String, val status: JobStatus)
+
+data class RestCreateUserRequest(
+        val username: String,
+        val password: String?,
+        val userType: UserType
+)
+
+// -------------------------------------------
+// Currently copied from processor directly
+// This should be changed later
+// -------------------------------------------
+
+interface StorageRequest {
+    val header: RequestHeader
+}
+
+data class RequestHeader(
+        val uuid: String,
+        val performedFor: ProxyClient
+)
+
+// This will chnage over time. Should use a token instead of a straight password. We won't need the username at that
+// point, since we could retrieve this from the auth service instead.
+data class ProxyClient(val username: String, val password: String)
+
+
+data class CreateUserRequest(
+        override val header: RequestHeader,
+
+        val username: String,
+        val password: String?,
+        val userType: UserType // <-- Shared type that lives inside storage interface
+) : StorageRequest
+
+data class ModifyUserRequest(
+        override val header: RequestHeader,
+
+        val currentUsername: String,
+        val newPassword: String?,
+        val newUserType: UserType?
+) : StorageRequest
+
+class StorageResponse<out InputType : Any>(
+        val successful: Boolean,
+        val errorMessage: String?,
+        val input: InputType
+)
+
+object StorageProcessor {
+    val PREFIX = "storage"
+}
+
+object UserGroupsProcessor {
+    val PREFIX = "${StorageProcessor.PREFIX}.ugs"
+
+    val CreateUser = RequestResponseStream.create<CreateUserRequest>("$PREFIX.create_user")
+    val ModifyUser = RequestResponseStream.create<ModifyUserRequest>("$PREFIX.modify_user")
+    val Bomb = RequestResponseStream.create<Unit>("$PREFIX.BOMB9392") // TODO FIXME REMOVE THIS LATER
+}
+
+enum class UserType {
+    USER,
+    ADMIN,
+    GROUP_ADMIN
+}
+
+// Potential candidates for inclusion in kafka-common if they prove useful
+const val REQUEST = "request"
+const val RESPONSE = "response"
+const val POLICY = "policy"
+
+@Suppress("MemberVisibilityCanPrivate")
+class RequestResponseStream<RequestType : Any>(
+        topicName: String,
+        requestSerde: Serde<RequestType>,
+        responseSerde: Serde<StorageResponse<RequestType>>
+) {
+    val requestStream = StreamDescription(
+            "$REQUEST.$topicName",
+            Serdes.String(),
+            requestSerde
+    )
+
+    val responseStream = StreamDescription(
+            "$RESPONSE.$topicName",
+            Serdes.String(),
+            responseSerde
+    )
+
+    companion object {
+        inline fun <reified RequestType : Any> create(topicName: String): RequestResponseStream<RequestType> {
+            return RequestResponseStream(topicName, jsonSerde(), jsonSerde())
+        }
+    }
 }
 
