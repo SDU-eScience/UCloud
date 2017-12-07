e754b7cd946ce633f39b6d8a956ab90ad4673bc2
Dan Sebastian Thrane <dthrane@gmail.com>
2017-12-07 13:40:08 +0100

    Moves call description test objects out of app-service

--- app-service/src/main/kotlin/org/esciencecloud/abc/http/AppController.kt
+++ app-service/src/main/kotlin/org/esciencecloud/abc/http/AppController.kt
@@ -1,19 +1,9 @@
 package org.esciencecloud.abc.http
 
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
 import io.ktor.http.HttpStatusCode
-import io.ktor.jackson.jackson
 import io.ktor.routing.Route
-import io.ktor.routing.route
-import io.ktor.routing.routing
-import io.ktor.server.cio.CIO
-import io.ktor.server.engine.embeddedServer
 import org.esciencecloud.abc.api.HPCApplicationDescriptions
 import org.esciencecloud.abc.services.ApplicationDAO
-import org.esciencecloud.client.GatewayJobResponse
 import org.esciencecloud.service.implement
 
 class AppController(private val source: ApplicationDAO) {
@@ -35,31 +25,5 @@ class AppController(private val source: ApplicationDAO) {
         implement(HPCApplicationDescriptions.ListAll.description) {
             ok(source.all())
         }
-
-        implement(HPCApplicationDescriptions.Test.description) {
-            ok(it)
-        }
-
-        implement(HPCApplicationDescriptions.AppRequest.Start.description) {
-            println(it)
-            ok(GatewayJobResponse.started("123", 1L, 1, 1L))
-        }
     }
-}
-
-fun main(args: Array<String>) {
-    embeddedServer(CIO, port = 8080) {
-        install(CallLogging)
-        install(ContentNegotiation) {
-            jackson {
-                registerKotlinModule()
-            }
-        }
-
-        routing {
-            route("/hpc/apps") {
-                AppController(ApplicationDAO).configure(this)
-            }
-        }
-    }.start(wait = true)
 }
--- service-common/src/test/kotlin/org/esciencecloud/service/RESTCallDescriptionsTest.kt
+++ service-common/src/test/kotlin/org/esciencecloud/service/RESTCallDescriptionsTest.kt
@@ -1,30 +1,31 @@
-package org.esciencecloud.abc.api
+package org.esciencecloud.service
 
 import io.netty.handler.codec.http.HttpMethod
 import org.esciencecloud.client.AuthenticatedCloud
-import org.esciencecloud.client.KafkaCallDescriptionBundle
 import org.esciencecloud.client.RESTDescriptions
 import org.esciencecloud.client.bindEntireRequestFromBody
 
-object HPCApplicationDescriptions : RESTDescriptions() {
-    val baseContext = "/hpc/apps/"
+class RESTCallDescriptionsTest {
+    // TODO Actually write tests for these things
+}
 
-    data class FindAllByName(val name: String) {
-        companion object {
-            val description = callDescription<FindAllByName, List<ApplicationDescription>, List<ApplicationDescription>> {
-                path {
-                    using(baseContext)
-                    +boundTo(FindAllByName::name)
-                }
-            }
-        }
+object Descriptions : RESTDescriptions() {
+    val baseContext = "/foo"
 
-        suspend fun call(cloud: AuthenticatedCloud) = description.prepare(this).call(cloud)
+    // Model
+    data class DummyModel(val text: String)
+
+    enum class TestEnum {
+        A,
+        B,
+        C,
+        D
     }
 
+    // Query and command objects
     data class FindByNameAndVersion(val name: String, val version: String) {
         companion object {
-            val description = callDescription<FindByNameAndVersion, ApplicationDescription, String> {
+            val description = callDescription<FindByNameAndVersion, DummyModel, DummyModel> {
                 path {
                     using(baseContext)
                     +boundTo(FindByNameAndVersion::name)
@@ -38,16 +39,33 @@ object HPCApplicationDescriptions : RESTDescriptions() {
 
     object ListAll {
         // TODO Pagination will be required for this
-        val description = callDescription<Unit, List<ApplicationDescription>, List<ApplicationDescription>> {
+        val description = callDescription<Unit, List<DummyModel>, List<DummyModel>> {
             path { using(baseContext) }
         }
 
         suspend fun call(cloud: AuthenticatedCloud) = description.prepare(Unit).call(cloud)
     }
 
+
+    data class Test(val name: String, val notRequired: Int?, val enum: TestEnum?) {
+        companion object {
+            val description = callDescription<Test, Test, Map<String, Any?>> {
+                path {
+                    using(baseContext)
+                    +"test"
+                    +boundTo(Test::name)
+                    +boundTo(Test::notRequired)
+                    +boundTo(Test::enum)
+                }
+            }
+        }
+
+        suspend fun call(cloud: AuthenticatedCloud) = description.prepare(this).call(cloud)
+    }
+
     sealed class AppRequest {
         data class Start(
-                val application: NameAndVersion,
+                val application: DummyModel,
                 val parameters: Map<String, Any>
         ) : AppRequest() {
             companion object {
@@ -82,13 +100,6 @@ object HPCApplicationDescriptions : RESTDescriptions() {
             }
 
             suspend fun call(cloud: AuthenticatedCloud) = description.prepare(this).call(cloud)
-        }
-
-        companion object {
-            // TODO This is not a robust solution. Will crash if not lazy
-            val descriptions: KafkaCallDescriptionBundle<AppRequest> by lazy {
-                listOf(AppRequest.Start.description, AppRequest.Cancel.description)
-            }
         }
     }
 }
