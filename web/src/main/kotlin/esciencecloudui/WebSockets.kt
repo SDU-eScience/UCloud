7bb0b179a44517f4dd7c379981b915d01aa1423d
Tiggles <jonas@hinchely.dk>
2017-12-08 15:36:58 +0100

    Send text to websocket from KTOR when connection is open

--- web/src/main/kotlin/esciencecloudui/WebSockets.kt
+++ web/src/main/kotlin/esciencecloudui/WebSockets.kt
@@ -1,6 +1,7 @@
 import esciencecloudui.requireAuthentication
 import io.ktor.routing.Route
 import io.ktor.websocket.Frame
+import io.ktor.websocket.FrameType
 import io.ktor.websocket.readText
 import io.ktor.websocket.webSocket
 import kotlinx.coroutines.experimental.channels.consumeEach
@@ -8,10 +9,6 @@ import kotlinx.coroutines.experimental.channels.consumeEach
 fun Route.webSockets() {
     requireAuthentication()
     webSocket("/analyses") {
-        incoming.consumeEach {
-            if (it is Frame.Text) {
-                println(it.readText())
-            }
-        }
+        send(Frame.Text("[Hello] there."))
     }
 }
