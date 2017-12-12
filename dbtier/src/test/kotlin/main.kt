37ff17b148223182d9d80c79485258ab049250ef
bhjakobsen <bhjakobsen@gmail.com>
2017-12-12 10:18:05 +0100

    Streamlined - column names

--- dbtier/src/test/kotlin/main.kt
+++ dbtier/src/test/kotlin/main.kt
@@ -1,6 +1,6 @@
-import escienceclouddb.payloadmodels.AppUiCommand
-import escienceclouddb.payloadmodels.App_payload
-
-fun main(args: Array<String>) {
-    val testApp = App_payload("aadfsdafsfds", AppUiCommand.create,0,"","")
-}
+//import sduclouddb.payloadmodels.AppUiCommand
+//import sduclouddb.payloadmodels.App_payload
+//
+//fun main(args: Array<String>) {
+//    val testApp = App_payload("aadfsdafsfds", AppUiCommand.create,0,"","")
+//}
