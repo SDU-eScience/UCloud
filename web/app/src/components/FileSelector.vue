462f8de798e04bd440700e423a334b6f351f819b
Tiggles <jonas@hinchely.dk>
2017-12-13 15:28:58 +0100

    Only allow creation of folder through file selector when shown. Loading icon added. Writes if folder is empty. Cancelling creation of folder no longer writes that it was successfull. Sort files and folders.

--- web/app/src/components/FileSelector.vue
+++ web/app/src/components/FileSelector.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
-    <div class="btn btn-default" v-on:click="show">Browse</div><div v-if="selectedFile">Currently selected file: {{ selectedFile.path.name }}</div>
+    <div class="btn btn-default" v-on:click="show">Browse</div>
+    <div v-if="selectedFile">Currently selected file: {{ selectedFile.path.name }}</div>
     <div ref="root" tabindex="-1" role="dialog" v-bind:class="{ hidden: !isShown }" class="modal modal-center"
          style="display: block;">
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
+          <loading-icon v-if="loading"></loading-icon>
           <div class="modal-header">
-            <div data-dismiss="modal" class="pull-right clickable"><em class="ion-close-round text-soft" v-on:click="hide()"></em></div>
+            <div data-dismiss="modal" class="pull-right clickable"><em class="ion-close-round text-soft"
+                                                                       v-on:click="hide()"></em></div>
             <ol v-cloak class="breadcrumb">
               <li v-for="breadcrumb in breadcrumbs" class="breadcrumb-item"><a
                 v-on:click="changePath(breadcrumb.second)"> {{breadcrumb.first}}</a></li>
             </ol>
             <h4 class="modal-title"><span>File selector</span></h4>
           </div>
-          <div class="modal-body pre-scrollable">
+          <div v-if="!loading" class="modal-body pre-scrollable">
+            <h4 v-if="!files.length">
+              <small>No files in current folder.</small>
+            </h4>
             <table class="table-datatable table table-striped table-hover mv-lg">
               <thead>
               <tr>
@@ -28,7 +34,7 @@
             </table>
           </div>
           <div class="modal-body">
-            <button class="btn btn-info" @click="createFolder">Create new folder</button>
+            <button class="btn btn-info" :disabled="!isShown" @click="createFolder">Create new folder</button>
           </div>
         </div>
       </div>
@@ -39,14 +45,17 @@
 <script>
   import $ from 'jquery'
   import swal from 'sweetalert2'
+  import LoadingIcon from "./LoadingIcon";
 
   export default {
+    components: {LoadingIcon},
     name: 'file-selector',
 
     data() {
       return {
         path: '/',
         files: [],
+        loading: true,
         breadcrumbs: [],
         isShown: false,
         selectedFile: null
@@ -63,6 +72,7 @@
             return a.path.name.localeCompare(b.path.name);
           }
         });
+        this.loading = false;
       });
       $.getJSON("/api/getBreadcrumbs", {path: this.path}).then((breadcrumbs) => {
         this.breadcrumbs = breadcrumbs
@@ -71,8 +81,19 @@
     },
     watch: {
       path: function () {
+        this.loading = true;
+        this.files = [];
         $.getJSON('/api/getFiles', {path: this.path}).then((files) => {
-          this.files = files;
+          this.files = files.sort((a, b) => {
+            if (a.type === "DIRECTORY" && b.type !== "DIRECTORY")
+              return -1;
+            else if (b.type === "DIRECTORY" && a.type !== "DIRECTORY")
+              return 1;
+            else {
+              return a.path.name.localeCompare(b.path.name);
+            }
+          });
+          this.loading = false;
         });
         $.getJSON("/api/getBreadcrumbs", {path: this.path}).then((breadcrumbs) => {
           this.breadcrumbs = breadcrumbs
@@ -118,22 +139,25 @@
           text: "Input the folder name:",
           input: "text",
           showCancelButton: true,
-          inputPlaceholder: "Folder name..."
-        }).then((inputValue) => {
-          if (inputValue === false) return false;
-          if (inputValue === "") {
-            swal.showInputError("You need to enter a folder name.");
-            return false
+          inputPlaceholder: "Folder name...",
+          preConfirm: (text) => {
+            if (text === "")
+              swal.showValidationError("You need to enter a folder name.");
           }
-          $.getJSON("/api/createDir", { dirPath: currentDir + inputValue.value } , (result) => {
-            if (result === 200) {
-              swal("Success", "Folder " + currentDir + inputValue.value + " created", "success");
-            } else {
-              swal("Error", "Folder " + currentDir + inputValue.value + " was not created", "error");
-            }
-          });
+        }).then((inputValue) => {
+          if (inputValue.dismiss !== 'cancel') {
+            $.getJSON("/api/createDir", {dirPath: currentDir + inputValue.value}, (result) => {
+              if (result === 200) {
+                swal("Success", "Folder " + currentDir + inputValue.value + " created", "success");
+              } else {
+                swal("Error", "Folder " + currentDir + inputValue.value + " was not created", "error");
+              }
+            });
+          }
         });
       }
     }
   }
 </script>
+
+
