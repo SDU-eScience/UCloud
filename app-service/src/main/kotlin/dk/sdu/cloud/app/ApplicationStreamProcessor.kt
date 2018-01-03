f76e6c76c19c37e59738aa55a18496c0b96d688f
Dan Sebastian Thrane <dthrane@gmail.com>
2018-01-03 13:38:53 +0100

    Minor changes

--- app-service/src/main/kotlin/dk/sdu/cloud/app/ApplicationStreamProcessor.kt
+++ app-service/src/main/kotlin/dk/sdu/cloud/app/ApplicationStreamProcessor.kt
@@ -2,7 +2,6 @@ package dk.sdu.cloud.app
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.github.zafarkhaja.semver.Version
 import dk.sdu.cloud.app.api.AppServiceDescription
 import dk.sdu.cloud.app.http.AppController
 import dk.sdu.cloud.app.http.JobController
@@ -14,6 +13,13 @@ import dk.sdu.cloud.app.services.*
 import dk.sdu.cloud.app.services.ssh.SSHConnectionPool
 import dk.sdu.cloud.app.services.ssh.SimpleSSHConfig
 import dk.sdu.cloud.service.*
+import dk.sdu.cloud.service.KafkaUtil.retrieveKafkaProducerConfiguration
+import dk.sdu.cloud.service.KafkaUtil.retrieveKafkaStreamsConfiguration
+import dk.sdu.cloud.storage.Error
+import dk.sdu.cloud.storage.ext.StorageConnection
+import dk.sdu.cloud.storage.ext.StorageConnectionFactory
+import dk.sdu.cloud.storage.ext.irods.IRodsConnectionInformation
+import dk.sdu.cloud.storage.ext.irods.IRodsStorageConnectionFactory
 import io.ktor.application.ApplicationCall
 import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
@@ -24,40 +30,27 @@ import io.ktor.response.respond
 import io.ktor.routing.route
 import io.ktor.util.AttributeKey
 import kotlinx.coroutines.experimental.runBlocking
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.StreamsConfig
-import dk.sdu.cloud.storage.Error
-import dk.sdu.cloud.storage.ext.StorageConnection
-import dk.sdu.cloud.storage.ext.StorageConnectionFactory
-import dk.sdu.cloud.storage.ext.irods.IRodsConnectionInformation
-import dk.sdu.cloud.storage.ext.irods.IRodsStorageConnectionFactory
 import org.irods.jargon.core.connection.AuthScheme
 import org.irods.jargon.core.connection.ClientServerNegotiationPolicy
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
-import java.util.*
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
-import kotlin.collections.set
 
 data class HPCConfig(
-        val kafka: KafkaConfiguration,
+        val connection: RawConnectionConfig,
         val ssh: SimpleSSHConfig,
         val storage: StorageConfiguration,
-        val rpc: RPCConfiguration,
-        val zookeeper: ZooKeeperHostInfo
+        val rpc: RPCConfiguration
 )
 
 data class StorageConfiguration(val host: String, val port: Int, val zone: String)
-data class KafkaConfiguration(val servers: List<String>)
 data class RPCConfiguration(val secretToken: String)
 
 private val storageConnectionKey = AttributeKey<StorageConnection>("StorageSession")
@@ -65,9 +58,7 @@ val ApplicationCall.storageConnection get() = attributes[storageConnectionKey]
 
 class ApplicationStreamProcessor(
         private val config: HPCConfig,
-        private val storageConnectionFactory: StorageConnectionFactory,
-        private val hostname: String,
-        private val rpcPort: Int
+        private val storageConnectionFactory: StorageConnectionFactory
 ) {
     private val log = LoggerFactory.getLogger(ApplicationStreamProcessor::class.java)
 
@@ -77,31 +68,17 @@ class ApplicationStreamProcessor(
     private lateinit var scheduledExecutor: ScheduledExecutorService
     private lateinit var slurmPollAgent: SlurmPollAgent
 
-    private fun retrieveKafkaStreamsConfiguration(): Properties = Properties().apply {
-        this[StreamsConfig.APPLICATION_ID_CONFIG] = "storage-processor"
-        this[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = config.kafka.servers.joinToString(",")
-        this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest" // Don't miss any events
-        this[StreamsConfig.APPLICATION_SERVER_CONFIG] = "$hostname:$rpcPort"
-    }
-
-    private fun retrieveKafkaProducerConfiguration(): Properties = Properties().apply {
-        this[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = config.kafka.servers.joinToString(",")
-        this[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName!!
-        this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName!!
-    }
-
     fun start() {
         // TODO How do we handle deserialization exceptions???
         // TODO Create some component interfaces
         // TODO This would most likely be a lot better if we could use DI in this
         if (initialized) throw IllegalStateException("Already started!")
 
-        val serviceDefinition = ServiceDefinition(AppServiceDescription.name,
-                Version.valueOf(AppServiceDescription.version))
-        val instance = ServiceInstance(serviceDefinition, hostname, rpcPort)
+        val connConfig = config.connection.processed
+        val instance = AppServiceDescription.instance(connConfig)
 
         val (zk, node) = runBlocking {
-            val zk = ZooKeeperConnection(listOf(config.zookeeper)).connect()
+            val zk = ZooKeeperConnection(connConfig.zookeeper.servers).connect()
             val node = zk.registerService(instance)
 
             Pair(zk, node)
@@ -109,13 +86,13 @@ class ApplicationStreamProcessor(
 
         log.info("Init Core Services")
         val streamBuilder = StreamsBuilder()
-        val producer = KafkaProducer<String, String>(retrieveKafkaProducerConfiguration())
+        val producer = KafkaProducer<String, String>(retrieveKafkaProducerConfiguration(connConfig))
         scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
 
         log.info("Init Application Services")
         val sshPool = SSHConnectionPool(config.ssh)
 
-        val hpcStore = HPCStore(hostname, rpcPort, config.rpc)
+        val hpcStore = HPCStore(connConfig.service.hostname, connConfig.service.port, config.rpc)
         val streamService = HPCStreamService(streamBuilder, producer)
         val sbatchGenerator = SBatchGenerator()
         slurmPollAgent = SlurmPollAgent(sshPool, scheduledExecutor, 0L, 15L, TimeUnit.SECONDS)
@@ -135,12 +112,12 @@ class ApplicationStreamProcessor(
         slurmPollAgent.start()
 
         log.info("Starting Core Services")
-        streamProcessor = KafkaStreams(streamBuilder.build(), retrieveKafkaStreamsConfiguration())
+        streamProcessor = KafkaStreams(streamBuilder.build(), retrieveKafkaStreamsConfiguration(connConfig))
         streamProcessor.start()
         streamProcessor.addShutdownHook()
 
         log.info("Starting HTTP Server")
-        rpcServer = HTTPServer(hostname, rpcPort)
+        rpcServer = HTTPServer(connConfig.service.hostname, connConfig.service.port)
         rpcServer.start {
             hpcStore.init(streamProcessor, this)
 
@@ -202,6 +179,7 @@ fun Exception.stackTraceToString(): String = StringWriter().apply { printStackTr
 fun main(args: Array<String>) {
     val mapper = jacksonObjectMapper()
     val hpcConfig = mapper.readValue<HPCConfig>(File("hpc_conf.json"))
+    hpcConfig.connection.configure(AppServiceDescription, 42200)
 
     val irodsConnectionFactory = IRodsStorageConnectionFactory(IRodsConnectionInformation(
             host = hpcConfig.storage.host,
@@ -212,7 +190,7 @@ fun main(args: Array<String>) {
             authScheme = AuthScheme.PAM
     ))
 
-    val processor = ApplicationStreamProcessor(hpcConfig, irodsConnectionFactory, "localhost", 42200)
+    val processor = ApplicationStreamProcessor(hpcConfig, irodsConnectionFactory)
     processor.start()
 }
 
