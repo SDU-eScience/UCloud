d80aaec5ae09ef05ce7d48290da9178b0818c3c5
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-02-07 14:19:50 +0100

    Package updates

--- frontend-web/webclient/__tests__/UtilityFuntions.test.js
+++ frontend-web/webclient/__tests__/UtilityFuntions.test.js
@@ -1,5 +1,5 @@
-import { getParentPath } from "../app/UtilityFunctions";
+const uf = require("../app/UtilityFunctions");
 
 test("Get parent path for file", () => {
-    expect(getParentPath("/path/to/home/file")).toBe("/path/to/home/");
+    expect(uf.getParentPath("/path/to/home/file")).toBe("/path/to/home/");
 });
