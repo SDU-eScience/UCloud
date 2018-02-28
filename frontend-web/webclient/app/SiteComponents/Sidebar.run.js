0e833ad4e4ff9557bb22326610e1a2b60c2bb97b
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-02-28 14:31:28 +0100

    Removed som JQuery usage for sidebar

--- frontend-web/webclient/app/SiteComponents/Sidebar.run.js
+++ frontend-web/webclient/app/SiteComponents/Sidebar.run.js
@@ -1,71 +1,38 @@
 import $ from 'jquery';
 
 function sidebarNav() {
+    let sidebarNav = document.querySelector(".sidebar-nav");
 
-    var $sidebarNav = $('.sidebar-nav');
-    var $sidebarContent = $('.sidebar-content');
-
-    activate($sidebarNav);
-
-    $sidebarNav.on('click', function(event) {
-        var item = getItemElement(event);
+    sidebarNav.onclick = (event) => {
+        let item = getItemElement(event);
         // check click is on a tag
         if (!item) return;
-
-        var ele = $(item),
-            liparent = ele.parent()[0];
-
-        var lis = ele.parent().parent().children(); // markup: ul > li > a
-        // remove .active from childs
-        lis.find('li').removeClass('active');
-        // remove .active from siblings ()
-        $.each(lis, function(idx, li) {
-            if (li !== liparent)
-                $(li).removeClass('active');
-        });
-
-        var next = ele.next();
-        if (next.length && next[0].tagName === 'UL') {
-            ele.parent().toggleClass('active');
+        let liParent = item.parentNode;
+        let lis = item.parentNode.parentNode.children; // markup: ul > li > a
+        // remove .active from children
+        for (let li of lis) {
+            console.log("DOM",li);
+            if (li !== liParent) {
+                li.classList.remove("active");
+            }
+        }
+        let next = item.nextSibling;
+        if (next && next.tagName === 'UL') {
+            item.parentNode.classList.toggle('active');
             event.preventDefault();
         }
-    });
-
-    // find the a element in click context
-    // doesn't check deeply, asumens two levels only
-    function getItemElement(event) {
-        var element = event.target,
-            parent = element.parentNode;
-        if (element.tagName.toLowerCase() === 'a') return element;
-        if (parent.tagName.toLowerCase() === 'a') return parent;
-        if (parent.parentNode.tagName.toLowerCase() === 'a') return parent.parentNode;
-    }
-
-    function activate(sidebar) {
-        sidebar.find('a').each(function() {
-            var href = $(this).attr('href').replace('#', '');
-            if (href !== '' && window.location.href.indexOf(href) >= 0) {
-                var item = $(this).parents('li').addClass('active');
-                // Animate scrolling to focus active item
-                // $sidebarContent.animate({
-                //     scrollTop: $sidebarContent.scrollTop() + item.position().top
-                // }, 1200);
-                return false; // exit foreach
-            }
-        });
-    }
-
-    var layoutContainer = $('.layout-container');
-    var $body = $('body');
+    };
+    let layoutContainer = $('.layout-container');
+    let $body = $('body');
     // Handler to toggle sidebar visibility on mobile
-    $('#sidebar-toggler').click(function(e) {
+    $('#sidebar-toggler').click(function (e) {
         e.preventDefault();
         layoutContainer.toggleClass('sidebar-visible');
         // toggle icon state
         $(this).parent().toggleClass('active');
     });
     // Close sidebar when click on backdrop
-    $('.sidebar-layout-obfuscator').click(function(e) {
+    $('.sidebar-layout-obfuscator').click(function (e) {
         e.preventDefault();
         layoutContainer.removeClass('sidebar-visible');
         // restore icon
@@ -73,7 +40,7 @@ function sidebarNav() {
     });
 
     // Handler to toggle sidebar visibility on desktop
-    $('#offcanvas-toggler').click(function(e) {
+    $('#offcanvas-toggler').click(function (e) {
         e.preventDefault();
         $body.toggleClass('offcanvas-visible');
         // toggle icon state
@@ -82,13 +49,22 @@ function sidebarNav() {
 
     // remove desktop offcanvas when app changes to mobile
     // so when it returns, the sidebar is shown again
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         if (window.innerWidth < 768) {
             $body.removeClass('offcanvas-visible');
             $('#offcanvas-toggler').parent().addClass('active');
         }
     });
+}
 
+// find the a element in click context
+// doesn't check deeply, assuming two levels only
+function getItemElement(event) {
+    let element = event.target,
+        parent = element.parentNode;
+    if (element.tagName.toLowerCase() === "a") return element;
+    if (parent.tagName.toLowerCase() === "a") return parent;
+    if (parent.parentNode.tagName.toLowerCase() === "a") return parent.parentNode;
 }
 
 export default sidebarNav;
