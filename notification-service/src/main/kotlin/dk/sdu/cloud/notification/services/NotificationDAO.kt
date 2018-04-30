a5324fdafdc444d4b37eb8bb2d0fa7978d16a887
Dan Sebastian Thrane <dthrane@gmail.com>
2018-04-30 12:05:48 +0200

    Sorting of notifications (and docs)

--- notification-service/src/main/kotlin/dk/sdu/cloud/notification/services/NotificationDAO.kt
+++ notification-service/src/main/kotlin/dk/sdu/cloud/notification/services/NotificationDAO.kt
@@ -10,6 +10,14 @@ import kotlin.math.min
 val FIRST_PAGE = NormalizedPaginationRequest(null, null)
 
 interface NotificationDAO {
+    /**
+     * Retrieves a page of notifications. The notifications must be sorted by descending time stamp
+     *
+     * @param user The user for which the notifications should be retrieved
+     * @param type The type of notification. If null all types will be accepted
+     * @param since Don't return notifications from before this timestamp
+     * @param paginationRequest Controls pagination of results
+     */
     fun findNotifications(
         user: String,
         type: String? = null,
@@ -17,10 +25,28 @@ interface NotificationDAO {
         paginationRequest: NormalizedPaginationRequest = FIRST_PAGE
     ): Page<Notification>
 
+    /**
+     * Creates a [notification] for [user]
+     *
+     * The [Notification.id] parameter is ignored.
+     *
+     * @return The ID of the newly created notification
+     */
     fun create(user: String, notification: Notification): NotificationId
 
+    /**
+     * Deletes a notification with [id]
+     *
+     * @return `true` if the notification exists and was deleted, `false` if the notification does not exist
+     */
     fun delete(id: NotificationId): Boolean
 
+    /**
+     * Marks a notification with [id] for [user] as read
+     *
+     * @return `true` if the notification exists and was marked as read. `false` will be returned if the
+     * notification doesn't exist
+     */
     fun markAsRead(user: String, id: NotificationId): Boolean
 }
 
@@ -43,6 +69,7 @@ class InMemoryNotificationDAO : NotificationDAO {
                 if (since == null) true
                 else it.ts >= since
             }
+            .sortedByDescending { it.ts }
 
         val startIndex = paginationRequest.itemsPerPage * paginationRequest.page
         val page =
