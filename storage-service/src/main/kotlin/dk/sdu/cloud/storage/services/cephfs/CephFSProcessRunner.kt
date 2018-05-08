28317e105d543b7c23edce999a5de569687cdc3b
Dan Sebastian Thrane <dthrane@gmail.com>
2018-05-08 14:29:02 +0200

    Boundary contained streams and processes. Required for #148

--- storage-service/src/main/kotlin/dk/sdu/cloud/storage/services/cephfs/CephFSProcessRunner.kt
+++ storage-service/src/main/kotlin/dk/sdu/cloud/storage/services/cephfs/CephFSProcessRunner.kt
@@ -1,10 +1,14 @@
 package dk.sdu.cloud.storage.services.cephfs
 
+import dk.sdu.cloud.service.GuardedOutputStream
 import dk.sdu.cloud.storage.util.BashEscaper
+import dk.sdu.cloud.storage.util.BoundaryContainedStream
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.Closeable
 import java.io.InputStream
 import java.io.OutputStream
+import java.util.*
 
 private inline fun Logger.debug(closure: () -> String) {
     if (isDebugEnabled) debug(closure())
@@ -12,7 +16,7 @@ private inline fun Logger.debug(closure: () -> String) {
 
 data class InMemoryProcessResultAsString(val status: Int, val stdout: String, val stderr: String)
 
-interface IProcess {
+interface IProcess : Closeable {
     val inputStream: InputStream
     val errorStream: InputStream
     val outputStream: OutputStream
@@ -30,11 +34,15 @@ class JavaProcess(private val realProcess: Process) : IProcess {
     override fun waitFor(): Int {
         return realProcess.waitFor()
     }
+
+    override fun close() {
+        realProcess.destroy()
+    }
 }
 
 typealias ProcessRunnerFactory = (user: String) -> ProcessRunner
 
-interface ProcessRunner {
+interface ProcessRunner : Closeable {
     val user: String
 
     fun run(command: List<String>, directory: String? = null): IProcess
@@ -60,6 +68,7 @@ interface ProcessRunner {
     }
 }
 
+@Suppress("FunctionName")
 fun SimpleCephFSProcessRunner(cloudToCephFsDao: CloudToCephFsDao, isDevelopment: Boolean): ProcessRunnerFactory {
     return { user: String ->
         CephFSProcessRunner(cloudToCephFsDao, isDevelopment, user)
@@ -70,7 +79,7 @@ class CephFSProcessRunner(
     private val cloudToCephFsDao: CloudToCephFsDao,
     private val isDevelopment: Boolean,
     override val user: String
-): ProcessRunner {
+) : ProcessRunner {
     private fun asUser(cloudUser: String): List<String> {
         val user = cloudToCephFsDao.findUnixUser(cloudUser) ?: throw IllegalStateException("Could not find user")
         return if (!isDevelopment) listOf("sudo", "-u", user) else emptyList()
@@ -86,15 +95,140 @@ class CephFSProcessRunner(
                 "cd ${BashEscaper.safeBashArgument(directory)} ; " +
                         command.joinToString(" ") { BashEscaper.safeBashArgument(it) }
             }
+            log.debug("Running command (user=$user): $bashCommand")
 
             val wrappedCommand = listOf("bash", "-c", bashCommand)
-            log.debug(wrappedCommand.toString())
             command(prefix + wrappedCommand)
         }.start().let { JavaProcess(it) }
+    }
+
+    override fun close() {
+        // No-op
     }
 
     companion object {
         private val log =
             LoggerFactory.getLogger(CephFSProcessRunner::class.java)
     }
+}
+
+fun main(args: Array<String>) {
+    val streamingProcessRunner = StreamingProcessRunner(CloudToCephFsDao(true), true, "jonas@hinchely.dk")
+    println(streamingProcessRunner.runWithResultAsInMemoryString(listOf("echo", "Hello!", "World!")))
+    println("Running last")
+    println(streamingProcessRunner.runWithResultAsInMemoryString(listOf("sleep", "1")))
+    println("Done")
+    streamingProcessRunner.close()
+}
+
+class StreamingProcessRunner(
+    private val cloudToCephFsDao: CloudToCephFsDao,
+    private val isDevelopment: Boolean,
+    override val user: String
+) : ProcessRunner {
+    private val bashProcess = run {
+        val unixUser = cloudToCephFsDao.findUnixUser(user) ?: throw IllegalStateException("Could not find user")
+        val command = if (isDevelopment) listOf("bash") else listOf("sudo", "-u", unixUser, "bash")
+
+        ProcessBuilder().apply { command(command) }.start()
+    }
+
+    private fun generateNewBoundary(): String {
+        return "process-runner-boundary-${UUID.randomUUID()}"
+    }
+
+    override fun run(command: List<String>, directory: String?): IProcess {
+        val boundary = generateNewBoundary()
+
+        val bashCommand = if (directory == null) {
+            command.joinToString(" ") { BashEscaper.safeBashArgument(it) }
+        } else {
+            "cd ${BashEscaper.safeBashArgument(directory)} ; " +
+                    command.joinToString(" ") { BashEscaper.safeBashArgument(it) }
+        }
+
+        log.debug("Running command (user=$user): $bashCommand")
+
+        bashProcess.outputStream.apply {
+            write((bashCommand + "\n").toByteArray())
+
+            // Save status, mark end of process, write status
+            write("STATUS=$?\n".toByteArray())
+            write("echo $boundary\n".toByteArray())
+            write("echo $boundary >&2\n".toByteArray())
+            write("echo \$STATUS\n".toByteArray())
+
+            // Always flush at end of command
+            flush()
+        }
+
+        val boundaryBytes = (boundary + '\n').toByteArray()
+        val wrappedStdout = BoundaryContainedStream(boundaryBytes, bashProcess.inputStream)
+        val wrappedStderr = BoundaryContainedStream(boundaryBytes, bashProcess.errorStream)
+        val outputStream = GuardedOutputStream(bashProcess.outputStream)
+
+        return BoundaryContainedProcess(
+            wrappedStdout,
+            wrappedStderr,
+            outputStream,
+            bashProcess.inputStream,
+            boundaryBytes
+        )
+    }
+
+    override fun close() {
+        bashProcess.outputStream.close()
+        bashProcess.destroy()
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(StreamingProcessRunner::class.java)
+    }
+}
+
+class BoundaryContainedProcess(
+    override val inputStream: BoundaryContainedStream,
+    override val errorStream: BoundaryContainedStream,
+    override val outputStream: OutputStream,
+
+    private val originalStream: InputStream,
+    private val boundaryBytes: ByteArray
+) : IProcess {
+    override fun waitFor(): Int {
+        inputStream.discardAll()
+        errorStream.discardAll()
+
+        val remaining = inputStream.readRemainingAfterBoundary()
+
+        val buffer = ByteArray(256)
+        System.arraycopy(remaining, 0, buffer, 0, remaining.size)
+        var ptr = remaining.size
+
+        if (ptr > boundaryBytes.size) {
+            for (i in boundaryBytes.size until ptr) {
+                if (buffer[i] == '\n'.toByte()) {
+                    return String(buffer, boundaryBytes.size, i - boundaryBytes.size).toInt()
+                }
+            }
+        }
+
+        while (true) {
+            val read = originalStream.read()
+            if (ptr < boundaryBytes.size) {
+                buffer[ptr++] = read.toByte()
+            } else {
+                when (read) {
+                    -1 -> throw IllegalStateException("Unexpected end of stream. Boundary not found")
+                    '\n'.toInt() -> return String(buffer, boundaryBytes.size, ptr - boundaryBytes.size).toInt()
+                    else -> buffer[ptr++] = read.toByte()
+                }
+            }
+        }
+    }
+
+    override fun close() {
+        inputStream.discardAll()
+        errorStream.discardAll()
+    }
+
 }
---
+++ storage-service/src/main/kotlin/dk/sdu/cloud/storage/util/BoundaryContainedStream.kt
@@ -0,0 +1,179 @@
+package dk.sdu.cloud.storage.util
+
+import java.io.InputStream
+import kotlin.math.min
+
+class BoundaryContainedStream(
+    private val boundaryBytes: ByteArray,
+    private val delegate: InputStream
+) : InputStream() {
+    private var clearedBytes = 0
+
+    private val internalBuffer = ByteArray(8 * 1024)
+    private var internalPointer = -1
+    private var internalBufferSize = -1
+
+    private var boundaryFound = false
+
+    // We need to, at all times, be sure that we don't deliver data that is part of the boundary.
+    // This means we cannot deliver data if it might be part of the boundary.
+
+    // This might become complicated if the boundary is split across internal buffers.
+    // Given the boundary: "boundary" and the byte stream:
+    // buffer1: [abcdefqboun], buffer2: [ary\n0]
+    // it is important that we don't  return the 'boun' bytes, even if the client isn't requesting more data
+
+    /**
+     * Discards the remaining data in the stream. Boundary and anything after is left in the stream.
+     * See [readRemainingAfterBoundary]
+     */
+    fun discardAll() {
+        val discardBuffer = ByteArray(8 * 1024)
+        while (true) {
+            if (read(discardBuffer) == -1) break
+        }
+    }
+
+    /**
+     * Includes the boundary bytes too.
+     */
+    fun readRemainingAfterBoundary(): ByteArray {
+        return internalBuffer.copyOfRange(internalPointer, internalBufferSize)
+    }
+
+    private fun clearAsMuchAsPossible(): Boolean {
+        assert(clearedBytes == 0)
+        var boundaryPtr = 0
+        var internalPtr = internalPointer
+        while (internalPtr < internalBufferSize) {
+            if (boundaryPtr == boundaryBytes.size) {
+                // Full match
+                break
+            } else if (internalBuffer[internalPtr] != boundaryBytes[boundaryPtr]) {
+                // Not a match, reset the search
+                boundaryPtr = 0
+            } else {
+                // A match, continue the search
+                boundaryPtr++
+            }
+
+            internalPtr++
+        }
+
+        // We don't clear bytes that could be a prefix (or a full match)
+        clearedBytes = (internalPtr - internalPointer) - boundaryPtr
+        return boundaryPtr == boundaryBytes.size
+    }
+
+    private fun readMoreData() {
+        assert(clearedBytes == 0)
+
+        if (internalPointer != -1 && internalPointer != internalBufferSize) {
+            // Copy existing data and read remaining
+            val remainingBufferSize = internalBufferSize - internalPointer
+            System.arraycopy(
+                internalBuffer,
+                internalPointer,
+                internalBuffer,
+                0,
+                remainingBufferSize
+            )
+
+            val read = delegate.read(
+                internalBuffer,
+                remainingBufferSize,
+                internalBuffer.size - remainingBufferSize
+            )
+
+            if (read == -1) throw IllegalStateException("Unexpected end of stream. Boundary not found")
+
+            internalBufferSize = remainingBufferSize + read
+            internalPointer = 0
+        } else {
+            // Do a complete read
+            internalBufferSize = delegate.read(internalBuffer)
+            internalPointer = 0
+        }
+    }
+
+    override fun read(): Int {
+        return if (clearedBytes > 0) {
+            clearedBytes--
+            internalBuffer[internalPointer++].toInt()
+        } else if (!boundaryFound) {
+            readMoreData()
+            boundaryFound = clearAsMuchAsPossible()
+
+            if (clearedBytes == 0) {
+                -1
+            } else {
+                clearedBytes--
+                internalBuffer[internalPointer++].toInt()
+            }
+        } else {
+            -1
+        }
+    }
+
+    override fun read(b: ByteArray): Int {
+        return if (clearedBytes > 0) {
+            val bytesToMove = min(b.size, clearedBytes)
+            System.arraycopy(internalBuffer, internalPointer, b, 0, bytesToMove)
+            internalPointer += bytesToMove
+            clearedBytes -= bytesToMove
+            return bytesToMove
+        } else if (!boundaryFound) {
+            readMoreData()
+            boundaryFound = clearAsMuchAsPossible()
+
+            if (clearedBytes == 0) {
+                -1
+            } else {
+                val bytesToMove = min(b.size, clearedBytes)
+                System.arraycopy(internalBuffer, internalPointer, b, 0, bytesToMove)
+                internalPointer += bytesToMove
+                clearedBytes -= bytesToMove
+                return bytesToMove
+            }
+        } else {
+            -1
+        }
+    }
+
+    override fun read(b: ByteArray, off: Int, len: Int): Int {
+        return if (clearedBytes > 0) {
+            val bytesToMove = min(len, clearedBytes)
+            System.arraycopy(internalBuffer, internalPointer, b, off, bytesToMove)
+            internalPointer += bytesToMove
+            clearedBytes -= bytesToMove
+            return bytesToMove
+        } else if (!boundaryFound) {
+            readMoreData()
+            boundaryFound = clearAsMuchAsPossible()
+
+            if (clearedBytes == 0) {
+                -1
+            } else {
+                val bytesToMove = min(len, clearedBytes)
+                System.arraycopy(internalBuffer, internalPointer, b, off, bytesToMove)
+                internalPointer += bytesToMove
+                clearedBytes -= bytesToMove
+                return bytesToMove
+            }
+        } else {
+            -1
+        }
+    }
+
+    override fun reset() {
+        // No-op
+    }
+
+    override fun close() {
+        // No-op
+    }
+
+    override fun markSupported(): Boolean {
+        return false
+    }
+}
