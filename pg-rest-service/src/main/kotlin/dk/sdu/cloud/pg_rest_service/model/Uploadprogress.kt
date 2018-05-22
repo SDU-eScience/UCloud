1a90cfb4cdad5f149ac73be5898f75932e43cf78
bhjakobsen <bhjakobsen@gmail.com>
2018-05-22 17:30:24 +0200

    Removed temp modules

--- pg-rest-service/src/main/kotlin/dk/sdu/cloud/pg_rest_service/model/Uploadprogress.kt
+++
@@ -1,9 +0,0 @@
-package dk.sdu.cloud.pg_rest_service.model
-import org.jetbrains.exposed.dao.IntIdTable
-
-object Uploadprogress : IntIdTable() {
-	val modified_ts = datetime("modified_ts")
-	val marked_for_delete = integer("marked_for_delete").nullable()
-	val devstagename = text("devstagename").nullable()
-	val created_ts = datetime("created_ts")
-}
