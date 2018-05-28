91a24bd331367925610930c806b4d7bae33ed9c2
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-05-28 10:38:01 +0200

    Updated packages. Have /home/Cloud.homefolder simply as home

--- frontend-web/webclient/app/SiteComponents/Breadcrumbs/Breadcrumbs.tsx
+++ frontend-web/webclient/app/SiteComponents/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Breadcrumb } from "semantic-ui-react";
 import "./Breadcrumbs.scss";
+import { Cloud } from "../../../authentication/SDUCloudObject";
 
 interface Breadcrumbs { currentPath: string, navigate: (path: string) => void }
 export const BreadCrumbs = ({ currentPath, navigate }: Breadcrumbs) => {
@@ -17,7 +18,7 @@ export const BreadCrumbs = ({ currentPath, navigate }: Breadcrumbs) => {
             <Breadcrumb.Divider />
         </React.Fragment>
     ));
-    
+
     return (
         <Breadcrumb className="breadcrumb-margin">
             {breadcrumbs}
@@ -29,6 +30,11 @@ export const BreadCrumbs = ({ currentPath, navigate }: Breadcrumbs) => {
 
 }
 
+type BreadCrumbMapping = {
+    actualPath: string
+    local: string
+}
+
 export function buildBreadCrumbs(path: string) {
     const paths = path.split("/").filter((path: string) => path);
     let pathsMapping = [];
@@ -38,6 +44,10 @@ export function buildBreadCrumbs(path: string) {
             actualPath += paths[j] + "/";
         }
         pathsMapping.push({ actualPath: actualPath, local: paths[i] });
+    }
+    if (path.includes(Cloud.homeFolder)) { // remove first two indices 
+        pathsMapping = 
+            [{ actualPath: Cloud.homeFolder, local: "Home" }].concat(pathsMapping.slice(2, pathsMapping.length));
     }
     return pathsMapping;
 }
