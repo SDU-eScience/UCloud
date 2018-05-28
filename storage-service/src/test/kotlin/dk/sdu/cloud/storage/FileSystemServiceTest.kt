14d126adae469bd86993799f2294063b79f559d0
Dan Sebastian Thrane <dthrane@gmail.com>
2018-05-28 13:29:23 +0200

    Fixes some storage-service tests

--- storage-service/src/test/kotlin/dk/sdu/cloud/storage/FileSystemServiceTest.kt
+++ storage-service/src/test/kotlin/dk/sdu/cloud/storage/FileSystemServiceTest.kt
@@ -3,9 +3,6 @@ package dk.sdu.cloud.storage
 import dk.sdu.cloud.storage.api.AccessRight
 import dk.sdu.cloud.storage.api.FileType
 import dk.sdu.cloud.storage.api.SensitivityLevel
-import dk.sdu.cloud.storage.services.cephfs.CephFSFileSystemService
-import dk.sdu.cloud.storage.services.cephfs.CloudToCephFsDao
-import io.mockk.mockk
 import org.junit.Assert.*
 import org.junit.Test
 import java.io.File
@@ -18,9 +15,9 @@ class FileSystemServiceTest {
         val where = File("/f/shared")
 
         val output = """
-           D,1,509,root,root,4096,1523862649,1523862649,1523862650,1,3,user1,14,user2,2,user3,6,CONFIDENTIAL,.
-           D,0,493,root,root,4096,1523862224,1523862224,1523862237,2,0,CONFIDENTIAL,..
-           F,0,420,root,root,0,1523862649,1523862649,1523862649,3,0,CONFIDENTIAL,qwe
+           D,1,509,root,root,4096,1523862649,1523862649,1523862650,1,3,user1,14,user2,2,user3,6,,CONFIDENTIAL,.
+           D,0,493,root,root,4096,1523862224,1523862224,1523862237,2,0,,CONFIDENTIAL,..
+           F,0,420,root,root,0,1523862649,1523862649,1523862649,3,0,,CONFIDENTIAL,qwe
         """.trimIndent()
 
         val result = service.parseDirListingOutput(where, output, includeImplicit = true).second
@@ -95,16 +92,16 @@ class FileSystemServiceTest {
             Favorites/Jobs_00000000
             /mnt/cephfs/home/jonas@hinchely.dk/Jobs
             1099511627815
-            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,8,1523962672,1523962672,1523962688,1099511627810,0,CONFIDENTIAL,.
-            D,0,493,root,root,4,1523883079,1523883079,1523973678,1099511627777,0,CONFIDENTIAL,..
-            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,1,1523880643,1523880643,1523968202,1099511627816,0,CONFIDENTIAL,A Link to Uploads
-            D,0,493,c_jonas_hinchely_dk,c_jonas_hinchely_dk,25,1523880056,1523880056,1523966653,1099511627819,0,CONFIDENTIAL,Dan's Master Thesis
-            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,1,1524036190,1524036190,1524036193,1099511627814,0,CONFIDENTIAL,Favorites
-            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,4,1523882904,1523882904,1524034628,1099511627815,0,CONFIDENTIAL,Jobs
-            D,0,493,c_jonas_hinchely_dk,c_jonas_hinchely_dk,4,1523880038,1496255424,1524033923,1099511627824,0,CONFIDENTIAL,Link to dir
-            F,0,420,c_jonas_hinchely_dk,c_jonas_hinchely_dk,104,1523880038,1496255424,1496255424,1099511627827,0,CONFIDENTIAL,Link to file.tex
-            D,0,493,c_jonas_hinchely_dk,c_jonas_hinchely_dk,0,1523887389,1523887389,1523887392,1099511627930,0,CONFIDENTIAL,Test
-            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,1,1523880643,1523880643,1523968202,1099511627816,0,CONFIDENTIAL,Uploads
+            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,8,1523962672,1523962672,1523962688,1099511627810,0,,CONFIDENTIAL,.
+            D,0,493,root,root,4,1523883079,1523883079,1523973678,1099511627777,0,,CONFIDENTIAL,..
+            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,1,1523880643,1523880643,1523968202,1099511627816,0,,CONFIDENTIAL,A Link to Uploads
+            D,0,493,c_jonas_hinchely_dk,c_jonas_hinchely_dk,25,1523880056,1523880056,1523966653,1099511627819,0,,CONFIDENTIAL,Dan's Master Thesis
+            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,1,1524036190,1524036190,1524036193,1099511627814,0,,CONFIDENTIAL,Favorites
+            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,4,1523882904,1523882904,1524034628,1099511627815,0,,CONFIDENTIAL,Jobs
+            D,0,493,c_jonas_hinchely_dk,c_jonas_hinchely_dk,4,1523880038,1496255424,1524033923,1099511627824,0,,CONFIDENTIAL,Link to dir
+            F,0,420,c_jonas_hinchely_dk,c_jonas_hinchely_dk,104,1523880038,1496255424,1496255424,1099511627827,0,,CONFIDENTIAL,Link to file.tex
+            D,0,493,c_jonas_hinchely_dk,c_jonas_hinchely_dk,0,1523887389,1523887389,1523887392,1099511627930,0,,CONFIDENTIAL,Test
+            D,0,448,c_jonas_hinchely_dk,c_jonas_hinchely_dk,1,1523880643,1523880643,1523968202,1099511627816,0,,CONFIDENTIAL,Uploads
         """.trimIndent()
         val parsed = service.parseDirListingOutput(File("/home/jonas@hinchely.dk"), output, false, true)
         assertEquals(1, parsed.first.size)
