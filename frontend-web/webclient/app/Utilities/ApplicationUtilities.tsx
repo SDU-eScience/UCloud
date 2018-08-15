7d1e72f8ae78b0ba79ad55e085e2e562a262118a
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-08-15 16:18:47 +0200

    Add version number to exporting/import parameters

--- frontend-web/webclient/app/Utilities/ApplicationUtilities.tsx
+++ frontend-web/webclient/app/Utilities/ApplicationUtilities.tsx
@@ -1,7 +1,8 @@
 import { infoNotification } from "UtilityFunctions";
-import { Application } from "Applications";
+import { Application, ParameterTypes } from "Applications";
 import Cloud from "Authentication/lib";
-import { Page} from "Types";
+import { Page } from "Types";
+
 
 export const hpcJobQuery = (id: string, stdoutLine: number, stderrLine: number, stdoutMaxLines: number = 1000, stderrMaxLines: number = 1000) =>
     `hpc/jobs/follow/${id}?stdoutLineStart=${stdoutLine}&stdoutMaxLines=${stdoutMaxLines}&stderrLineStart=${stderrLine}&stderrMaxLines=${stderrMaxLines}`;
@@ -25,4 +26,36 @@ export const favoriteApplicationFromPage = (application: Application, page: Page
         cloud.delete(`/applications/favorite?name=${info.name}&version=${info.name}`, {})
     } 
     */
+}
+
+export const extractParametersVersion1 = (parameters, allowedParameterKeys, siteVersion: number) => {
+    let extractedParameters = {};
+    if (siteVersion === 1) {
+        allowedParameterKeys.forEach(par => {
+            if (parameters[par.name] !== undefined) {
+                if (compareType(par.type, parameters[par.name])) {
+                    extractedParameters[par.name] = parameters[par.name];
+                }
+            }
+        });
+    }
+    return extractedParameters;
+}
+
+const compareType = (type, parameter): boolean => {
+    switch (type) {
+        case ParameterTypes.Boolean:
+            return typeof parameter === "boolean";
+        case ParameterTypes.Integer:
+            return typeof parameter === "number" && parameter % 1 === 0;
+        case ParameterTypes.FloatingPoint:
+            return typeof parameter === "number";
+        case ParameterTypes.Text:
+            return typeof parameter === "string";
+        case ParameterTypes.InputDirectory:
+        case ParameterTypes.InputFile:
+            return typeof parameter.destination === "string" && typeof parameter.source === "string";
+        default:
+            return false;
+    }
 }
