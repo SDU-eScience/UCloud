08e9f8efc0bc02905c24639438c67f961f35078d
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-08-16 12:17:23 +0200

    Updated packages, and fixed test suite not recompiling correctly

--- frontend-web/webclient/__tests__/UtilityFuntions.test.tsx
+++ frontend-web/webclient/__tests__/UtilityFuntions.test.tsx
@@ -1,73 +1,94 @@
-import { getParentPath, toLowerCaseAndCapitalize, fileSizeToString, getFilenameFromPath } from "UtilityFunctions";
-//import initializeTestCloudObject from "./mock/TestCloudObject";
+import * as UF from "UtilityFunctions";
+import initializeTestCloudObject from "./mock/MockCloudObject";
 
 // GET PARENT PATH
 test("Get parent path for file", () => {
-    expect(getParentPath("/path/to/home/file")).toBe("/path/to/home/");
+    expect(UF.getParentPath("/path/to/home/file")).toBe("/path/to/home/");
 });
 
 test("Ignore additional backslashes", () => {
-   expect(getParentPath("//path///////to///home//////////////file////////////")).toBe("/path/to/home/")
+   expect(UF.getParentPath("//path///////to///home//////////////file////////////")).toBe("/path/to/home/")
 });
 
 test("Empty path", () => {
-   expect(getParentPath("")).toBe("");
+   expect(UF.getParentPath("")).toBe("");
 });
 
 test("Null path", () => {
-    expect(getParentPath(null)).toBe("");
+    expect(UF.getParentPath(null)).toBe("");
 });
 
 // TO LOWER CASE AND CAPITALIZE
 
 test("All upper case and numbers", () => {
-    expect(toLowerCaseAndCapitalize("ABACUS 2.0")).toBe("Abacus 2.0");
+    expect(UF.toLowerCaseAndCapitalize("ABACUS 2.0")).toBe("Abacus 2.0");
 });
 
 test("All lower case", () => {
-    expect(toLowerCaseAndCapitalize("abacus")).toBe("Abacus");
+    expect(UF.toLowerCaseAndCapitalize("abacus")).toBe("Abacus");
 });
 
 test("Mixed case and special characters", () => {
-    expect(toLowerCaseAndCapitalize("aBaCuS 2.0 !@#$%^&*()")).toBe("Abacus 2.0 !@#$%^&*()")
+    expect(UF.toLowerCaseAndCapitalize("aBaCuS 2.0 !@#$%^&*()")).toBe("Abacus 2.0 !@#$%^&*()")
 });
 
 test("Empty string", () => {
-    expect(toLowerCaseAndCapitalize("")).toBe("");
+    expect(UF.toLowerCaseAndCapitalize("")).toBe("");
 });
 
 // FILE SIZE TO STRINGS
 
-test("500 bytes to string", () => {
-    expect(fileSizeToString(500)).toBe("500 B");
-});
+test("500 bytes to string", () =>
+    expect(UF.fileSizeToString(500)).toBe("500 B")
+);
 
-test("1500 bytes to string", () => {
-    expect(fileSizeToString(1500)).toBe("1.50 KB");
-});
+test("1500 bytes to string", () => 
+    expect(UF.fileSizeToString(1500)).toBe("1.50 KB")
+);
 
-test("1500 * 1000 bytes to string ", () => {
-    expect(fileSizeToString(1500 * 1000)).toBe("1.50 MB")
-});
+test("1500 * 1000 bytes to string ", () => 
+    expect(UF.fileSizeToString(1500 * 1000)).toBe("1.50 MB")
+);
 
-test("1500 * 1000**2 bytes to string ", () => {
-    expect(fileSizeToString(1500 * 1000**2)).toBe("1.50 GB")
-});
+test("1500 * 1000**2 bytes to string ", () =>
+    expect(UF.fileSizeToString(1500 * 1000**2)).toBe("1.50 GB")
+);
 
-test("1500 * 1000**3 bytes to string ", () => {
-    expect(fileSizeToString(1500 * 1000**3)).toBe("1.50 TB")
-});
+test("1500 * 1000**3 bytes to string ", () =>
+    expect(UF.fileSizeToString(1500 * 1000**3)).toBe("1.50 TB")
+);
 
-test("1500 * 1000**4 bytes to string ", () => {
-    expect(fileSizeToString(1500 * 1000**4)).toBe("1.50 PB")
-});
+test("1500 * 1000**4 bytes to string ", () => 
+    expect(UF.fileSizeToString(1500 * 1000**4)).toBe("1.50 PB")
+);
 
-test("1500 * 1000**5 bytes to string ", () => {
-    expect(fileSizeToString(1500 * 1000**5)).toBe("1.50 EB")
-});
+test("1500 * 1000**5 bytes to string ", () => 
+    expect(UF.fileSizeToString(1500 * 1000**5)).toBe("1.50 EB")
+);
 
 // Get filename from path
 
-test("Filename from path", () => {
-    expect(getFilenameFromPath("/Home/folder")).toBe("folder");
+test("Filename from path", () =>
+    expect(UF.getFilenameFromPath("/Home/folder")).toBe("folder")
+);
+
+test("Filename from path with special character", () => {
+    expect(UF.getFilenameFromPath("/Home/folder_2 (1)")).toBe("folder_2 (1)");
 });
+
+// Replace homefolder
+
+const mockHomeFolder = "/home/user@mail.co.uk/";
+
+test("Replace homefolder", () =>
+    expect(UF.replaceHomeFolder("/home/user@mail.co.uk/", mockHomeFolder)).toBe("Home/")
+);
+
+test("Replace homefolder subfolder", () =>
+    expect(UF.replaceHomeFolder("/home/user@mail.co.uk/subFolder/withSomething", mockHomeFolder)).toBe("Home/subFolder/withSomething/")
+);
+
+const noHomeFolder = "NotHomeFolder/subfolder/";
+test("Replace homefolder, no homefolder", () =>
+    expect(UF.replaceHomeFolder(noHomeFolder, mockHomeFolder)).toBe(`${noHomeFolder}`)
+);
