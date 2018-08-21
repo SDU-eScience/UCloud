aecc746f43232875e3fdc589dccb72f4b6326f1d
Henrik Schulz <henrikschulz@hotmail.com>
2018-08-21 09:48:58 +0200

    added tests to App-service (66% CC)

---
+++ app-service/src/test/kotlin/dk/sdu/cloud/app/api/AppsAndToolsTest.kt
@@ -0,0 +1,330 @@
+package dk.sdu.cloud.app.api
+
+import io.mockk.mockk
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class AppsAndToolsTest{
+
+    @Test
+    fun `create simple V1 Application description with string invocation`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf("string"),
+            mapOf(Pair("string", mockk(relaxed = true))),
+            listOf("globs")
+        )
+
+        assertEquals("name@2.2", v1.tool.toString())
+        assertEquals("name", v1.name)
+        assertEquals("2.2", v1.version)
+        assertEquals("title", v1.title)
+        assertEquals("name", v1.tool.name)
+        assertEquals("2.2", v1.tool.version)
+        assertEquals("Authors", v1.authors.first())
+        assertEquals("title", v1.title)
+        assertEquals("description", v1.description)
+        assertEquals("string", v1.parameters.keys.first())
+        assertEquals("globs", v1.outputFileGlobs.first())
+
+        val normTool = v1.normalize()
+
+        assertEquals("name", normTool.info.name)
+        assertEquals("2.2", normTool.info.version)
+        assertEquals("title", normTool.title)
+        assertEquals("description", normTool.description)
+        assertEquals("Authors", normTool.authors.first())
+        assertEquals("globs", normTool.outputFileGlobs.first().toString())
+        assertEquals("name", normTool.tool.name)
+        assertEquals("2.2", normTool.tool.version)
+
+    }
+
+    @Test
+    fun `create simple V1 Application description with int invocation`() {
+        ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(2),
+            mapOf(Pair("string", mockk(relaxed = true))),
+            listOf("globs")
+        )
+    }
+
+    @Test
+    fun `create simple V1 Application description with var invocation`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "var"), Pair("vars", "hello"))),
+            mapOf(Pair("hello", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description with var invocation - missing var`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "var"))),
+            mapOf(Pair("hello", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+    @Test
+    fun `create simple V1 Application description with flag invocation`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "flag"), Pair("var", "hello"), Pair("flag", "true"))),
+            mapOf(Pair("hello", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description - not correct type`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "NO"))),
+            mapOf(Pair("hello", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description with flag invocation - missing var`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "flag"))),
+            mapOf(Pair("hello", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description with flag invocation - missing flag`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "flag"), Pair("var", "hello"))),
+            mapOf(Pair("hello", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description with var invocation - missin type tag`() {
+        ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("notType", "var"))),
+            mapOf(Pair("string", mockk(relaxed = true))),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException::class)
+    fun `create simple V1 Application description with missing params`() {
+        val v1 = ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(mapOf(Pair("type", "var"), Pair("vars", "hello"))),
+            mapOf(Pair("string", ApplicationParameter.Bool("hello") )),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException.DuplicateDefinition::class)
+    fun `create simple V1 Application description with duplicate glob`() {
+        ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(2),
+            mapOf(Pair("string", mockk(relaxed = true))),
+            listOf("globs", "globs")
+        )
+    }
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description with bad invocation`() {
+        ApplicationDescription.V1(
+            "name",
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf(Pair("type", "var")),
+            mapOf(Pair("string", mockk(relaxed = true))),
+            listOf("globs")
+        )
+    }
+
+    @Test
+    fun `create simple V1 tool description`() {
+        val v1 = ToolDescription.V1(
+            "name",
+            "2.2",
+            "title",
+            "container",
+            ToolBackend.SINGULARITY,
+            listOf("Authors"),
+            2,
+            2,
+            SimpleDuration(1, 2, 30),
+            listOf("modules"),
+            "description"
+        )
+
+        assertEquals("name", v1.name)
+        assertEquals("2.2", v1.version)
+        assertEquals("title", v1.title)
+        assertEquals("container", v1.container)
+        assertEquals(ToolBackend.SINGULARITY, v1.backend)
+        assertEquals("Authors", v1.authors.first())
+        assertEquals(2, v1.defaultNumberOfNodes)
+        assertEquals(2, v1.defaultTasksPerNode)
+        assertEquals(1, v1.defaultMaxTime.hours)
+        assertEquals(2, v1.defaultMaxTime.minutes)
+        assertEquals(30, v1.defaultMaxTime.seconds)
+        assertEquals("modules", v1.requiredModules.first())
+        assertEquals("description", v1.description)
+
+        val normTool = v1.normalize()
+
+        assertEquals("name", normTool.info.name)
+        assertEquals("2.2", normTool.info.version)
+        assertEquals("title", normTool.title)
+        assertEquals("container", normTool.container)
+        assertEquals(ToolBackend.SINGULARITY, normTool.backend)
+        assertEquals("Authors", normTool.authors.first())
+        assertEquals(2, normTool.defaultNumberOfNodes)
+        assertEquals(2, normTool.defaultTasksPerNode)
+        assertEquals(1, normTool.defaultMaxTime.hours)
+        assertEquals(2, normTool.defaultMaxTime.minutes)
+        assertEquals(30, normTool.defaultMaxTime.seconds)
+        assertEquals("modules", normTool.requiredModules.first())
+        assertEquals("description", normTool.description)
+
+    }
+
+    @Test (expected = ApplicationVerificationException.BadValue::class)
+    fun `create simple V1 Application description - Too long name`() {
+        ApplicationDescription.V1(
+            "name".repeat(1000),
+            "2.2",
+            NameAndVersion("name", "2.2"),
+            listOf("Authors"),
+            "title",
+            "description",
+            listOf("string"),
+            mapOf(Pair("string", mockk(relaxed = true))),
+            listOf("globs")
+        )
+    }
+
+    @Test (expected = ToolVerificationException.BadValue::class)
+    fun `create simple V1 tool description - throws exception (to long name)`() {
+        ToolDescription.V1(
+            "name".repeat(1000),
+            "2.2",
+            "title",
+            "container",
+            ToolBackend.SINGULARITY,
+            listOf("Authors"),
+            2,
+            2,
+            SimpleDuration(1, 2, 30),
+            listOf("modules"),
+            "description"
+        )
+
+    }
+
+    @Test (expected = ToolVerificationException.BadValue::class)
+    fun `create simple V1 tool description - newline in version`() {
+        ToolDescription.V1(
+            "name",
+            "2.2\n",
+            "title",
+            "container",
+            ToolBackend.SINGULARITY,
+            listOf("Authors"),
+            2,
+            2,
+            SimpleDuration(1, 2, 30),
+            listOf("modules"),
+            "description"
+        )
+
+    }
+
+    @Test (expected = ToolVerificationException.BadValue::class)
+    fun `create simple V1 tool description - bad authors`() {
+        ToolDescription.V1(
+            "name",
+            "2.2",
+            "title",
+            "container",
+            ToolBackend.SINGULARITY,
+            listOf("Authors\n", "author"),
+            2,
+            2,
+            SimpleDuration(1, 2, 30),
+            listOf("modules"),
+            "description"
+        )
+
+    }
+}
---
+++ app-service/src/test/kotlin/dk/sdu/cloud/app/http/ToolTest.kt
@@ -0,0 +1,223 @@
+package dk.sdu.cloud.app.http
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import dk.sdu.cloud.app.api.*
+import dk.sdu.cloud.app.services.JobService
+import dk.sdu.cloud.app.services.ToolHibernateDAO
+import dk.sdu.cloud.auth.api.Role
+import dk.sdu.cloud.metadata.utils.withAuthMock
+import dk.sdu.cloud.metadata.utils.withDatabase
+import dk.sdu.cloud.service.Page
+import dk.sdu.cloud.service.db.HibernateSession
+import dk.sdu.cloud.service.db.HibernateSessionFactory
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+
+private fun Application.configureToolServer(
+    db: HibernateSessionFactory,
+    toolDao: ToolHibernateDAO
+) {
+    configureBaseServer(ToolController<HibernateSession>(db, toolDao))
+}
+
+class ToolTest{
+
+    private val mapper = jacksonObjectMapper()
+
+    private val normAppDesc = NormalizedApplicationDescription(
+        NameAndVersion("name", "2.2"),
+        NameAndVersion("name", "2.2"),
+        listOf("Authors"),
+        "title",
+        "app description",
+        mockk(relaxed = true),
+        mockk(relaxed = true),
+        listOf("glob")
+    )
+
+    private val normToolDesc = NormalizedToolDescription(
+        NameAndVersion("name", "2.2"),
+        "container",
+        2,
+        2,
+        SimpleDuration(1, 0, 0),
+        listOf(""),
+        listOf("auther"),
+        "title",
+        "description",
+        ToolBackend.UDOCKER
+    )
+
+    private val tool = Tool(
+        "owner",
+        1234567,
+        123456789,
+        normToolDesc
+    )
+
+    private val app = dk.sdu.cloud.app.api.Application(
+        "owner",
+        1234567,
+        123456789,
+        normAppDesc,
+        tool
+    )
+
+    @Test
+    fun `find By name test `() {
+        withDatabase { db ->
+            withAuthMock {
+                withTestApplication(
+                    moduleFunction = {
+                        val toolDao = mockk<ToolHibernateDAO>()
+                        configureToolServer(db, toolDao)
+
+                        every { toolDao.findAllByName(any(), any(), any(), any()) } answers {
+                            Page(1, 10, 0, listOf(tool))
+
+                        }
+                    },
+
+                    test = {
+                        val response =
+                            handleRequest(HttpMethod.Get, "/api/hpc/tools/name?itemsPerPage=10&page=0") {
+                                addHeader("Job-Id", UUID.randomUUID().toString())
+                                setUser()
+                            }.response
+
+                        assertEquals(HttpStatusCode.OK, response.status())
+
+                        val obj = mapper.readTree(response.content)
+                        assertEquals(1, obj["itemsInTotal"].asInt())
+                    }
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `find By name and version test `() {
+        withDatabase { db ->
+            withAuthMock {
+                withTestApplication(
+                    moduleFunction = {
+                        val toolDao = mockk<ToolHibernateDAO>()
+                        configureToolServer(db, toolDao)
+
+                        every { toolDao.findByNameAndVersion(any(), any(), any(), any()) } returns tool
+                    },
+
+                    test = {
+                        val response =
+                            handleRequest(HttpMethod.Get, "/api/hpc/tools/name/2.2") {
+                                addHeader("Job-Id", UUID.randomUUID().toString())
+                                setUser()
+                            }.response
+
+                        assertEquals(HttpStatusCode.OK, response.status())
+
+                        val obj = mapper.readTree(response.content)
+
+                        assertEquals("\"owner\"", obj["owner"].toString())
+                    }
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `list all test `() {
+        withDatabase { db ->
+            withAuthMock {
+                withTestApplication(
+                    moduleFunction = {
+                        val toolDao = mockk<ToolHibernateDAO>()
+                        configureToolServer(db, toolDao)
+
+                        every { toolDao.listLatestVersion(any(), any(), any()) } answers {
+                            Page(1, 10, 0, listOf(tool))
+                        }
+                    },
+
+                    test = {
+                        val response =
+                            handleRequest(HttpMethod.Get, "/api/hpc/tools") {
+                                addHeader("Job-Id", UUID.randomUUID().toString())
+                                setUser()
+                            }.response
+
+                        assertEquals(HttpStatusCode.OK, response.status())
+
+                        val obj = mapper.readTree(response.content)
+                        assertEquals(1, obj["itemsInTotal"].asInt())
+                        assertTrue(obj["items"].toString().contains("\"owner\":\"owner\""))
+                    }
+                )
+            }
+        }
+    }
+
+    //TODO NEED SUPPORT FOR YAML CANT TEST
+    @Test
+    fun `Create test - Cant read YAML`() {
+        withDatabase { db ->
+            withAuthMock {
+                withTestApplication(
+                    moduleFunction = {
+                        val toolDao = mockk<ToolHibernateDAO>()
+                        configureToolServer(db, toolDao)
+
+                    },
+
+                    test = {
+                        val response =
+                            handleRequest(HttpMethod.Put, "/api/hpc/tools") {
+                                addHeader("Job-Id", UUID.randomUUID().toString())
+                                setUser(role = Role.ADMIN)
+                            }.response
+
+                        assertEquals(HttpStatusCode.BadRequest, response.status())
+
+                    }
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Create test - not priviliged `() {
+        withDatabase { db ->
+            withAuthMock {
+                withTestApplication(
+                    moduleFunction = {
+                        val toolDao = mockk<ToolHibernateDAO>()
+                        configureToolServer(db, toolDao)
+
+                    },
+
+                    test = {
+                        val response =
+                            handleRequest(HttpMethod.Put, "/api/hpc/tools") {
+                                addHeader("Job-Id", UUID.randomUUID().toString())
+                                setUser()
+                            }.response
+
+                        assertEquals(HttpStatusCode.Unauthorized, response.status())
+
+                    }
+                )
+            }
+        }
+    }
+
+}
