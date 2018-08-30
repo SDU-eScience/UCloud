12b8a87738aa0a88a34af6105432f2d81d3c2ecb
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-08-30 15:19:46 +0200

    Test component with nested Link tag

--- frontend-web/webclient/__tests__/Files.test.tsx
+++ frontend-web/webclient/__tests__/Files.test.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import * as Renderer from "react-test-renderer";
-import { emptyPage } from "DefaultObjects"; 
+import { emptyPage } from "DefaultObjects";
 import { FilesTable } from "Files/Files";
 import { File, SortOrder, SortBy } from "Files";
+import { mockFiles_SensitivityConfidential } from "./mock/Files"
+import { MemoryRouter } from "react-router-dom";
 
 test("Render empty files table", () => {
-    const filestable = Renderer.create(
+    expect(Renderer.create(
         <FilesTable
             files={emptyPage.items as File[]}
             fileOperations={[]}
@@ -17,6 +19,23 @@ test("Render empty files table", () => {
             sortBy={SortBy.PATH}
             onFavoriteFile={() => null}
         />
-    ).toJSON();
-    expect(filestable).toMatchSnapshot();
+    ).toJSON()).toMatchSnapshot();
+});
+
+test("Files in filestable", () => {
+    expect(Renderer.create(
+        <MemoryRouter>
+            <FilesTable
+                files={mockFiles_SensitivityConfidential.items}
+                fileOperations={[]}
+                sortOrder={SortOrder.ASCENDING}
+                sortingColumns={[SortBy.PATH, SortBy.MODIFIED_AT]}
+                sortFiles={() => null}
+                onCheckFile={() => null}
+                refetchFiles={() => null}
+                sortBy={SortBy.PATH}
+                onFavoriteFile={() => null}
+            />
+        </MemoryRouter>
+    ).toJSON()).toMatchSnapshot()
 });
