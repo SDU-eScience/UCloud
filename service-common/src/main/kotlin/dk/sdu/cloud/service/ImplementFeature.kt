dc327a9898c1e03f0510e79d6ff7c9cf33fb3ee4
Dan Sebastian Thrane <dthrane@gmail.com>
2018-09-14 11:32:01 +0200

    Resolves #291

--- service-common/src/main/kotlin/dk/sdu/cloud/service/ImplementFeature.kt
+++ service-common/src/main/kotlin/dk/sdu/cloud/service/ImplementFeature.kt
@@ -11,10 +11,12 @@ import dk.sdu.cloud.CommonErrorMessage
 import dk.sdu.cloud.client.*
 import io.ktor.application.*
 import io.ktor.features.conversionService
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.pipeline.PipelineContext
 import io.ktor.request.receiveOrNull
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.application
 import io.ktor.routing.method
@@ -329,7 +331,11 @@ class RESTHandler<P : Any, S : Any, E : Any, A : Any>(
         finalize(result)
 
         if (result == Unit) call.respond(status)
-        else call.respond(status, result)
+        else {
+            call.respondText(contentType = ContentType.Application.Json, status = status) {
+               defaultMapper.writerFor(restCall.responseTypeSuccess).writeValueAsString(result)
+            }
+        }
     }
 
     suspend fun error(error: E, status: HttpStatusCode) {
---
+++ service-common/src/test/kotlin/dk/sdu/cloud/service/ImplementGenericTest.kt
@@ -0,0 +1,104 @@
+package dk.sdu.cloud.service
+
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import dk.sdu.cloud.AccessRight
+import dk.sdu.cloud.Roles
+import dk.sdu.cloud.client.RESTDescriptions
+import dk.sdu.cloud.client.defaultMapper
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.routing.Route
+import io.ktor.routing.routing
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+private const val PROP_TYPE = "type"
+
+// Covers issue #291
+class ImplementGenericTest {
+    @JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = PROP_TYPE
+    )
+    @JsonSubTypes(
+        JsonSubTypes.Type(SealedType.A::class, name = "a"),
+        JsonSubTypes.Type(SealedType.B::class, name = "b")
+    )
+    sealed class SealedType {
+        data class A(val a: String) : SealedType()
+        data class B(val b: Int) : SealedType()
+    }
+
+    object GenericDescriptions : RESTDescriptions("generic") {
+        val call = callDescription<Unit, Page<SealedType>, Unit> {
+            name = "call"
+            auth {
+                roles = Roles.PUBLIC
+                access = AccessRight.READ
+            }
+
+            path {
+                using("/")
+            }
+        }
+    }
+
+
+    class GenericController : Controller {
+        override val baseContext: String = "/"
+
+        override fun configure(routing: Route): Unit = with(routing) {
+            implement(GenericDescriptions.call) {
+                ok(
+                    listOf(
+                        SealedType.A("a"),
+                        SealedType.B(42)
+                    ).paginate(PaginationRequest().normalize())
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `test that generic type erasure does not occur`() {
+        withTestApplication(
+            moduleFunction = {
+                routing {
+                    installDefaultFeatures(
+                        mockk(relaxed = true),
+                        mockk(relaxed = true),
+                        mockk(relaxed = true),
+                        requireJobId = false
+                    )
+
+                    configureControllers(GenericController())
+                }
+            },
+
+            test = {
+                val response = handleRequest(HttpMethod.Get, "/")
+                assertTrue(response.requestHandled)
+                assertTrue(
+                    ContentType.parse(response.response.headers[HttpHeaders.ContentType]!!).match(
+                        ContentType.Application.Json
+                    )
+                )
+
+                val tree = defaultMapper.readTree(response.response.content)?.takeIf { !it.isNull && !it.isMissingNode }
+                tree!!
+
+                val items = tree["items"]?.takeIf { !it.isNull && !it.isMissingNode }!!
+                assertEquals(2, items.size())
+                assertEquals("a", items[0][PROP_TYPE].asText())
+                assertEquals("b", items[1][PROP_TYPE].asText())
+            }
+        )
+    }
+}
