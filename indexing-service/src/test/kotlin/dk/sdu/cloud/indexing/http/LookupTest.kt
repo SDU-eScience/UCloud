48a5013275abb51f74c0ac14e9ae2ebda5ae2ed4
Henrik Schulz <henrikschulz@hotmail.com>
2018-09-21 13:40:50 +0200

    Added tests for Lookup controler

---
+++ indexing-service/src/test/kotlin/dk/sdu/cloud/indexing/http/LookupTest.kt
@@ -0,0 +1,76 @@
+package dk.sdu.cloud.indexing.http
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import dk.sdu.cloud.Role
+import dk.sdu.cloud.indexing.services.IndexQueryService
+import dk.sdu.cloud.indexing.services.ReverseLookupService
+import dk.sdu.cloud.indexing.utils.withAuthMock
+import dk.sdu.cloud.service.Controller
+import dk.sdu.cloud.service.Page
+import dk.sdu.cloud.service.configureControllers
+import dk.sdu.cloud.service.installDefaultFeatures
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.routing.routing
+import io.ktor.server.testing.TestApplicationRequest
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import java.util.*
+import kotlin.test.assertEquals
+
+private fun TestApplicationRequest.setUser(username: String = "user", role: Role = Role.USER) {
+    addHeader(io.ktor.http.HttpHeaders.Authorization, "Bearer $username/$role")
+}
+
+private fun Application.configureBaseServer(vararg controllers: Controller) {
+    installDefaultFeatures(
+        mockk(relaxed = true),
+        mockk(relaxed = true),
+        mockk(relaxed = true),
+        requireJobId = true
+    )
+
+    routing {
+        configureControllers(*controllers)
+    }
+}
+
+private fun Application.configureLookupServer(reverseLookupService: ReverseLookupService) {
+    configureBaseServer(LookupController(reverseLookupService))
+}
+
+class LookupTest{
+
+    private val mapper = jacksonObjectMapper()
+
+    @Test
+    fun `testing something`() {
+        withAuthMock {
+            withTestApplication (
+                moduleFunction = {
+                    val reverseLookupService = mockk<ReverseLookupService>()
+                    every { reverseLookupService.reverseLookup(any()) } returns "This is what I found"
+                    every { reverseLookupService.reverseLookupBatch(any()) } returns listOf("This is what I found")
+                    configureLookupServer(reverseLookupService)
+                },
+
+                test = {
+                    val response =
+                        handleRequest(HttpMethod.Get, "/api/file-search/lookup/reverse?fileId=1") {
+                            addHeader("Job-Id", UUID.randomUUID().toString())
+                            setUser(role = Role.ADMIN)
+                        }.response
+
+                    assertEquals(HttpStatusCode.OK, response.status())
+                    val obj = mapper.readTree(response.content)
+                    assertEquals("This is what I found", obj["canonicalPath"].first().textValue())
+                }
+
+            )
+        }
+    }
+}
