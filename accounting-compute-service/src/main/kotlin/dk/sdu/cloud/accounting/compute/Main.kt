f8aa262ec933400c0d5c89a5207af6ff0e3472c2
Dan Sebastian Thrane <dthrane@gmail.com>
2018-10-02 08:04:49 +0200

    Calls initWithDefaultFeatures

--- accounting-compute-service/src/main/kotlin/dk/sdu/cloud/accounting/compute/Main.kt
+++ accounting-compute-service/src/main/kotlin/dk/sdu/cloud/accounting/compute/Main.kt
@@ -1,12 +1,13 @@
 package dk.sdu.cloud.accounting.compute
 
+import dk.sdu.cloud.accounting.compute.api.AccountingComputeServiceDescription
 import dk.sdu.cloud.auth.api.RefreshingJWTCloudFeature
 import dk.sdu.cloud.auth.api.refreshingJwtCloud
 import dk.sdu.cloud.service.*
 
 fun main(args: Array<String>) {
     val micro = Micro().apply {
-        //initWithDefaultFeatures() TODO BAD NAME IN GENERATED DESCRIPTION FILE
+        initWithDefaultFeatures(AccountingComputeServiceDescription, args)
         install(HibernateFeature)
         install(RefreshingJWTCloudFeature)
     }
