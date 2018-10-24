1f0cd6c2de48c6d32be04d1f10fab83df61695ba
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-10-24 10:59:56 +0200

    ClickableDropdown file

---
+++ frontend-web/webclient/app/ui-components/ClickableDropdown.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Dropdown, DropdownContent } from "./Dropdown";
+
+type ClickableDropdownState = { open: boolean }
+type ClickableDropdownProps = { children: React.ReactNode, trigger: React.ReactNode, width?: string }
+
+class ClickableDropdown extends React.Component<ClickableDropdownProps, ClickableDropdownState> {
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
+        document.addEventListener("click", this.handleClickOutside, true);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside, true);
+    }
+
+    // https://stackoverflow.com/questions/32553158/detect-click-outside-react-component#42234988
+    handleClickOutside = event => {
+        const domNode = ReactDOM.findDOMNode(this);
+        if (!domNode || !domNode.contains(event.target)) this.setState(() => ({ open: false }));
+    }
+
+    render() {
+        return (
+            <Dropdown>
+                <span onClick={() => this.setState(() => ({ open: true }))}>{this.props.trigger}</span>
+                {this.state.open ?
+                    <DropdownContent width={this.props.width} hover={false} onClick={() => this.setState(() => ({ open: false }))}>
+                        {this.props.children}
+                    </DropdownContent> : null}
+            </Dropdown>
+        )
+    }
+}
+
+export default ClickableDropdown;
