1e0ae04f602bb93b30af8fdc841b04c5fa0dac74
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-10-24 10:48:48 +0200

    Proprietary clickable dropdown. Remove Header depdendency for Files

--- frontend-web/webclient/app/ui-components/Banner.tsx
+++ frontend-web/webclient/app/ui-components/Banner.tsx
@@ -3,7 +3,7 @@ import Box from "./Box"
 import Flex from "./Flex"
 import Text, { TextSpan } from "./Text"
 import Icon, { IconName } from "./Icon"
-import Heading from "./Heading"
+import * as Heading from "./Heading"
 import CloseButton from "./CloseButton"
 import { TextAlign } from "./Types";
 import { BoxProps } from "./Box";
