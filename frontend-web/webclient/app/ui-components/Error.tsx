bc432f8668ef156ffb80942ee886794f879bde5b
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-11-15 14:23:57 +0100

    Fixes #385

--- frontend-web/webclient/app/ui-components/Error.tsx
+++ frontend-web/webclient/app/ui-components/Error.tsx
@@ -4,10 +4,10 @@ import CloseButton from "./CloseButton";
 
 interface ErrorProps { clearError?: () => void, error?: string }
 const Error = (props: ErrorProps) => !!props.error ?
-    <Card borderRadius="0.5em" height="auto" color="black" bg="lightRed" borderColor="red">
-        <Flex ml="1em" mt="1em">
+    <Card borderRadius="0.5em" height="auto" p="1em 1em 1em 1em" color="black" bg="lightRed" borderColor="red">
+        <Flex>
             <Text fontSize={2} color="red">{props.error}</Text>
-            {props.clearError ? <Box ml="auto"><CloseButton ml="-40px" onClick={props.clearError} /></Box> : null}
+            {props.clearError ? <Box ml="auto"><CloseButton color="black" onClick={props.clearError} /></Box> : null}
         </Flex>
     </Card> : null;
 
