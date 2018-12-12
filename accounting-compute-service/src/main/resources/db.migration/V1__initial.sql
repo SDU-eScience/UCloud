88bd379f601398e60fc3b90f324d7f11cab00539
Henrik Schulz <henrikschulz@hotmail.com>
2018-12-12 13:16:34 +0100

    added Inital DB

---
+++ accounting-compute-service/src/main/resources/db.migration/V1__initial.sql
@@ -0,0 +1,12 @@
+set search_path to accounting_compute;
+
+create table accounting_compute.job_completed_events (
+   job_id varchar(255) not null,
+    application_name varchar(255),
+    application_version varchar(255),
+    duration_in_ms int8 not null,
+    nodes int4 not null,
+    started_by varchar(255),
+    timestamp timestamp,
+    primary key (job_id)
+);
