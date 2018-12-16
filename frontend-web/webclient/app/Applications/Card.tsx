11e13b0b6af13a55822e0d560cacc9a37521e62b
Claudio Pica <pica@cp3.sdu.dk>
2018-12-16 11:52:01 +0100

    test with colors in app card

--- frontend-web/webclient/app/Applications/Card.tsx
+++ frontend-web/webclient/app/Applications/Card.tsx
@@ -140,6 +140,45 @@ const Tag = ({ label }: { label: string }) => (
     <RatingBadge mr={"3px"} bg={"darkGray"}><Heading.h6>{label}</Heading.h6></RatingBadge>
 )
 
+// function hslToHex(h, s, l) {
+//     h /= 360;
+//     s /= 100;
+//     l /= 100;
+//     let r, g, b;
+//     if (s === 0) {
+//         r = g = b = l; // achromatic
+//     } else {
+//         const hue2rgb = (p, q, t) => {
+//             if (t < 0) t += 1;
+//             if (t > 1) t -= 1;
+//             if (t < 1 / 6) return p + (q - p) * 6 * t;
+//             if (t < 1 / 2) return q;
+//             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+//             return p;
+//         };
+//         const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+//         const p = 2 * l - q;
+//         r = hue2rgb(p, q, h + 1 / 3);
+//         g = hue2rgb(p, q, h);
+//         b = hue2rgb(p, q, h - 1 / 3);
+//     }
+//     const toHex = x => {
+//         const hex = Math.round(x * 255).toString(16);
+//         return hex.length === 1 ? '0' + hex : hex;
+//     };
+//     return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+// }
+
+// const hues = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]; 
+// //const hues = [...Array(12).keys()].map( x=> (360/12)*x);
+
+// const bgGradients2 = hues.map( x=> ( 
+//     {
+//         color1: hslToHex(x,60,60),
+//         color2: hslToHex(x,60,40)
+//     }
+// ));
+
 const bgGradients = [
     //["#0096ff", "#043eff"], // blue
     ["#F7D06A", "#C46927"], // golden
@@ -222,6 +261,7 @@ let i=0;
 export const NewApplicationCard: React.FunctionComponent<ApplicationCardProps> = ({ app, onFavorite, isFavorite, linkToRun }: ApplicationCardProps) => {
     const appDesc = app.description;
     const hash=hashF(appDesc.title);
+    console.log(bgGradients);
     return (
         <NewAppCard to={linkToRun ? Pages.runApplication(app) : Pages.viewApplication(app)}>
             <Absolute right={0} top={0}>
