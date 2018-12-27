1488fdccacbdde313e7aaec001c9a6e0f1f3c664
Claudio Pica <pica@cp3.sdu.dk>
2018-12-27 18:19:42 +0100

    removed "withComponents" in Link

--- frontend-web/webclient/app/ui-components/Link.tsx
+++ frontend-web/webclient/app/ui-components/Link.tsx
@@ -1,11 +1,10 @@
+import * as React from 'react';
 import { Link as ReactRouterLink } from "react-router-dom";
 import BaseLink from "./BaseLink";
 
-const Link = BaseLink.withComponent(ReactRouterLink);
-Link.defaultProps = {
-  color: "text",
-  hoverColor: "textHighlight"
-};
+// const Link = BaseLink.withComponent(ReactRouterLink);
+
+const Link = (props) => <BaseLink as={ReactRouterLink} {...props} />;
 
 
 export default Link;
