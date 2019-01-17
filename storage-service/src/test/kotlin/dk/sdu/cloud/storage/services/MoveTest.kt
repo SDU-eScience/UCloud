98e85fdc361b720d035066c3375bdfeb685f275f
Dan Sebastian Thrane <dthrane@gmail.com>
2019-01-17 08:16:26 +0100

    Fixes #501

--- storage-service/src/test/kotlin/dk/sdu/cloud/storage/services/MoveTest.kt
+++ storage-service/src/test/kotlin/dk/sdu/cloud/storage/services/MoveTest.kt
@@ -2,11 +2,13 @@ package dk.sdu.cloud.storage.services
 
 import dk.sdu.cloud.file.api.WriteConflictPolicy
 import dk.sdu.cloud.file.api.joinPath
+import dk.sdu.cloud.file.services.BackgroundScope
 import dk.sdu.cloud.file.services.CoreFileSystemService
 import dk.sdu.cloud.file.services.unixfs.UnixFSCommandRunner
 import dk.sdu.cloud.file.services.unixfs.UnixFSCommandRunnerFactory
 import dk.sdu.cloud.file.services.withBlockingContext
 import dk.sdu.cloud.file.util.FSException
+import dk.sdu.cloud.service.test.assertThatPropertyEquals
 import dk.sdu.cloud.storage.util.createDummyFS
 import dk.sdu.cloud.storage.util.mkdir
 import dk.sdu.cloud.storage.util.touch
@@ -30,12 +32,17 @@ class MoveTest {
     )
 
     private fun runTest(user: String = "user1", consumer: suspend TestContext.() -> Unit) {
-        val fsRoot = createDummyFS()
-        val (runner, service) = createService(fsRoot.absolutePath)
+        BackgroundScope.reset()
+        try {
+            val fsRoot = createDummyFS()
+            val (runner, service) = createService(fsRoot.absolutePath)
 
-        runner.withBlockingContext(user) {
-            val testContext = TestContext(fsRoot, runner, service, it)
-            testContext.consumer()
+            runner.withBlockingContext(user) {
+                val testContext = TestContext(fsRoot, runner, service, it)
+                testContext.consumer()
+            }
+        } finally {
+            BackgroundScope.stop()
         }
     }
 
@@ -113,6 +120,119 @@ class MoveTest {
 
                 mkdir(wrapper) {
                     touch(conflictFileName)
+                }
+            }
+
+            service.move(
+                ctx,
+                joinPath(userRootPath, conflictFileName),
+                joinPath(userRootPath, wrapper, conflictFileName),
+                WriteConflictPolicy.OVERWRITE
+            )
+        }
+    }
+
+    @Test
+    fun `test moving a non empty directory into an empty directory`() {
+        runTest {
+            val userRootPath = "/home/user1"
+            val conflictFileName = "conflict"
+            val wrapper = "wrapper"
+            val source: File
+            val destination: File
+
+            fsRoot.resolve(".$userRootPath").apply {
+                source = mkdir(conflictFileName) {
+                    touch("a")
+                }
+
+                mkdir(wrapper) {
+                    destination = mkdir(conflictFileName) {}
+                }
+            }
+
+            service.move(
+                ctx,
+                joinPath(userRootPath, conflictFileName),
+                joinPath(userRootPath, wrapper, conflictFileName),
+                WriteConflictPolicy.OVERWRITE
+            )
+
+            assertThatPropertyEquals(destination.list(), { it.size }, 1)
+            assertThatPropertyEquals(source, { it.exists() }, false)
+        }
+    }
+
+    @Test
+    fun `moving file to non-empty directory`() {
+        runTest {
+            val userRootPath = "/home/user1"
+            val conflictFileName = "conflict"
+            val wrapper = "wrapper"
+            val source: File
+            val destination: File
+
+            fsRoot.resolve(".$userRootPath").apply {
+                source = touch(conflictFileName)
+
+                mkdir(wrapper) {
+                    destination = mkdir(conflictFileName) {
+                        touch("a")
+                    }
+                }
+            }
+
+            service.move(
+                ctx,
+                joinPath(userRootPath, conflictFileName),
+                joinPath(userRootPath, wrapper, conflictFileName),
+                WriteConflictPolicy.OVERWRITE
+            )
+        }
+    }
+
+    @Test(expected = FSException.BadRequest::class)
+    fun `test moving empty directory into a non-empty directory`() {
+        runTest {
+            val userRootPath = "/home/user1"
+            val conflictFileName = "conflict"
+            val wrapper = "wrapper"
+
+            fsRoot.resolve(".$userRootPath").apply {
+                mkdir(conflictFileName) {}
+
+                mkdir(wrapper) {
+                    mkdir(conflictFileName) {
+                        touch("a")
+                    }
+                }
+            }
+
+            service.move(
+                ctx,
+                joinPath(userRootPath, conflictFileName),
+                joinPath(userRootPath, wrapper, conflictFileName),
+                WriteConflictPolicy.OVERWRITE
+            )
+        }
+    }
+
+    @Test(expected = FSException.BadRequest::class)
+    fun `test moving non-empty directory into a non-empty directory`() {
+        runTest {
+            val userRootPath = "/home/user1"
+            val conflictFileName = "conflict"
+            val wrapper = "wrapper"
+
+            fsRoot.resolve(".$userRootPath").apply {
+                mkdir(conflictFileName) {
+                    touch("a")
+                }
+
+                mkdir(wrapper) {
+                    mkdir(conflictFileName) {
+                        touch("b")
+                    }
                 }
             }
 
