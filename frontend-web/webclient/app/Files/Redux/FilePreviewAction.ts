7b231f94ec2b836edc9c6cb2fcaf66a6cb523c44
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-01-24 10:48:05 +0100

    Add file preview to store

---
+++ frontend-web/webclient/app/Files/Redux/FilePreviewAction.ts
@@ -0,0 +1,18 @@
+import { FILE_PREVIEW_RECEIVE_FILE, FILE_PREVIEW_SET_ERROR } from "./FilePreviewReducer";
+import { Cloud } from "Authentication/SDUCloudObject";
+import { statFileQuery } from "Utilities/FileUtilities";
+
+export const fetchPreviewFile = (path: string) =>
+    Cloud.get<File>(statFileQuery(path))
+        .then(({ response }) => receiveFile(response))
+        .catch(({ request }: { request: XMLHttpRequest }) => setFilePreviewError(request.statusText));
+
+export const setFilePreviewError = (error?: string) => ({
+    type: FILE_PREVIEW_SET_ERROR,
+    payload: { error }
+});
+
+const receiveFile = (file: File) => ({
+    type: FILE_PREVIEW_RECEIVE_FILE,
+    payload: { file }
+})
