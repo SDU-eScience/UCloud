c625dd427397eab761daaec206f20b094ea920c1
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-02-07 13:46:54 +0100

    Make AvatarModification use Hooks

--- frontend-web/webclient/app/UserSettings/Avataaar.tsx
+++ frontend-web/webclient/app/UserSettings/Avataaar.tsx
@@ -41,161 +41,147 @@ interface AvataaarModificationOperations {
     setActivePage: () => void
 }
 
-type AvataaarModificationProps = AvataaarModificationStateProps & AvataaarModificationOperations;
-
-class AvataaarModification extends React.Component<AvataaarModificationProps, AvataaarModificationState> {
-    constructor(props: Readonly<AvataaarModificationProps>) {
-        super(props);
-        this.state = {
-            ...defaultAvatar,
-            loading: true,
-            promises: new PromiseKeeper()
-        };
-        this.fetchCurrentAvatar();
-        props.setActivePage()
-    }
-
-    private fetchCurrentAvatar = () =>
-        this.state.promises.makeCancelable(Cloud.get(findAvatarQuery)).promise
-            .then(it => this.setState(() => ({ ...it.response, loading: false })))
-            .catch(it => (failureNotification("An error occurred fetching current Avatar"), this.setState(() => ({ loading: false }))))
-
-    private save() {
-        const { loading, promises, ...state } = this.state;
-        this.props.save(state);
-    }
-
-    componentWillUnmount() {
-        this.state.promises.cancelPromises();
-    }
-
-    render() {
-        const { ...state } = this.state;
-        return (
-            <MainContainer
-                headerSize={220}
-                header={<>
-                    <Flex>
-                        <Box ml="auto" />
-                        <Avataaar
-                            style={{ height: "150px" }}
-                            avatarStyle="circle"
-                            topType={state.top}
-                            accessoriesType={state.topAccessory}
-                            hairColor={state.hairColor}
-                            facialHairType={state.facialHair}
-                            facialHairColor={state.facialHairColor}
-                            clotheType={state.clothes}
-                            clotheColor={state.colorFabric}
-                            graphicType={state.clothesGraphic}
-                            eyeType={state.eyes}
-                            eyebrowType={state.eyebrows}
-                            mouthType={state.mouthTypes}
-                            skinColor={state.skinColors}
-                        />
-                        <Box mr="auto" />
-                    </Flex>
-                    <Flex>
-                        <OutlineButton
-                            ml="auto"
-                            mr="auto"
-                            onClick={() => this.save()}
-                            mt="5px"
-                            mb="5px"
-                            color="blue"
-                        >Update avatar</OutlineButton>
-                    </Flex></>}
-
-                main={
-                    this.state.loading ? (<Spinner size={24} />) : <>
-                        <AvatarSelect
-                            defaultValue={state.top}
-                            update={value => this.setState(() => ({ top: value }))}
-                            options={Options.Top}
-                            title="Top"
-                            disabled={false}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.topAccessory}
-                            update={value => this.setState(() => ({ topAccessory: value }))}
-                            options={Options.TopAccessory}
-                            title="Accessories"
-                            disabled={state.top === "Eyepatch"}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.hairColor}
-                            update={value => this.setState(() => ({ hairColor: value }))}
-                            options={Options.HairColor}
-                            title="Hair color"
-                            disabled={!state.top.includes("Hair") || state.top === "LongHairFrida"}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.facialHair}
-                            update={value => this.setState(() => ({ facialHair: value }))}
-                            options={Options.FacialHair}
-                            title="Facial Hair"
-                            disabled={state.top === "Hijab"}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.facialHairColor}
-                            update={value => this.setState(() => ({ facialHairColor: value }))}
-                            options={Options.FacialHairColor}
-                            title="Facial Hair Color"
-                            disabled={state.facialHair === "Blank"}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.clothes}
-                            update={value => this.setState(() => ({ clothes: value }))}
-                            options={Options.Clothes}
-                            title="Clothes"
-                            disabled={false}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.colorFabric}
-                            title="Clothes Fabric"
-                            options={Options.ColorFabric}
-                            update={value => this.setState(() => ({ colorFabric: value }))}
-                            disabled={state.clothes === "BlazerShirt" || state.clothes === "BlazerSweater"}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.clothesGraphic}
-                            title="Graphic"
-                            update={value => this.setState(() => ({ clothesGraphic: value }))}
-                            options={Options.ClothesGraphic}
-                            disabled={state.clothes !== "GraphicShirt"}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.eyes}
-                            title="Eyes"
-                            options={Options.Eyes}
-                            update={value => this.setState(() => ({ eyes: value }))}
-                            disabled={false}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.eyebrows}
-                            title="Eyebrow"
-                            options={Options.Eyebrows}
-                            update={value => this.setState(() => ({ eyebrows: value }))}
-                            disabled={false}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.mouthTypes}
-                            title="Mouth type"
-                            options={Options.MouthTypes}
-                            update={value => this.setState(() => ({ mouthTypes: value }))}
-                            disabled={false}
-                        />
-                        <AvatarSelect
-                            defaultValue={state.skinColors}
-                            title={"Skin color"}
-                            options={Options.SkinColors}
-                            update={value => this.setState(() => ({ skinColors: value }))}
-                            disabled={false}
-                        />
-                    </>
+function Modification(props: AvataaarModificationOperations) {
+    const [state, setState] = React.useState({ ...defaultAvatar, loading: true })
+    React.useEffect(() => {
+        const promises = new PromiseKeeper();
+        promises.makeCancelable(Cloud.get<AvatarType>(findAvatarQuery)).promise
+            .then(it => setState({ ...it.response, loading: false }))
+            .catch(it => {
+                if (!it.isCanceled) {
+                    failureNotification("An error occurred fetching current Avatar");
+                    setState({ ...state, loading: false });
                 }
-            />)
-    }
+            });
+        return () => promises.cancelPromises();
+    }, []);
+
+    const { loading, ...avatar } = state;
+
+    return (
+        <MainContainer
+            headerSize={220}
+            header={<>
+                <Flex>
+                    <Box ml="auto" />
+                    <Avataaar
+                        style={{ height: "150px" }}
+                        avatarStyle="circle"
+                        topType={avatar.top}
+                        accessoriesType={avatar.topAccessory}
+                        hairColor={avatar.hairColor}
+                        facialHairType={avatar.facialHair}
+                        facialHairColor={avatar.facialHairColor}
+                        clotheType={avatar.clothes}
+                        clotheColor={avatar.colorFabric}
+                        graphicType={avatar.clothesGraphic}
+                        eyeType={avatar.eyes}
+                        eyebrowType={avatar.eyebrows}
+                        mouthType={avatar.mouthTypes}
+                        skinColor={avatar.skinColors}
+                    />
+                    <Box mr="auto" />
+                </Flex>
+                <Flex>
+                    <OutlineButton
+                        ml="auto"
+                        mr="auto"
+                        onClick={() => props.save(avatar)}
+                        mt="5px"
+                        mb="5px"
+                        color="blue"
+                    >Update avatar</OutlineButton>
+                </Flex></>}
+
+            main={
+                loading ? (<Spinner size={24} />) : <>
+                    <AvatarSelect
+                        defaultValue={avatar.top}
+                        update={value => setState({ loading, ...avatar, top: value })}
+                        options={Options.Top}
+                        title="Top"
+                        disabled={false}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.topAccessory}
+                        update={value => setState({ loading, ...avatar, topAccessory: value })}
+                        options={Options.TopAccessory}
+                        title="Accessories"
+                        disabled={state.top === "Eyepatch"}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.hairColor}
+                        update={value => setState({ loading, ...avatar, hairColor: value })}
+                        options={Options.HairColor}
+                        title="Hair color"
+                        disabled={!state.top.includes("Hair") || state.top === "LongHairFrida"}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.facialHair}
+                        update={value => setState({ loading, ...avatar, facialHair: value })}
+                        options={Options.FacialHair}
+                        title="Facial Hair"
+                        disabled={state.top === "Hijab"}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.facialHairColor}
+                        update={value => setState({ loading, ...avatar, facialHairColor: value })}
+                        options={Options.FacialHairColor}
+                        title="Facial Hair Color"
+                        disabled={state.facialHair === "Blank"}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.clothes}
+                        update={value => setState({ loading, ...avatar, clothes: value })}
+                        options={Options.Clothes}
+                        title="Clothes"
+                        disabled={false}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.colorFabric}
+                        title="Clothes Fabric"
+                        options={Options.ColorFabric}
+                        update={value => setState({ loading, ...avatar, colorFabric: value })}
+                        disabled={state.clothes === "BlazerShirt" || state.clothes === "BlazerSweater"}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.clothesGraphic}
+                        title="Graphic"
+                        update={value => setState({ loading, ...avatar, clothesGraphic: value })}
+                        options={Options.ClothesGraphic}
+                        disabled={state.clothes !== "GraphicShirt"}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.eyes}
+                        title="Eyes"
+                        options={Options.Eyes}
+                        update={value => setState({ loading, ...avatar, eyes: value })}
+                        disabled={false}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.eyebrows}
+                        title="Eyebrow"
+                        options={Options.Eyebrows}
+                        update={value => setState({ loading, ...avatar, eyebrows: value })}
+                        disabled={false}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.mouthTypes}
+                        title="Mouth type"
+                        options={Options.MouthTypes}
+                        update={value => setState({ loading, ...avatar, mouthTypes: value })}
+                        disabled={false}
+                    />
+                    <AvatarSelect
+                        defaultValue={state.skinColors}
+                        title={"Skin color"}
+                        options={Options.SkinColors}
+                        update={value => setState({ loading, ...avatar, skinColors: value })}
+                        disabled={false}
+                    />
+                </>
+            }
+        />)
 }
 
 interface AvatarSelect<T1, T2> {
@@ -240,5 +226,5 @@ const defaultAvatar = ({
 });
 
 type AvatarType = typeof defaultAvatar;
-export default connect<AvataaarModificationStateProps, AvataaarModificationOperations>(mapStateToProps, mapDispatchToProps)(AvataaarModification);
+export default connect<AvataaarModificationStateProps, AvataaarModificationOperations>(mapStateToProps, mapDispatchToProps)(Modification);
 export { defaultAvatar, AvatarType }
