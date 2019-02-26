a0265b36ce83d85c19b43350c712ec24c45ddfae
Henrik Schulz <henrikschulz@hotmail.com>
2019-02-26 14:10:48 +0100

    minor change

--- alerting-service/src/main/kotlin/dk/sdu/cloud/alerting/services/ElasticAlerting.kt
+++ alerting-service/src/main/kotlin/dk/sdu/cloud/alerting/services/ElasticAlerting.kt
@@ -1,5 +1,6 @@
 package dk.sdu.cloud.alerting.services
 
+import kotlinx.coroutines.delay
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
@@ -20,15 +21,11 @@ class ElasticAlerting(
         val clusterResponse = elastic.cluster().health(ClusterHealthRequest(), RequestOptions.DEFAULT)
         println("errorRed: $errorRed, errorYellow: $errorYellow, red = $red, yellow = $yellow")
         if (clusterResponse.status.toString() == "RED") {
-            println("Checking red")
             errorRed = true
-            println(clusterResponse.status)
             return
         }
         if (clusterResponse.status.toString() == "YELLOW") {
-            println("Checking yellow")
             errorYellow = true
-            println(clusterResponse.status)
             return
         }
         errorYellow = false
@@ -37,35 +34,37 @@ class ElasticAlerting(
         red = 0
     }
 
-    fun start(time: Long) {
+    suspend fun start(time: Long) {
         while (true) {
             if (!errorRed && !errorYellow) {
                 checkClusterStatus()
-                Thread.sleep(time)
+                delay(time)
             }
             else {
                 if (errorRed) {
                     if (red < 2) {
                         red++
-                        Thread.sleep(time)
+                        delay(time)
                         checkClusterStatus()
                         continue
                     }
                     else {
                         println("SENDING SLACK")
-                        Thread.sleep(ONE_MINUTE)
+                        delay(ONE_MINUTE)
+                        checkClusterStatus()
                     }
                 }
                 if (errorYellow) {
                     if (yellow < 10) {
                         yellow++
-                        Thread.sleep(time)
+                        delay(time)
                         checkClusterStatus()
                         continue
                     }
                     else {
                         println("SENDING SLACK")
-                        Thread.sleep(ONE_MINUTE)
+                        delay(ONE_MINUTE)
+                        checkClusterStatus()
                     }
                 }
             }
