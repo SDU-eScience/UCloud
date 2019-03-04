eea64f83b0a896a3968990efa592d5bf740dab53
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-03-04 09:27:23 +0100

    #546, files corrected

--- frontend-web/webclient/app/Files/FilesTable.tsx
+++ frontend-web/webclient/app/Files/FilesTable.tsx
@@ -194,6 +194,8 @@ export const ContextBar = ({ files, ...props }: ContextBarProps) => (
 const SidebarContent = styled.div`
     grid: auto-flow;
     & > * {
+        min-width: 75px;
+        max-width: 175px;
         margin-left: 5px;
         margin-right: 5px;
     }
@@ -203,10 +205,10 @@ export const ContextButtons = ({ createFolder, showUploader, inTrashFolder, toHo
     <VerticalButtonGroup>
         {!inTrashFolder ?
             <SidebarContent>
-                <Button color="blue" style={{ maxWidth: "201px" }} onClick={showUploader} data-tag="uploadButton">Upload Files</Button>
-                <OutlineButton color="blue" style={{ maxWidth: "201px" }} onClick={createFolder} data-tag="newFolder">New folder</OutlineButton>
+                <Button color="blue" onClick={showUploader} data-tag="uploadButton">Upload Files</Button>
+                <OutlineButton color="blue" onClick={createFolder} data-tag="newFolder">New folder</OutlineButton>
             </SidebarContent> :
-            <Button color="red" style={{ maxWidth: "201px" }} onClick={() => clearTrash(Cloud, () => toHome())}>
+            <Button color="red" onClick={() => clearTrash(Cloud, () => toHome())}>
                 Empty trash
             </Button>}
     </VerticalButtonGroup>
