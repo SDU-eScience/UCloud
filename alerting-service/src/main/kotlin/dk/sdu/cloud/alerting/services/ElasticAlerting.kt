994c6ca121f9e37c4192f09401a431939471c354
Henrik Schulz <henrikschulz@hotmail.com>
2019-03-07 15:11:43 +0100

    fixed requested changes. Set 5xx fail limit percentage to be 10%

--- alerting-service/src/main/kotlin/dk/sdu/cloud/alerting/services/ElasticAlerting.kt
+++ alerting-service/src/main/kotlin/dk/sdu/cloud/alerting/services/ElasticAlerting.kt
@@ -1,9 +1,8 @@
 package dk.sdu.cloud.alerting.services
 
+import dk.sdu.cloud.service.Loggable
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest
-import org.elasticsearch.action.admin.cluster.settings.ClusterGetSettingsRequest
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
@@ -12,86 +11,72 @@ import org.elasticsearch.search.builder.SearchSourceBuilder
 import java.time.LocalDate
 import java.util.*
 
-private const val FIFTEEN_SEC = 15*1000L
-private const val THIRTY_SEC = 30*1000L
-private const val FIVE_MIN = 5*60*1000L
-private const val FIFTEEN_MIN = 15*60*1000L
-private const val LIMIT_5XX_PERCENTAGE = 1
+private const val FIFTEEN_SEC = 15 * 1000L
+private const val THIRTY_SEC = 30 * 1000L
+private const val FIVE_MIN = 5 * 60 * 1000L
+private const val FIFTEEN_MIN = 15 * 60 * 1000L
+private const val LIMIT_5XX_PERCENTAGE = 10
+
+enum class Status(val isError: Boolean, val failuresForATrigger: Int) {
+    RED(isError = true, failuresForATrigger = 2),
+    YELLOW(isError = true, failuresForATrigger = 10),
+    GREEN(isError = false, failuresForATrigger = 0)
+}
 
 class ElasticAlerting(
     private val elastic: RestHighLevelClient,
     private val alertService: AlertingService
 ) {
-
-    private var errorYellow: Boolean = false
-    private var errorRed: Boolean = false
-    private var yellow: Int = 0
-    private var red: Int = 0
+    private var status: Status = Status.GREEN
+    private var errorCount: Int = 0
     private var alertSent = false
 
-    private fun checkClusterStatus() {
-        val clusterResponse = elastic.cluster().health(ClusterHealthRequest(), RequestOptions.DEFAULT)
-        println("errorRed: $errorRed, errorYellow: $errorYellow, red = $red, yellow = $yellow")
-        if (clusterResponse.status.toString() == "RED") {
-            errorRed = true
-            return
-        }
-        if (clusterResponse.status.toString() == "YELLOW") {
-            errorYellow = true
-            return
-        }
-        if (clusterResponse.status.toString() == "GREEN") {
-            errorYellow = false
-            errorRed = false
-            yellow = 0
-            red = 0
-            if (alertSent) {
-                runBlocking {
-                    alertService.createAlert(Alert("OK: Elastic is in GREEN again"))
+    private suspend fun checkClusterStatus() {
+        for (i in 1..3) {
+            try {
+                val clusterResponse = elastic.cluster().health(ClusterHealthRequest(), RequestOptions.DEFAULT)
+                log.debug("Current Status: $status, errorCount = $errorCount")
+                if (clusterResponse.status.toString() == Status.RED.name) {
+                    status = Status.RED
+                    return
                 }
-                alertSent = false
+                if (clusterResponse.status.toString() == Status.YELLOW.name) {
+                    status = Status.YELLOW
+                    return
+                }
+                if (clusterResponse.status.toString() == Status.GREEN.name) {
+                    status = Status.GREEN
+                    errorCount = 0
+                    if (alertSent) {
+                        alertService.createAlert(Alert("OK: Elastic is in GREEN again"))
+                        alertSent = false
+                    }
+                    return
+                }
+            } catch (ex: java.net.ConnectException) {
+                delay(FIFTEEN_SEC)
             }
         }
+        throw java.net.ConnectException("Lost connection to Elasticsearch")
     }
 
     suspend fun alertOnClusterHealth() {
         while (true) {
-            if (!errorRed && !errorYellow) {
+            if (!status.isError) {
                 checkClusterStatus()
                 delay(THIRTY_SEC)
-            }
-            else {
-                if (errorRed) {
-                    if (red < 2) {
-                        red++
-                        delay(THIRTY_SEC)
-                        checkClusterStatus()
-                        continue
+            } else {
+                if (errorCount < status.failuresForATrigger) {
+                    errorCount++
+                    checkClusterStatus()
+                    delay(THIRTY_SEC)
+                } else {
+                    if (!alertSent) {
+                        alertService.createAlert(Alert("ALERT: Elastic has status: $status"))
+                        alertSent = true
                     }
-                    else {
-                        if (!alertSent) {
-                            alertService.createAlert(Alert("ALERT: Elastic has status RED"))
-                            alertSent = true
-                        }
-                        delay(THIRTY_SEC)
-                        checkClusterStatus()
-                    }
-                }
-                if (errorYellow) {
-                    if (yellow < 10) {
-                        yellow++
-                        delay(THIRTY_SEC)
-                        checkClusterStatus()
-                        continue
-                    }
-                    else {
-                        if (!alertSent) {
-                            alertService.createAlert(Alert("ALERT: Elastic has status YELLOW"))
-                            alertSent = true
-                        }
-                        delay(THIRTY_SEC)
-                        checkClusterStatus()
-                    }
+                    delay(THIRTY_SEC)
+                    checkClusterStatus()
                 }
             }
         }
@@ -99,13 +84,17 @@ class ElasticAlerting(
 
     suspend fun alertOnStatusCode() {
         var alertOnStatus = false
+        var numberOfRetries = 0
         while (true) {
+            if (numberOfRetries == 3) {
+                throw java.net.ConnectException("Lost connection to Elasticsearch")
+            }
             //Period Format = YYYY.MM.dd
             val yesterdayPeriodFormat = LocalDate.now().minusDays(1).toString().replace("-", ".")
             val todayPeriodFormat = LocalDate.now().toString().replace("-", ".")
             val requestFor5xxCodes = SearchRequest()
 
-            requestFor5xxCodes.indices("http_logs_*$yesterdayPeriodFormat", "http_logs_*$todayPeriodFormat")
+            requestFor5xxCodes.indices("http_logs_*$yesterdayPeriodFormat*", "http_logs_*$todayPeriodFormat*")
             requestFor5xxCodes.source(
                 SearchSourceBuilder().query(
                     QueryBuilders.boolQuery()
@@ -123,13 +112,18 @@ class ElasticAlerting(
                         )
                 )
             )
-            val numberOf5XXStatusCodes = elastic.search(requestFor5xxCodes, RequestOptions.DEFAULT).hits.totalHits.toDouble()
+            val numberOf5XXStatusCodes = try {
+                elastic.search(requestFor5xxCodes, RequestOptions.DEFAULT).hits.totalHits.toDouble()
+            } catch (ex: java.net.ConnectException) {
+                numberOfRetries++
+                delay(FIFTEEN_SEC)
+                continue
+            }
 
+            val totalNumberOfEntriesRequest = SearchRequest()
 
-            val TotalNumberOfEntriesRequest = SearchRequest()
-
-            TotalNumberOfEntriesRequest.indices("http_logs_*$yesterdayPeriodFormat", "http_logs_*$todayPeriodFormat")
-            TotalNumberOfEntriesRequest.source(
+            totalNumberOfEntriesRequest.indices("http_logs_*$yesterdayPeriodFormat*", "http_logs_*$todayPeriodFormat*")
+            totalNumberOfEntriesRequest.source(
                 SearchSourceBuilder().query(
                     QueryBuilders.boolQuery()
                         .must(
@@ -143,12 +137,22 @@ class ElasticAlerting(
                 )
             )
 
-            val totalNumberOfEntries =
-                elastic.search(TotalNumberOfEntriesRequest, RequestOptions.DEFAULT).hits.totalHits.toDouble()
+            val totalNumberOfEntries = try {
+                    elastic.search(totalNumberOfEntriesRequest, RequestOptions.DEFAULT).hits.totalHits.toDouble()
+                } catch (ex: java.net.ConnectException) {
+                    numberOfRetries++
+                    delay(FIFTEEN_SEC)
+                    continue
+                }
+            //If no indices that is by name http_logs_*date
+            if (totalNumberOfEntries.toInt() == 0) {
+                delay(FIVE_MIN)
+                continue
+            }
             val percentage = numberOf5XXStatusCodes / totalNumberOfEntries * 100
-            println(percentage)
+            log.debug("Current percentage is: $percentage")
             if (percentage > LIMIT_5XX_PERCENTAGE && !alertOnStatus) {
-                val message = "ALERT: To many 5XX status codes\n" +
+                val message = "ALERT: Too many 5XX status codes\n" +
                         "Entries last 15 min: $totalNumberOfEntries \n" +
                         "Number of 5XX status codes: $numberOf5XXStatusCodes \n" +
                         "Percentage: ${percentage}% (Limit is $LIMIT_5XX_PERCENTAGE %)"
@@ -161,7 +165,11 @@ class ElasticAlerting(
                 alertOnStatus = false
             }
             delay(FIVE_MIN)
+            numberOfRetries = 0
         }
     }
 
+    companion object : Loggable {
+        override val log = ElasticAlerting.logger()
+    }
 }
