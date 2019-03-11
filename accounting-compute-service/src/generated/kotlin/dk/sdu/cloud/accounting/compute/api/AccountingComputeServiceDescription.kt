3cb70ef426f3d1bed4cd4fe71b689ac687f2ecf0
Henrik Schulz <henrikschulz@hotmail.com>
2019-03-11 11:13:16 +0100

    Updated build gradle in  half  of services

--- accounting-compute-service/src/generated/kotlin/dk/sdu/cloud/accounting/compute/api/AccountingComputeServiceDescription.kt
+++ accounting-compute-service/src/generated/kotlin/dk/sdu/cloud/accounting/compute/api/AccountingComputeServiceDescription.kt
@@ -1,8 +1,8 @@
 package dk.sdu.cloud.accounting.compute.api
 
-import dk.sdu.cloud.client.ServiceDescription
+import dk.sdu.cloud.ServiceDescription
 
 object AccountingComputeServiceDescription : ServiceDescription {
     override val name: String = "accounting-compute"
-    override val version: String = "1.0.0"
+    override val version: String = "1.0.1"
 }
