3c7141cc0e71ca14fb23579c082c29afa24916b4
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-05-03 09:35:10 +0200

    More tests, fix CreateUpdate usage

--- frontend-web/webclient/__tests__/Metadata/CreateUpdate.test.tsx
+++ frontend-web/webclient/__tests__/Metadata/CreateUpdate.test.tsx
@@ -1,21 +1,25 @@
 import * as React from "react";
 import { create } from "react-test-renderer";
-import CreateUpdate from "Project/CreateUpdate";
+import CreateUpdate from "../../app/Project/CreateUpdate";
 import { createMemoryHistory } from "history";
 import { Provider } from "react-redux";
-import { configureStore } from "Utilities/ReduxUtilities";
-import { initStatus } from "DefaultObjects";
-import status from "Navigation/Redux/StatusReducer";
+import { configureStore, responsive } from "../../app/Utilities/ReduxUtilities";
+import { initStatus } from "../../app/DefaultObjects";
+import theme from "../../app/ui-components/theme";
+import status from "../../app/Navigation/Redux/StatusReducer";
 import "jest-styled-components";
+import { ThemeProvider } from "styled-components";
 
 describe("CreateUpdate Component", () => {
-    test.skip("Mount", () => {
+    test("Mount", () => {
         expect(create(
-            <Provider store={configureStore({ status: initStatus() }, { status })}>
-                <CreateUpdate
-                    location={{ search: "?" }}
-                    history={createMemoryHistory()}
-                />
+            <Provider store={configureStore({ status: initStatus() }, { status, responsive })}>
+                <ThemeProvider theme={theme}>
+                    <CreateUpdate
+                        location={{ search: "?" }}
+                        history={createMemoryHistory()}
+                    />
+                </ThemeProvider>
             </Provider>
         ).toJSON()).toMatchSnapshot()
     });
