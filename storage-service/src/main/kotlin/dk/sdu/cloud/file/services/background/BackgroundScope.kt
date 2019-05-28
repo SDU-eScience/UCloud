185988ef60d23e76305d35b50f17b1d342626601
Dan Sebastian Thrane <dthrane@gmail.com>
2019-05-28 10:43:58 +0200

    Fixes broken tests

--- storage-service/src/main/kotlin/dk/sdu/cloud/file/services/background/BackgroundScope.kt
+++ storage-service/src/main/kotlin/dk/sdu/cloud/file/services/background/BackgroundScope.kt
@@ -1,14 +1,17 @@
 package dk.sdu.cloud.file.services.background
 
+import dk.sdu.cloud.service.Loggable
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
+import org.slf4j.Logger
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
-object BackgroundScope : CoroutineScope {
+object BackgroundScope : CoroutineScope, Loggable {
+    override val log: Logger = logger()
     private lateinit var dispatcher: CoroutineDispatcher
     private lateinit var executor: ExecutorService
     private lateinit var job: Job
@@ -17,8 +20,10 @@ object BackgroundScope : CoroutineScope {
         get() = dispatcher + job
 
     fun init() {
+        log.debug("Calling init()")
         synchronized(this) {
-            if (!this::job.isInitialized) {
+            if (!this::job.isInitialized || job.isCompleted) {
+                log.info("Initializing BackgroundScope")
                 executor = Executors.newCachedThreadPool()
                 dispatcher = executor.asCoroutineDispatcher()
                 job = Job()
@@ -27,12 +32,15 @@ object BackgroundScope : CoroutineScope {
     }
 
     fun stop() {
+        log.debug("Calling stop()")
         job.cancel()
         executor.shutdown()
     }
 
     fun reset() {
+        log.debug("Calling reset()")
         if (this::job.isInitialized && !job.isCompleted) {
+            log.info("Resetting BackgroundScope")
             stop()
             while (!executor.isShutdown) {
                 Thread.sleep(10)
