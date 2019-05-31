602f203edaf1b2b74d1823ba06c16dc2eb7edd57
Claudio Pica <pica@cp3.sdu.dk>
2019-05-31 18:54:14 +0200

    Changed login page.

--- frontend-web/webclient/app/ui-components/Error.tsx
+++ frontend-web/webclient/app/ui-components/Error.tsx
@@ -8,9 +8,9 @@ import Card from "ui-components/Card";
 interface ErrorProps { clearError?: () => void, error?: string, width?: string | number }
 const Error = (props: ErrorProps) => !!props.error ?
     <Card borderRadius="0.5em" height="auto" p="1em 1em 1em 1em" color="black" bg="lightRed" borderColor="red" width={props.width}>
-        <Flex>
-            <Box><Text fontSize={2} color="red">{props.error}</Text></Box>
-            {props.clearError ? <Box ml="auto"><Icon name="close" color="black" onClick={e => (props.clearError!(), e.stopPropagation())} /></Box> : null}
+        <Flex alignItems="center">
+            <Box><Text fontSize={1} color="red">{props.error}</Text></Box>
+            {props.clearError ? <Box ml="auto"><Icon size="1em" name="close" color="black" onClick={e => (props.clearError!(), e.stopPropagation())} /></Box> : null}
         </Flex>
     </Card> : null;
 
