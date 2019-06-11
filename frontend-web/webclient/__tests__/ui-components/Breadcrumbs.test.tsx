1d24e1c63a17b11d24666b200a357247068cca11
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-06-11 11:59:04 +0200

    fix failing tests

--- frontend-web/webclient/__tests__/ui-components/Breadcrumbs.test.tsx
+++ frontend-web/webclient/__tests__/ui-components/Breadcrumbs.test.tsx
@@ -1,36 +1,43 @@
 import * as React from "react";
-import { create } from "react-test-renderer";
-import { BreadCrumbs } from "../../app/ui-components/Breadcrumbs";
-import { configure, mount } from "enzyme";
+import {create} from "react-test-renderer";
+import {BreadCrumbs} from "../../app/ui-components/Breadcrumbs";
+import theme from "../../app/ui-components/theme";
+import {configure, mount} from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import "jest-styled-components";
-configure({ adapter: new Adapter() });
+import {ThemeProvider} from "styled-components";
+configure({adapter: new Adapter()});
 
 
 describe("Breadcrumbs", () => {
     it("Build breadcrumbs", () => {
-        expect(create(<BreadCrumbs 
-            currentPath="/home/mail@mailhost.dk/folder1"
-            navigate={() => undefined} 
-            homeFolder={"/home/mail@mailhost.dk"}
-        />)).toMatchSnapshot();
+        expect(create(
+            <ThemeProvider theme={theme}>
+                <BreadCrumbs
+                    currentPath="/home/mail@mailhost.dk/folder1"
+                    navigate={() => undefined}
+                    homeFolder={"/home/mail@mailhost.dk"}
+                />
+            </ThemeProvider>)).toMatchSnapshot();
     });
 
     it("Build breadcrumbs with empty path", () => {
-        expect(create(<BreadCrumbs 
+        expect(create(
+            <ThemeProvider theme={theme}>
+        <BreadCrumbs
             currentPath=""
-            navigate={() => undefined} 
+            navigate={() => undefined}
             homeFolder={"mail@mailhost.dk"}
-        />)).toMatchSnapshot();
+        /></ThemeProvider>)).toMatchSnapshot();
     });
 
     it("Using navigate", () => {
         const navigate = jest.fn();
-        const breadcrumbs = mount(<BreadCrumbs 
+        const breadcrumbs = mount(<ThemeProvider theme={theme}><BreadCrumbs
             currentPath="/home/mail@mailhost.dk/folder1"
-            navigate={navigate} 
+            navigate={navigate}
             homeFolder={"/home/mail@mailhost.dk"}
-        />);
+        /></ThemeProvider>);
         breadcrumbs.find("span").first().simulate("click");
         expect(navigate).toHaveBeenCalled();
     });
