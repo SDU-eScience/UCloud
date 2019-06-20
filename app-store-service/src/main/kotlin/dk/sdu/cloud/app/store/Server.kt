c2b92837b258ad7c8b39ecae42433db00305549c
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-06-20 14:50:17 +0200

    Tests for app-store. Fix k8s resource for app-services. Move docs

--- app-store-service/src/main/kotlin/dk/sdu/cloud/app/store/Server.kt
+++ app-store-service/src/main/kotlin/dk/sdu/cloud/app/store/Server.kt
@@ -1,16 +1,21 @@
 package dk.sdu.cloud.app.store
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import dk.sdu.cloud.app.store.api.ApplicationDescription
+import dk.sdu.cloud.app.store.api.ToolDescription
 import dk.sdu.cloud.app.store.rpc.AppStoreController
 import dk.sdu.cloud.app.store.rpc.ToolController
 import dk.sdu.cloud.app.store.services.AppStoreService
 import dk.sdu.cloud.app.store.services.ApplicationHibernateDAO
 import dk.sdu.cloud.app.store.services.ToolHibernateDAO
+import dk.sdu.cloud.app.store.util.yamlMapper
 import dk.sdu.cloud.micro.Micro
+import dk.sdu.cloud.micro.developmentModeEnabled
 import dk.sdu.cloud.micro.hibernateDatabase
 import dk.sdu.cloud.micro.server
-import dk.sdu.cloud.service.CommonServer
-import dk.sdu.cloud.service.configureControllers
-import dk.sdu.cloud.service.startServices
+import dk.sdu.cloud.service.*
+import dk.sdu.cloud.service.db.withTransaction
+import java.io.File
 
 class Server(override val micro: Micro) : CommonServer {
     override val log = logger()
@@ -27,6 +32,38 @@ class Server(override val micro: Micro) : CommonServer {
                 AppStoreController(appStoreService),
                 ToolController(db, toolDAO)
             )
+        }
+
+        if (micro.developmentModeEnabled) {
+            val listOfApps = db.withTransaction {
+                applicationDAO.listLatestVersion(it, null, NormalizedPaginationRequest(null, null))
+            }
+
+            if (listOfApps.itemsInTotal == 0) {
+                db.withTransaction { session ->
+                    val tools = File("yaml", "tools")
+                    tools.listFiles().forEach {
+                        try {
+                            val description = yamlMapper.readValue<ToolDescription>(it)
+                            toolDAO.create(session, "admin@dev", description.normalize())
+                        } catch (ex: Exception) {
+                            log.info("Could not create tool: $it")
+                            log.info(ex.stackTraceToString())
+                        }
+                    }
+
+                    val apps = File("yaml", "apps")
+                    apps.listFiles().forEach {
+                        try {
+                            val description = yamlMapper.readValue<ApplicationDescription>(it)
+                            applicationDAO.create(session, "admin@dev", description.normalize())
+                        } catch (ex: Exception) {
+                            log.info("Could not create app: $it")
+                            log.info(ex.stackTraceToString())
+                        }
+                    }
+                }
+            }
         }
 
         startServices()
---
+++ app-store-service/src/test/kotlin/dk/sdu/cloud/app/store/util/TestData.kt
@@ -0,0 +1,91 @@
+package dk.sdu.cloud.app.store.util
+
+import dk.sdu.cloud.app.store.api.*
+import io.mockk.mockk
+
+val normAppDesc = Application(
+    ApplicationMetadata(
+        "name",
+        "2.2",
+        listOf("Authors"),
+        "title",
+        "app description",
+        emptyList(),
+        null
+    ),
+    ApplicationInvocationDescription(
+        ToolReference("tool", "1.0.0", null),
+        mockk(relaxed = true),
+        emptyList(),
+        listOf("glob")
+    )
+)
+
+val normAppDesc2 = normAppDesc.withNameAndVersion("app", "1.2")
+
+val normAppDesc3 = normAppDesc
+    .withInvocation(
+        listOf(
+            VariableInvocationParameter(listOf("int"), prefixVariable = "--int "),
+            VariableInvocationParameter(listOf("great"), prefixVariable = "--great "),
+            VariableInvocationParameter(listOf("missing"), prefixGlobal = "--missing ")
+        )
+    )
+    .withParameters(
+        listOf(
+            ApplicationParameter.Integer("int"),
+            ApplicationParameter.Text("great", true),
+            ApplicationParameter.Integer("missing", true)
+        )
+    )
+
+fun Application.withNameAndVersion(name: String, version: String): Application {
+    return copy(
+        metadata = normAppDesc.metadata.copy(
+            name = name,
+            version = version
+        )
+    )
+}
+
+fun Application.withNameAndVersionAndTitle(name: String, version: String, title: String): Application {
+    return copy(
+        metadata = normAppDesc.metadata.copy(
+            name = name,
+            version = version,
+            title = title
+        )
+    )
+}
+
+fun Application.withTags(tags: List<String>): Application = copy(
+    metadata = metadata.copy(
+        tags = tags
+    )
+)
+
+fun Application.withInvocation(invocation: List<InvocationParameter>): Application = copy(
+    invocation = this.invocation.copy(
+        invocation = invocation
+    )
+)
+
+fun Application.withParameters(parameters: List<ApplicationParameter<*>>): Application = copy(
+    invocation = this.invocation.copy(
+        parameters = parameters
+    )
+)
+
+val normToolDesc = NormalizedToolDescription(
+    NameAndVersion("tool", "1.0.0"),
+    "container",
+    2,
+    2,
+    SimpleDuration(1, 0, 0),
+    listOf(""),
+    listOf("Author"),
+    "title",
+    "description",
+    ToolBackend.DOCKER,
+    "MIT"
+)
