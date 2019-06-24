239525f999daf0d4c4a7a2d014ff738431a86f57
Henrik Schulz <henrikschulz@hotmail.com>
2019-06-24 17:31:46 +0200

    added more to support doc

---
+++ app-service/src/generated/kotlin/dk/sdu/cloud/app/api/AppServiceDescription.kt
@@ -0,0 +1,8 @@
+package dk.sdu.cloud.app.api
+
+import dk.sdu.cloud.ServiceDescription
+
+object AppServiceDescription : ServiceDescription {
+    override val name: String = "app"
+    override val version: String = "1.11.0-PROJECTS.3"
+}
