cf7cad0acc171b241b0736a57a1fe023b2d0cd5b
Henrik Schulz <henrikschulz@hotmail.com>
2019-06-25 15:42:04 +0200

    Jenkins failed due to missing jenkinsfile in app-service. Had been reintroduced due to a generated file. Removed dir

--- app-service/src/generated/kotlin/dk/sdu/cloud/app/api/AppServiceDescription.kt
+++
@@ -1,8 +0,0 @@
-package dk.sdu.cloud.app.api
-
-import dk.sdu.cloud.ServiceDescription
-
-object AppServiceDescription : ServiceDescription {
-    override val name: String = "app"
-    override val version: String = "1.11.0-PROJECTS.3"
-}
