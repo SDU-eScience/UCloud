8143349020cf4dba69bc9b4acf12e16452c8ae9d
Dan Sebastian Thrane <dthrane@gmail.com>
2019-08-08 12:35:29 +0200

    Fixes #903

--- service-common/src/main/kotlin/dk/sdu/cloud/events/RedisStreamService.kt
+++ service-common/src/main/kotlin/dk/sdu/cloud/events/RedisStreamService.kt
@@ -91,7 +91,7 @@ class RedisStreamService(
                     messages: List<StreamMessage<String, String>>
                 ) {
                     val events = messages.mapNotNull { message ->
-                        val textValue = message.body.values.first()
+                        val textValue = message.body?.values?.first() ?: return@mapNotNull null
                         if (textValue == STREAM_INIT_MSG) return@mapNotNull null
 
                         runCatching { stream.deserialize(textValue) }.getOrNull()
