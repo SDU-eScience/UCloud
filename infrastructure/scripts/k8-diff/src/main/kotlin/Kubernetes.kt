9999dc31e62ff2e3c4f0d6866071fc7cfe9262c0
Dan Sebastian Thrane <dthrane@gmail.com>
2019-08-09 07:35:30 +0200

    Allow k8-diff to use context

--- infrastructure/scripts/k8-diff/src/main/kotlin/Kubernetes.kt
+++ infrastructure/scripts/k8-diff/src/main/kotlin/Kubernetes.kt
@@ -1,8 +1,13 @@
 import com.fasterxml.jackson.module.kotlin.readValue
 
 object Kubernetes {
-    fun readResource(type: String, name: String, namespace: String? = null): K8Doc? {
-        val content = runProcess("kubectl", "get", "-o", "yaml", type, name, "--namespace", namespace ?: "default")
+    fun readResource(type: String, name: String, namespace: String? = null, context: String? = null): K8Doc? {
+        val command = arrayListOf("kubectl", "get", "-o", "yaml", type, name)
+
+        if (namespace != null) command.addAll(listOf("--namespace", namespace))
+        if (context != null) command.addAll(listOf("--context", context))
+
+        val content = runProcess(*command.toTypedArray())
         if (content.isEmpty()) return null
 
         return yamlMapper.readValue<K8Doc>(content).cleanDocument()
