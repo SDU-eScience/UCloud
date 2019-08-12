601cfe4e4e35c92c14d31b2bef68ed827289c96a
Dan Sebastian Thrane <dthrane@gmail.com>
2019-08-12 12:22:35 +0200

    Upgrade service common and bootstrap for all services

--- infrastructure/scripts/set_bootstrap_version.kts
+++ infrastructure/scripts/set_bootstrap_version.kts
@@ -16,8 +16,8 @@ inputDirectories.forEach {
     val tempBuildGradle = File(baseDir, "build.gradle2")
     tempBuildGradle.outputStream().bufferedWriter().use { outs ->
         buildGradle.bufferedReader().forEachLine { line ->
-            if (line.contains("GradleBootstrap") && line.contains("apply from:") {
-                outs.write("apply from: \"https://raw.githubusercontent.com/SDU-eScience/GradleBootstrap/v$version/sdu-cloud.gradle\"")
+            if (line.contains("GradleBootstrap") && line.contains("apply from:")) {
+                outs.write("apply from: \"https://raw.githubusercontent.com/SDU-eScience/GradleBootstrap/v$version/sdu-cloud.gradle\"\n")
             } else {
                 outs.write(line + "\n")
             }
