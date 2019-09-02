b68052c9c9d6e3127d85728fb36df5be645c0708
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-09-02 10:22:36 +0200

    Remove some unsafe method use

--- frontend-web/webclient/app/AvataaarLib/options/Selector.tsx
+++ frontend-web/webclient/app/AvataaarLib/options/Selector.tsx
@@ -1,87 +1,86 @@
-import * as PropTypes from 'prop-types'
-import * as React from 'react'
+import * as PropTypes from "prop-types";
+import * as React from "react";
+import Option from "./Option";
+import OptionContext from "./OptionContext";
 
-import Option from './Option'
-import OptionContext from './OptionContext'
-
-function getComponentOptionValue (component: React.ComponentClass) {
+function getComponentOptionValue(component: React.ComponentClass) {
   const optionValue = (component as any).optionValue;
   if (!optionValue) {
-    throw new Error(`optionValue should be provided for ${component}`)
+    throw new Error(`optionValue should be provided for ${component}`);
   }
-  return optionValue
+  return optionValue;
 }
 
 export interface Props {
-  option: Option
-  defaultOption: React.ComponentClass | string
+  option: Option;
+  defaultOption: React.ComponentClass | string;
 }
 
 export default class Selector extends React.Component<Props> {
-  static contextTypes = {
+  public static contextTypes = {
     optionContext: PropTypes.instanceOf(OptionContext)
   };
 
-  private get optionContext (): OptionContext {
-    return this.context.optionContext
+  private get optionContext(): OptionContext {
+    return this.context.optionContext;
   }
 
-  componentWillMount () {
-    const { option, defaultOption } = this.props;
-    const { optionContext } = this;
+  public componentDidMount() {
+    const {option, defaultOption} = this.props;
+    const {optionContext} = this;
     const defaultValue = (
-      typeof defaultOption === 'string' ?
-      defaultOption : getComponentOptionValue(defaultOption)
+      typeof defaultOption === "string" ?
+        defaultOption : getComponentOptionValue(defaultOption)
     );
     optionContext.addStateChangeListener(this.optionContextUpdate);
     optionContext.optionEnter(option.key);
     const optionState = optionContext.getOptionState(option.key);
     this.updateOptionValues();
     if (optionState) {
-      optionContext.setDefaultValue(option.key, defaultValue)
+      optionContext.setDefaultValue(option.key, defaultValue);
     }
   }
 
-  componentWillUpdate (nextProps: Props & { children?: React.ReactNode }) {
-    this.updateOptionValues(nextProps)
+  public componentDidUpdate(nextProps: Props & {children?: React.ReactNode}) {
+    this.updateOptionValues(nextProps);
   }
 
-  componentWillUnmount () {
+  public componentWillUnmount() {
     this.optionContext.removeStateChangeListener(this.optionContextUpdate);
-    this.optionContext.optionExit(this.props.option.key)
+    this.optionContext.optionExit(this.props.option.key);
   }
 
-  render () {
+  public render() {
     let result: React.ReactNode | null = null;
-    const { option, children } = this.props;
+    const {option, children} = this.props;
     const value = this.optionContext.getValue(option.key)!;
     React.Children.forEach(children, child => {
       if (getComponentOptionValue((child as any).type) === value) {
-        result = child
+        result = child;
       }
     });
-    return result
+    return result;
   }
 
   private optionContextUpdate = () => {
-    this.forceUpdate()
-  };
+    this.forceUpdate();
+  }
 
-  private updateOptionValues (
-    nextProps?: Props & { children?: React.ReactNode }
+  private updateOptionValues(
+    nextProps?: Props & {children?: React.ReactNode}
   ) {
     if (nextProps && this.props.children === nextProps.children) {
-      return
+      return;
     }
-    const { option, children } = this.props;
+    const {option, children} = this.props;
     const values = React.Children.map(
       children,
-      // TODO: also validate and throw error if we don't see optionValue
+      // TODO: also validate and throw error if we don"t see optionValue
       child => getComponentOptionValue((child as any).type)
     );
     if (new Set(values).size !== values.length) {
-      throw new Error('Duplicate values')
+      throw new Error("Duplicate values");
     }
-    this.optionContext.setOptions(option.key, values)
+    this.optionContext.setOptions(option.key, values);
   }
 }
