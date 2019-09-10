aa66deac50a419ed69b7c269a1291b5a0ff75912
Dan Sebastian Thrane <dthrane@gmail.com>
2019-09-10 10:02:11 +0200

    Fixes #971

--- frontend-web/webclient/app/Applications/AppToolLogo.tsx
+++ frontend-web/webclient/app/Applications/AppToolLogo.tsx
@@ -21,11 +21,15 @@ export const AppToolLogo: React.FunctionComponent<AppToolLogoProps> = props => {
     useEffect(() => setLoadedImage(true), [props.cacheBust]);
 
     return <>
-        <AppToolImage
-            onError={() => setLoadedImage(false)}
-            style={hasLoadedImage ? {width: size, height: size} : {display: "none"}}
+        <img
+            onErrorCapture={() => {
+                setLoadedImage(false);
+                // For some reason the state is not always correctly set. This is the worst possible work around.
+                setTimeout(() => setLoadedImage(false), 50);
+            }}
+            style={hasLoadedImage ? {width: size, height: size, objectFit: "contain"} : {display: "none"}}
             src={Cloud.computeURL("/api", `/hpc/${context}/logo/${props.name}?cacheBust=${props.cacheBust}`)}
-        />
+            alt={props.name}/>
 
         {hasLoadedImage ? null : <AppLogo size={size} hash={hashF(props.name)}/>}
     </>;
