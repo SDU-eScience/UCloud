dcd5541200bc64edfeb7323115ffd5f7573ed513
Dan Sebastian Thrane <dthrane@gmail.com>
2019-09-23 13:28:54 +0200

    Fixes #985

--- rpc-test-service/src/main/kotlin/dk/sdu/cloud/rpc/test/Server.kt
+++ rpc-test-service/src/main/kotlin/dk/sdu/cloud/rpc/test/Server.kt
@@ -3,12 +3,16 @@ package dk.sdu.cloud.rpc.test
 import dk.sdu.cloud.auth.api.authenticator
 import dk.sdu.cloud.calls.client.OutgoingHttpCall
 import dk.sdu.cloud.calls.client.OutgoingWSCall
+import dk.sdu.cloud.calls.client.subscribe
 import dk.sdu.cloud.micro.Micro
 import dk.sdu.cloud.micro.server
+import dk.sdu.cloud.rpc.test.api.TestA
 import dk.sdu.cloud.rpc.test.rpc.TestController
 import dk.sdu.cloud.service.CommonServer
 import dk.sdu.cloud.service.configureControllers
 import dk.sdu.cloud.service.startServices
+import kotlinx.coroutines.runBlocking
+import kotlin.system.exitProcess
 
 class Server(override val micro: Micro) : CommonServer {
     override val log = logger()
