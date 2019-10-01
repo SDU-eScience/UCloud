75b4a74120e0bf9790de458c7c4e6a14407920d2
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-10-01 13:26:35 +0200

    Fixes #1009

--- frontend-web/webclient/app/Core.tsx
+++ frontend-web/webclient/app/Core.tsx
@@ -41,67 +41,87 @@ import AvataaarModification from "UserSettings/Avataaar";
 import UserSettings from "UserSettings/UserSettings";
 import {inDevEnvironment} from "UtilityFunctions";
 
-const NotFound = () => (<MainContainer main={<div><h1>Not found.</h1></div>}/>);
+const NotFound = () => (<MainContainer main={<div><h1>Not found.</h1></div>} />);
 
 const Core = () => {
-    return <>
-        <Dialog/>
-        <Snackbars/>
-        <Uploader/>
-        <Sidebar/>
-        <ErrorBoundary>
-            <Switch>
-                <Route exact path="/login" component={LoginPage}/>
-                <Route exact path="/loginSuccess" component={LoginSuccess}/>
-                <Route exact path="/login/wayf" component={Wayf}/>
-                <Route exact path="/" component={requireAuth(Dashboard)}/>
-                <Route exact path="/dashboard" component={requireAuth(Dashboard)}/>
+    return (
+        <>
+            <Dialog />
+            <Snackbars />
+            <Uploader />
+            <Sidebar />
+            <ErrorBoundary>
+                <Switch>
+                    <Route exact path="/login" component={LoginPage} />
+                    <Route exact path="/loginSuccess" component={LoginSuccess} />
+                    <Route exact path="/login/wayf" component={Wayf} />
+                    <Route exact path="/" component={requireAuth(Dashboard)} />
+                    <Route exact path="/dashboard" component={requireAuth(Dashboard)} />
 
-                <Route exact path="/files/info" component={requireAuth(FileInfo)}/>
-                <Route exact path="/files/preview" component={requireAuth(FilePreview)}/>
-                <Route exact path="/files" component={requireAuth(Files)}/>
+                    <Route exact path="/files/info" component={requireAuth(FileInfo)} />
+                    <Route exact path="/files/preview" component={requireAuth(FilePreview)} />
+                    <Route exact path="/files" component={requireAuth(Files)} />
 
-                <Route exact path="/activity" component={requireAuth(Activity)}/>
-                <Route exact path="/status" component={requireAuth(Status)}/>
-                <Route exact path="/accounting/:resource/:subResource"
-                       component={requireAuth(Accounting.DetailedPage)}/>
+                    <Route
+                        exact
+                        path="/activity"
+                        component={requireAuth(Activity)}
+                    />
+                    <Route
+                        exact
+                        path="/status"
+                        component={requireAuth(Status)}
+                    />
+                    <Route
+                        exact
+                        path="/accounting/:resource/:subResource"
+                        component={requireAuth(Accounting.DetailedPage)}
+                    />
 
-                <Route exact path="/novnc" component={requireAuth(NoVNCClient)}/>
+                    <Route exact path="/novnc" component={requireAuth(NoVNCClient)} />
 
-                <Route exact path="/applications" component={requireAuth(ApplicationsBrowse)}/>
-                <Route exact path="/applications/overview" component={requireAuth(ApplicationsOverview)}/>
-                <Route exact path="/applications/installed" component={requireAuth(ApplicationsInstalled.default)}/>
-                <Route exact path="/applications/details/:appName/:appVersion"
-                       component={requireAuth(ApplicationView)}/>
-                <Route exact path="/applications/results" component={requireAuth(JobResults)}/>
-                <Route exact path="/applications/results/:jobId" component={requireAuth(DetailedResult)}/>
-                <Route exact path="/applications/:appName/:appVersion" component={requireAuth(Run)}/>
+                    <Route exact path="/applications" component={requireAuth(ApplicationsBrowse)} />
+                    <Route exact path="/applications/overview" component={requireAuth(ApplicationsOverview)} />
+                    <Route
+                        exact
+                        path="/applications/installed"
+                        component={requireAuth(ApplicationsInstalled.default)}
+                    />
+                    <Route
+                        exact
+                        path="/applications/details/:appName/:appVersion"
+                        component={requireAuth(ApplicationView)}
+                    />
+                    <Route exact path="/applications/results" component={requireAuth(JobResults)} />
+                    <Route exact path="/applications/results/:jobId" component={requireAuth(DetailedResult)} />
+                    <Route exact path="/applications/:appName/:appVersion" component={requireAuth(Run)} />
 
-                <Route exact path={"/applications/studio"} component={requireAuth(AppStudioPage)}/>
-                <Route exact path={"/applications/studio/t/:name"} component={requireAuth(AppStudioTools)}/>
-                <Route exact path={"/applications/studio/a/:name"} component={requireAuth(AppStudioApps)}/>
+                    <Route exact path={"/applications/studio"} component={requireAuth(AppStudioPage)} />
+                    <Route exact path={"/applications/studio/t/:name"} component={requireAuth(AppStudioTools)} />
+                    <Route exact path={"/applications/studio/a/:name"} component={requireAuth(AppStudioApps)} />
 
-                {!inDevEnvironment() ? null :
-                    <Route exact path={"/playground"} component={Playground}/>
-                }
+                    {!inDevEnvironment() ? null :
+                        <Route exact path={"/playground"} component={Playground} />
+                    }
 
-                <Route exact path="/shares" component={requireAuth(Share.List)}/>
+                    <Route exact path="/shares" component={requireAuth(Share.List)} />
 
-                <Route exact path="/admin/usercreation" component={requireAuth(UserCreation)}/>
+                    <Route exact path="/admin/usercreation" component={requireAuth(UserCreation)} />
 
-                <Route exact path="/users/settings" component={requireAuth(UserSettings)}/>
-                <Route exact path="/users/avatar" component={requireAuth(AvataaarModification)}/>
+                    <Route exact path="/users/settings" component={requireAuth(UserSettings)} />
+                    <Route exact path="/users/avatar" component={requireAuth(AvataaarModification)} />
 
-                <Route exact path="/search/:priority" component={requireAuth(Search)}/>
+                    <Route exact path="/search/:priority" component={requireAuth(Search)} />
 
-                <Route exact path="/projects" component={requireAuth(ProjectList)}/>
-                <Route exact path="/projects/create" component={requireAuth(ProjectCreate)}/>
-                <Route exact path="/projects/view/:id" component={requireAuth(ProjectView)}/>
+                    <Route exact path="/projects" component={requireAuth(ProjectList)} />
+                    <Route exact path="/projects/create" component={requireAuth(ProjectCreate)} />
+                    <Route exact path="/projects/view/:id" component={requireAuth(ProjectView)} />
 
-                <Route component={NotFound}/>
-            </Switch>
-        </ErrorBoundary>
-    </>;
+                    <Route component={NotFound} />
+                </Switch>
+            </ErrorBoundary>
+        </>
+    );
 };
 
 const requireAuth = Delegate => props => {
@@ -112,7 +132,7 @@ const requireAuth = Delegate => props => {
     return <Delegate {...props} />;
 };
 
-const LoginSuccess = (props: { history: History }) => {
+const LoginSuccess = (props: {history: History}) => {
     dispatchUserAction(USER_LOGIN);
     onLogin();
     props.history.push("/");
