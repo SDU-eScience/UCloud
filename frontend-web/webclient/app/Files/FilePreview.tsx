a22a9f5b357953a702273c6be767ffc716b6fb6e
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-11-22 13:13:12 +0100

    Add fullscreen icon. More preview

--- frontend-web/webclient/app/Files/FilePreview.tsx
+++ frontend-web/webclient/app/Files/FilePreview.tsx
@@ -7,11 +7,13 @@ import * as React from "react";
 import {connect} from "react-redux";
 import {match} from "react-router";
 import {Dispatch} from "redux";
-import {Box} from "ui-components";
-import {extensionTypeFromPath, removeTrailingSlash} from "UtilityFunctions";
-import {fetchPreviewFile, setFilePreviewError} from "./Redux/FilePreviewAction";
-import {statFileOrNull} from "Utilities/FileUtilities";
 import {snackbarStore} from "Snackbar/SnackbarStore";
+import styled from "styled-components";
+import {Icon} from "ui-components";
+import {Spacer} from "ui-components/Spacer";
+import {statFileOrNull} from "Utilities/FileUtilities";
+import {extensionTypeFromPath, removeTrailingSlash, requestFullScreen} from "UtilityFunctions";
+import {fetchPreviewFile, setFilePreviewError} from "./Redux/FilePreviewAction";
 
 interface FilePreviewStateProps {
     file: File;
@@ -44,26 +46,24 @@ const FilePreview = (props: FilePreviewProps) => {
             } else if (stat.size! > 30_000_000) {
                 snackbarStore.addFailure("File size too large. Download instead.");
             } else {
-                if ((stat?.size ?? 30_000_001) > 30_000_000) {
-                    Client.createOneTimeTokenWithPermission("files.download:read").then((token: string) => {
-                        fetch(Client.computeURL(
-                            "/api",
-                            `/files/download?path=${encodeURIComponent(path)}&token=${encodeURIComponent(token)}`
-                        )).then(async content => {
-                            switch (fileType) {
-                                case "image":
-                                case "video":
-                                case "pdf":
-                                    setFileContent(URL.createObjectURL(await content.blob()));
-                                    break;
-                                case "code":
-                                case "text":
-                                    setFileContent(await content.text());
-                                    break;
-                            }
-                        }).catch(it => typeof it === "string" ? setError(it) : setError("An error occurred fetching file content"));
-                    });
-                }
+                Client.createOneTimeTokenWithPermission("files.download:read").then((token: string) => {
+                    fetch(Client.computeURL(
+                        "/api",
+                        `/files/download?path=${encodeURIComponent(path)}&token=${encodeURIComponent(token)}`
+                    )).then(async content => {
+                        switch (fileType) {
+                            case "image":
+                            case "video":
+                            case "pdf":
+                                setFileContent(URL.createObjectURL(await content.blob()));
+                                break;
+                            case "code":
+                            case "text":
+                                setFileContent(await content.text());
+                                break;
+                        }
+                    }).catch(it => typeof it === "string" ? setError(it) : setError("An error occurred fetching file content"));
+                });
             }
         });
     }, []);
@@ -73,21 +73,36 @@ const FilePreview = (props: FilePreviewProps) => {
     }
 
     function showContent() {
-        if (!fileContent) return (<LoadingIcon size={18} />);
+        if (!fileContent) return (<LoadingIcon size={36} />);
         switch (fileType) {
             case "text":
             case "code":
-                return (<code style={{whiteSpace: "pre-wrap"}}>{fileContent}</code>);
+                /* TODO: Syntax highlighting (Google Prettify?) */
+                return (
+                    <>
+                        <Spacer left={<div />} right={<ExpandingIcon name="fullscreen" onClick={onTryFullScreen} />} />
+                        <code className="fullscreen" style={{whiteSpace: "pre-wrap"}}>{fileContent}</code>
+                    </>
+                );
             case "image":
                 return <img src={fileContent} />;
             case "audio":
             case "video":
-                return <video src={fileContent} controls />;
+                return <video style={{maxWidth: "100%", maxHeight: "100%", verticalAlign: "middle"}} src={fileContent} controls />;
             case "pdf":
-                return <embed src={fileContent} />;
+                return (
+                    <>
+                        <Spacer left={<div />} right={<ExpandingIcon name="fullscreen" mb="5px" onClick={onTryFullScreen} />} />
+                        <embed className="fullscreen" width="999999" height="1080" style={{maxWidth: "100%", maxHeight: "100%"}} src={fileContent} />
+                    </>
+                );
             default:
                 return (<div>Can't render content</div>);
         }
+    }
+
+    function onTryFullScreen() {
+        requestFullScreen(document.getElementsByClassName("fullscreen")[0]!, () => snackbarStore.addFailure("Failed to enter fullscreen."));
     }
 
     function filepath() {
@@ -101,14 +116,17 @@ const FilePreview = (props: FilePreviewProps) => {
 
     return (
         <MainContainer
-            main={(
-                <Box maxWidth="80vw">
-                    {showContent()}
-                </Box>
-            )}
+            main={showContent()}
         />
     );
 };
+
+const ExpandingIcon = styled(Icon)`
+    &:hover {
+        transform: scale(1.05);
+        cursor: pointer;
+    }
+`;
 
 const mapStateToProps = ({filePreview}: ReduxObject): FilePreviewReduxState => ({
     file: filePreview.file
