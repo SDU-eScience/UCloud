e3b607026c0a08f572c0e370223e512d8ab0234d
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-11-28 13:24:08 +0100

    Improve Eyecon

--- frontend-web/webclient/app/ui-components/icons/components/eye.tsx
+++ frontend-web/webclient/app/ui-components/icons/components/eye.tsx
@@ -6,9 +6,16 @@ const SvgEye = (props: any) => (
       d="M2 10c8 10 38 10 48 0M2 10c8-10 38-10 48 0"
       stroke="currentcolor"
       fill="transparent"
-      strokeWidth={0.5}
+      strokeWidth={2}
     />
-    <circle cx={26} cy={10} r={4} stroke="currentcolor" fill="currentcolor" />
+    <circle
+      cx={26}
+      cy={10}
+      r={4}
+      stroke="currentcolor"
+      strokeWidth={4}
+      fill="none"
+    />
   </svg>
 );
 
