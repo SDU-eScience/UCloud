abb3dd4f12165aa1222f2026a10fbda27a4b3915
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-12-03 16:25:59 +0100

    Add more functionality to DowntimeManagement

--- frontend-web/webclient/app/Admin/DowntimeManagement.tsx
+++ frontend-web/webclient/app/Admin/DowntimeManagement.tsx
@@ -1,27 +1,36 @@
 import {Client} from "Authentication/HttpClientInstance";
 import {format} from "date-fns/esm";
 import {MainContainer} from "MainContainer/MainContainer";
+import {setActivePage} from "Navigation/Redux/StatusActions";
 import * as React from "react";
 import {connect} from "react-redux";
-import {Box, Flex, Icon, Input, List, InputGroup, Button} from "ui-components";
+import {Dispatch} from "redux";
+import {SnackType} from "Snackbar/Snackbars";
+import {snackbarStore} from "Snackbar/SnackbarStore";
+import {Box, Button, Flex, Icon, Input, InputGroup, List} from "ui-components";
 import {DatePicker} from "ui-components/DatePicker";
 import * as Heading from "ui-components/Heading";
+import {SidebarPages} from "ui-components/Sidebar";
+import {addStandardDialog} from "UtilityComponents";
+import {stopPropagationAndPreventDefault} from "UtilityFunctions";
 
 interface Downtime {
+    id: number;
     from: number;
     to: number;
 }
 
-export function DowntimeManagement() {
+export function DowntimeManagement(props: {setActivePage: () => void}) {
     const [start, setStart] = React.useState<Date | null>(null);
     const [end, setEnd] = React.useState<Date | null>(null);
     const [downtimes, setDowntimes] = React.useState<Downtime[]>([]);
     React.useEffect(() => {
+        props.setActivePage();
         setDowntimes([
-            {from: new Date().getTime(), to: new Date().getTime() + 3_600_000},
-            {from: new Date().getTime(), to: new Date().getTime() + 3_600_000},
-            {from: new Date().getTime() - 3_600_000 * 5, to: new Date().getTime() - 3_600_000 * 3},
-            {from: new Date().getTime() + 3_600_000 * 12, to: new Date().getTime() + 3_600_000 * 15}
+            {id: 1, from: new Date().getTime(), to: new Date().getTime() + 3_600_000},
+            {id: 2, from: new Date().getTime(), to: new Date().getTime() + 3_600_000},
+            {id: 3, from: new Date().getTime() - 3_600_000 * 5, to: new Date().getTime() - 3_600_000 * 3},
+            {id: 4, from: new Date().getTime() + 3_600_000 * 12, to: new Date().getTime() + 3_600_000 * 15}
         ]);
     }, []);
 
@@ -36,52 +45,88 @@ export function DowntimeManagement() {
         <MainContainer
             header={<Heading.h2>Downtimes</Heading.h2>}
             main={(
-                <Box>
-                    <Flex mx="6px">
-                        <InputGroup>
-                            <DatePicker
-                                placeholderText="From"
-                                fontSize="18px"
-                                value={start ? format(start, "dd/MM/yyyy HH:mm:ss") : undefined}
-                                onChange={setStart}
-                                selectsStart
-                                showTimeSelect
-                                endDate={end}
-                            />
-                            <DatePicker
-                                placeholderText="To"
-                                fontSize="18px"
-                                value={end ? format(end, "dd/MM/yyyy HH:mm:ss") : undefined}
-                                onChange={setEnd}
-                                startDate={start}
-                                showTimeSelect
-                                selectsEnd
-                            />
-                        </InputGroup>
-                        <Button ml="5px" mr="-5px" onClick={() => alert("TODO ")}>Add</Button>
-                    </Flex>
-                    <Box width="420px">
-                        <DowntimeList downtimes={inProgress} name="In progress" remove={() => alert("TODO")} />
-                        <DowntimeList downtimes={upcoming} name="Upcoming" remove={() => alert("TODO")} />
-                        <DowntimeList downtimes={expired} name="Expired" remove={() => alert("TODO")} />
+                <Flex justifyContent="center">
+                    <Box>
+                        <form onSubmit={submit}>
+                            <Flex mx="6px">
+                                <InputGroup>
+                                    <DatePicker
+                                        placeholderText="From"
+                                        fontSize="18px"
+                                        value={start ? format(start, DATE_FORMAT) : undefined}
+                                        onChange={setStart}
+                                        selectsStart
+                                        showTimeSelect
+                                        endDate={end}
+                                    />
+                                    <DatePicker
+                                        placeholderText="To"
+                                        fontSize="18px"
+                                        value={end ? format(end, DATE_FORMAT) : undefined}
+                                        onChange={setEnd}
+                                        startDate={start}
+                                        showTimeSelect
+                                        selectsEnd
+                                    />
+                                </InputGroup>
+                                <Button ml="5px" mr="-5px">Add</Button>
+                            </Flex>
+                        </form>
+                        <Box width="420px" mt="40px">
+                            <DowntimeList downtimes={inProgress} name="In progress" remove={remove} />
+                            <DowntimeList downtimes={upcoming} name="Upcoming" remove={remove} />
+                            <DowntimeList downtimes={expired} name="Expired" remove={remove} />
+                        </Box>
                     </Box>
-                </Box>
+                </Flex>
             )}
         />
     );
+
+    function submit(e: React.FormEvent<HTMLFormElement>) {
+        stopPropagationAndPreventDefault(e);
+        if (start == null) {
+            snackbarStore.addFailure("Please add a starting date and time.");
+            return;
+        } else if (end == null) {
+            snackbarStore.addFailure("Please add an end date and time.");
+            return;
+        }
+
+        snackbarStore.addSnack({
+            type: SnackType.Success,
+            message: "\"submitted\""
+        });
+    }
+
+    function remove(id: number) {
+        addStandardDialog({
+            title: "Remove planned downtime?",
+            message: "",
+            onConfirm: () => setDowntimes(downtimes.filter(it => it.id !== id))
+        });
+    }
 }
 
-function DowntimeList(props: {downtimes: Downtime[], name: string, remove: () => void}) {
+const DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
+function DowntimeList(props: {downtimes: Downtime[], name: string, remove: (id: number) => void}) {
     return (
         <>
             {props.name}
             <List>
-                {name}
                 {props.downtimes.map(it => (
-                    <Flex key={it.to}>
-                        <Input my="6px" mx="6px" readOnly width="50%" value={format(it.from, "HH:mm:ss dd/MM/yyyy")} />
-                        <Input my="6px" mx="6px" readOnly width="50%" value={format(it.to, "HH:mm:ss dd/MM/yyyy")} />
-                        <Icon mt="16px" ml="5px" cursor="pointer" name="close" color="red" onClick={props.remove} />
+                    <Flex key={it.id}>
+                        <Input my="6px" mx="6px" readOnly width="50%" value={format(it.from, DATE_FORMAT)} />
+                        <Input my="6px" mx="6px" readOnly width="50%" value={format(it.to, DATE_FORMAT)} />
+                        <Icon
+                            mt="16px"
+                            ml="5px"
+                            cursor="pointer"
+                            name="close"
+                            color="red"
+                            onClick={() => props.remove(it.id)}
+                        />
                     </Flex>
                 ))}
             </List>
@@ -89,4 +134,8 @@ function DowntimeList(props: {downtimes: Downtime[], name: string, remove: () =>
     );
 }
 
-export default connect(null)(DowntimeManagement);
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    setActivePage: () => dispatch(setActivePage(SidebarPages.Admin)),
+});
+
+export default connect(null, mapDispatchToProps)(DowntimeManagement);
