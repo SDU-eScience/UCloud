b92fee31b70c33785a23cf4e942a146e6f1ab94d
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-02-24 13:35:41 +0100

    Fixes #1272

--- integration-testing/src/main/kotlin/dk/sdu/cloud/integration/Main.kt
+++ integration-testing/src/main/kotlin/dk/sdu/cloud/integration/Main.kt
@@ -3,10 +3,7 @@ package dk.sdu.cloud.integration
 import dk.sdu.cloud.auth.api.RefreshingJWTAuthenticator
 import dk.sdu.cloud.auth.api.RefreshingJWTCloudFeature
 import dk.sdu.cloud.calls.client.OutgoingHttpCall
-import dk.sdu.cloud.integration.backend.AvatarTesting
-import dk.sdu.cloud.integration.backend.FileFavoriteTest
-import dk.sdu.cloud.integration.backend.FileTesting
-import dk.sdu.cloud.integration.backend.UserAndClient
+import dk.sdu.cloud.integration.backend.*
 import dk.sdu.cloud.micro.*
 import dk.sdu.cloud.service.Loggable
 import dk.sdu.cloud.service.TokenValidationJWT
@@ -35,30 +32,9 @@ suspend fun main(args: Array<String>) {
         micro.tokenValidation as TokenValidationJWT
     ).authenticateClient(OutgoingHttpCall)
 
-    val authenticatedClientB = RefreshingJWTAuthenticator(
-        micro.client,
-        config.userB.refreshToken,
-        micro.tokenValidation as TokenValidationJWT
-    ).authenticateClient(OutgoingHttpCall)
-
     while (true) {
         try {
             Integration.log.info("Running tests")
-
-            FileTesting(
-                UserAndClient(config.userA.username, authenticatedClientA),
-                UserAndClient(config.userB.username, authenticatedClientB)
-            ).runTest()
-
-            FileFavoriteTest(
-                UserAndClient(config.userA.username, authenticatedClientA)
-            ).runTest()
-            
-            AvatarTesting(
-                UserAndClient(config.userA.username, authenticatedClientA),
-                UserAndClient(config.userB.username, authenticatedClientB)
-            ).runTest()
-
             SupportTesting(
                 UserAndClient(config.userA.username, authenticatedClientA)
             ).runTest()
