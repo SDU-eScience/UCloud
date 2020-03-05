ad857cbc446daf3b1550ea63fa6a3af1a4ce7d88
Dan Sebastian Thrane <dthrane@gmail.com>
2020-03-05 12:10:41 +0100

    Fixes #1311

--- app-fs-kubernetes-service/src/main/kotlin/dk/sdu/cloud/app/fs/kubernetes/api/KubernetesFileSystemCalls.kt
+++
@@ -1,7 +0,0 @@
-package dk.sdu.cloud.app.fs.kubernetes.api
-
-import dk.sdu.cloud.app.fs.api.FileSystemCalls
-
-object KubernetesFileSystemCalls : FileSystemCalls("kubernetes")
-
-const val ROOT_DIRECTORY = "app-filesystems"
