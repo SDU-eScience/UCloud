be75352f6bf9b6b1b126295e1aea416ea9a45ee4
Brian Alberg <alberg@imada.sdu.dk>
2020-03-05 14:57:12 +0100

    Added html template

--- mail-service/src/main/kotlin/dk/sdu/cloud/mail/services/MailService.kt
+++ mail-service/src/main/kotlin/dk/sdu/cloud/mail/services/MailService.kt
@@ -10,7 +10,9 @@ import dk.sdu.cloud.calls.client.orThrow
 import io.ktor.http.HttpStatusCode
 import javax.mail.*
 import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeBodyPart
 import javax.mail.internet.MimeMessage
+import javax.mail.internet.MimeMultipart
 
 class MailService(
     private val authenticatedClient: AuthenticatedClient,
@@ -38,13 +40,53 @@ class MailService(
 
         try {
             val message = MimeMessage(session)
-
             message.setFrom(InternetAddress(fromAddress, "eScience Support"))
-
             message.addRecipient(Message.RecipientType.TO, recipientAddress)
-
             message.subject = subject
-            message.setText(text)
+
+            val multipart = MimeMultipart()
+
+            val bodyPart = MimeBodyPart()
+            val template = """
+                <!DOCTYPE HTML>
+                <html>
+                    <body style='margin:0; padding:0; font-family: "IBM Plex Sans", sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Oxygen, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";'>
+                        <div style="padding: .4em .3em .1em 2em; background: #0069FD;">
+                            <a href="https://cloud.sdu.dk">
+                                <img style="margin-right: .2em;" src="cid:ucloud_logo">
+                            </a>
+                        </div>
+                        <div style="padding: 2em 4em 2em 4em; max-width: 600px; margin-left: auto; margin-right: auto;">
+                            $text
+                            
+                            <p>Best regards,</p>
+                            <p>
+                                <strong>The UCloud Team<br>SDU eScience Center</strong>
+                            </p>
+                        </div> 
+                        <div style="text-align: right; padding: 1em;">
+                            <img src="cid:escience_logo">
+                        </div>
+                    </body>
+                </html> 
+            """.trimIndent()
+
+            bodyPart.setContent(template, "text/html")
+            multipart.addBodyPart(bodyPart)
+
+            val escienceLogo = MimeBodyPart()
+            escienceLogo.disposition = MimeBodyPart.INLINE
+            escienceLogo.contentID = "escience_logo"
+            escienceLogo.attachFile("sdu_escience_logo.png")
+            multipart.addBodyPart(escienceLogo)
+
+            val ucloudLogo = MimeBodyPart()
+            ucloudLogo.disposition = MimeBodyPart.INLINE
+            ucloudLogo.contentID = "ucloud_logo"
+            ucloudLogo.attachFile("ucloud_logo.png")
+            multipart.addBodyPart(ucloudLogo)
+
+            message.setContent(multipart)
 
             Transport.send(message)
         } catch (e: Throwable) {
