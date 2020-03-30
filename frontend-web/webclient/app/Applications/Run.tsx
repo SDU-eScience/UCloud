e19ebe9ee80934f402e362f2d712d5643298768b
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-03-30 09:29:24 +0200

    Fixes #1399

--- frontend-web/webclient/app/Applications/Run.tsx
+++ frontend-web/webclient/app/Applications/Run.tsx
@@ -442,7 +442,7 @@ class Run extends React.Component<RunAppProps, RunAppState> {
                 itemsPerPage: 25,
                 attrs: [FileResource.PATH],
                 order: SortOrder.DESCENDING,
-                sortBy: SortBy.PATH
+                sortBy: SortBy.MODIFIED_AT
             }));
             this.setState(() => ({previousRuns}));
         } catch {
