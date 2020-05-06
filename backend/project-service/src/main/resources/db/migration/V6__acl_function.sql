b324a97e8c258d57be8d3b9af4947b9310f7edaf
Dan Sebastian Thrane <dthrane@gmail.com>
2020-05-06 07:44:54 +0200

    Refactoring of project acl function

--- backend/project-service/src/main/resources/db/migration/V6__acl_function.sql
+++ backend/project-service/src/main/resources/db/migration/V6__acl_function.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION check_group_acl(uname text, is_admin boolean, g groups)
+CREATE OR REPLACE FUNCTION check_group_acl(uname text, is_admin boolean, group_filter text)
 RETURNS BOOLEAN AS $$
 DECLARE passed BOOLEAN;
 BEGIN
@@ -9,7 +9,7 @@ BEGIN
             from group_members gm
             where
                 gm.username = uname and
-                gm.the_group = g.the_group
+                gm.the_group = group_filter
         )
     ) into passed;
 
