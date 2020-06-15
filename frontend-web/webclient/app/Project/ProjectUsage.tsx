6b2bdf4dce81cf890fc5e91eb29b87bafe974f29
Dan Sebastian Thrane <dthrane@gmail.com>
2020-06-15 08:21:06 +0200

    Formatting

--- frontend-web/webclient/app/Project/ProjectUsage.tsx
+++ frontend-web/webclient/app/Project/ProjectUsage.tsx
@@ -110,7 +110,7 @@ const durationOptions: Duration[] = [
         bucketSizeText: "every 30 days",
         timeInPast: 1000 * 60 * 60 * 24 * 365
     },
-]
+];
 
 const ProjectUsage: React.FunctionComponent<ProjectUsageOperations> = props => {
     const {projectId, ...projectManagement} = useProjectManagementStatus();
