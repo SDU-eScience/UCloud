58a5f9656b1c847bf544b2273a191f80c4a11fb9
Henrik Schulz <henrikschulz@hotmail.com>
2020-06-16 09:50:40 +0200

    refactored to DBAsync

---
+++ backend/app-license-service/src/main/kotlin/app/license/services/AppLicenseAsyncDao.kt
@@ -0,0 +1,236 @@
+package dk.sdu.cloud.app.license.services
+
+import com.github.jasync.sql.db.RowData
+import dk.sdu.cloud.Roles
+import dk.sdu.cloud.SecurityPrincipal
+import dk.sdu.cloud.app.license.api.*
+import dk.sdu.cloud.service.db.async.DBContext
+import dk.sdu.cloud.service.db.async.SQLTable
+import dk.sdu.cloud.service.db.async.getField
+import dk.sdu.cloud.service.db.async.insert
+import dk.sdu.cloud.service.db.async.int
+import dk.sdu.cloud.service.db.async.sendPreparedStatement
+import dk.sdu.cloud.service.db.async.text
+import dk.sdu.cloud.service.db.async.withSession
+
+object TagLicenseTable : SQLTable ("tags") {
+    val name = text("name", notNull = true)
+    val serverId = text("license_server", notNull = true)
+}
+
+object LicenseServerTable : SQLTable("license_servers") {
+    val id = text("id", notNull = true)
+    val name = text("name", notNull = true)
+    val address = text("address", notNull = true)
+    val port = int("port", notNull = true)
+    val license = text("license")
+}
+
+fun RowData.toLicenseServerWithId(): LicenseServerWithId {
+    return LicenseServerWithId(
+        id = getField(LicenseServerTable.id),
+        name = getField(LicenseServerTable.name),
+        address = getField(LicenseServerTable.address),
+        port = getField(LicenseServerTable.port),
+        license = if (getField(LicenseServerTable.license).isBlank()) {null}
+                    else {getField(LicenseServerTable.license)}
+    )
+}
+
+fun RowData.toIdentifiable(): LicenseServerId {
+    return LicenseServerId(
+        id = getField(LicenseServerTable.id),
+        name = getField(LicenseServerTable.name)
+    )
+}
+
+
+class AppLicenseAsyncDao {
+
+    suspend fun create(db: DBContext, serverId: String, appLicenseServer: LicenseServer) {
+        db.withSession { session ->
+            session.insert(LicenseServerTable) {
+                set(LicenseServerTable.id, serverId)
+                set(LicenseServerTable.name, appLicenseServer.name)
+                set(LicenseServerTable.address, appLicenseServer.address)
+                set(LicenseServerTable.port, appLicenseServer.port)
+                set(LicenseServerTable.license, appLicenseServer.license)
+            }
+        }
+    }
+
+    suspend fun getById(
+        db: DBContext,
+        id: String
+    ): LicenseServerWithId? {
+        return db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("serverID", id)
+                    },
+                    """
+                        SELECT * 
+                        FROM license_servers
+                        WHERE id = ?serverID
+                    """.trimIndent()
+                ).rows.firstOrNull()?.toLicenseServerWithId()
+        }
+    }
+
+    suspend fun list(
+        db: DBContext,
+        tags: List<String>,
+        securityPrincipal: SecurityPrincipal,
+        projectGroups: List<ProjectAndGroup>
+    ): List<LicenseServerId>? {
+        var query = """
+            SELECT LS.id, LS.name, LS.address, LS.port, LS.license FROM license_servers AS LS
+            INNER JOIN permissions as P
+                ON LS.id = P.server_id
+            WHERE LS.id IN (SELECT T.license_server FROM tags AS T where T.name IN (select unnest(?tags)))
+                AND (
+                    P.username = ?user
+        """
+
+        if (projectGroups.isNotEmpty()) {
+            query += " OR ("
+            for((i, index) in projectGroups.indices.withIndex()) {
+                query += "(P.project = ?project$index AND P.project_group = ?group$index)"
+                if (i < projectGroups.size - 1) {
+                    query += " OR "
+                }
+            }
+            query += ")"
+        }
+
+        query += " AND (P.permission = 'READ_WRITE' OR P.permission = 'READ'))"
+
+        return db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("tags", tags)
+                        setParameter("user", securityPrincipal.username)
+                        projectGroups.forEachIndexed { index, projectAndGroup ->
+                            setParameter("project$index", projectAndGroup.project)
+                            setParameter("group$index", projectAndGroup.group)
+                        }
+                    },
+                    query
+                ).rows.map { it.toIdentifiable() }
+
+        }
+    }
+
+    suspend fun listAll(
+        db: DBContext,
+        user: SecurityPrincipal
+    ): List<LicenseServerWithId>? {
+        return db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("role", user.role.toString())
+                        setParameter("privileged", Roles.PRIVILEDGED.toList())
+                    },
+                    """
+                        SELECT * 
+                        FROM license_servers
+                        WHERE ?role in (select unnest(?privileged)) 
+                    """.trimIndent()
+                ).rows.map { it.toLicenseServerWithId() }
+        }
+    }
+
+    suspend fun update(db: DBContext, appLicenseServer: LicenseServerWithId) {
+        db.withSession { session ->
+            val existing = session
+                .sendPreparedStatement(
+                    {
+                        setParameter("licenseID", appLicenseServer.id)
+                    },
+                    """
+                        SELECT * 
+                        FROM license_servers
+                        WHERE id = ?licenseID
+                    """.trimIndent()
+                ).rows.firstOrNull()
+
+            if (existing != null) {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("address", appLicenseServer.address)
+                            setParameter("port", appLicenseServer.port)
+                            setParameter("license", appLicenseServer.license)
+                            setParameter("name", appLicenseServer.name)
+                            setParameter("licenseID", appLicenseServer.id)
+                        },
+                        """
+                            UPDATE license_servers
+                            SET address = ?address, port = ?port, license = ?license, name = ?name
+                            WHERE id = ?licenseID
+                        """.trimIndent()
+                    )
+            }
+        }
+    }
+
+    suspend fun delete(db: DBContext, serverId: String) {
+        db.withSession{ session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("serverID", serverId)
+                    },
+                    """
+                        DELETE FROM license_servers
+                        WHERE id = ?serverID
+                    """.trimIndent()
+                )
+        }
+    }
+
+    suspend fun addTag(db: DBContext, name: String, serverId: String) {
+        db.withSession { session ->
+            session
+                .insert(TagLicenseTable) {
+                    set(TagLicenseTable.name, name)
+                    set(TagLicenseTable.serverId, serverId)
+                }
+        }
+    }
+
+    suspend fun listTags(db: DBContext, serverId: String): List<String> {
+        return db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("serverId", serverId)
+                    },
+                    """
+                        SELECT * 
+                        FROM tags
+                        WHERE license_server = ?serverId
+                    """.trimIndent()
+                ).rows.map { it.getField(TagLicenseTable.name) }
+        }
+    }
+
+    suspend fun deleteTag(db: DBContext, name: String, serverId: String) {
+        db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("serverId", serverId)
+                        setParameter("name", name)
+                    },
+                    """
+                        DELETE FROM tags
+                        WHERE license_server = ?serverId AND name = ?name
+                    """.trimIndent()
+                )
+        }
+    }
+}
---
+++ backend/app-license-service/src/main/kotlin/app/license/services/acl/AclAsyncDao.kt
@@ -0,0 +1,236 @@
+package dk.sdu.cloud.app.license.services.acl
+
+import com.github.jasync.sql.db.RowData
+import dk.sdu.cloud.app.license.api.*
+import dk.sdu.cloud.service.db.async.DBContext
+import dk.sdu.cloud.service.db.async.SQLTable
+import dk.sdu.cloud.service.db.async.getField
+import dk.sdu.cloud.service.db.async.insert
+import dk.sdu.cloud.service.db.async.sendPreparedStatement
+import dk.sdu.cloud.service.db.async.text
+import dk.sdu.cloud.service.db.async.withSession
+
+object PermissionTable : SQLTable ("permissions") {
+    val user = text("username", notNull = true)
+    val project = text("project", notNull = true)
+    val group = text("project_group", notNull = true)
+    val serverId = text("server_id", notNull = true)
+    val permission = text("permission", notNull = true)
+}
+
+class AclAsyncDao {
+    suspend fun hasPermission(
+        db: DBContext,
+        serverId: String,
+        accessEntity: AccessEntity,
+        permission: ServerAccessRight
+    ): Boolean {
+        return db.withSession { session ->
+            if (permission == ServerAccessRight.READ_WRITE) {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("user", accessEntity.user ?: "")
+                            setParameter("project", accessEntity.project ?: "")
+                            setParameter("group", accessEntity.group ?: "")
+                            setParameter("write", ServerAccessRight.READ_WRITE.toString())
+                        },
+                        """
+                            SELECT *
+                            WHERE (
+                                (username = ?user) OR (project = ?project AND project_group = ?group)
+                            ) AND ( permission = ?write)
+                        """.trimIndent()
+                    ).rows.isNotEmpty()
+            } else {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("user", accessEntity.user ?: "")
+                            setParameter("project", accessEntity.project ?: "")
+                            setParameter("group", accessEntity.group ?: "")
+                            setParameter("read", ServerAccessRight.READ.toString())
+                            setParameter("write", ServerAccessRight.READ_WRITE.toString())
+                        },
+                        """
+                            SELECT *
+                            WHERE (
+                                (username = ?user) OR (project = ?project AND project_group = ?group)
+                            ) AND (permission = ?read OR permission = ?write)
+                        """.trimIndent()
+                    ).rows.isNotEmpty()
+            }
+        }
+    }
+
+
+    suspend fun updatePermissions(
+        db: DBContext,
+        serverId: String,
+        accessEntity: AccessEntity,
+        permissions: ServerAccessRight
+    ) {
+        db.withSession { session ->
+            val found = session
+                .sendPreparedStatement(
+                    {
+                        setParameter("server", serverId)
+                        setParameter("user", accessEntity.user ?: "")
+                        setParameter("project", accessEntity.project ?: "")
+                        setParameter("group", accessEntity.group ?: "")
+                    },
+                    """
+                        SELECT *
+                        FROM permissions
+                        WHERE (server_id = ?server) AND
+                            (project = ?project) AND 
+                            (username = ?user) AND
+                            (project_group = ?group)                        
+                    """.trimIndent()
+                ).rows.singleOrNull()
+
+            if (found != null) {
+                session
+                    .sendPreparedStatement(
+                    {
+                        setParameter("permission", permissions.toString())
+                        setParameter("server", serverId)
+                        setParameter("user", accessEntity.user ?: "")
+                        setParameter("project", accessEntity.project ?: "")
+                        setParameter("group", accessEntity.group ?: "")
+                    },
+                    """
+                        UPDATE permissions
+                        SET permission = ?permission
+                        WHERE (server_id = ?server) AND
+                            (project = ?project) AND 
+                            (username = ?user) AND
+                            (project_group = ?group)
+                    """.trimIndent()
+                )
+            } else {
+                session.insert(PermissionTable) {
+                    set(PermissionTable.permission, permissions.toString())
+                    set(PermissionTable.group, accessEntity.group ?: "")
+                    set(PermissionTable.project, accessEntity.project ?: "")
+                    set(PermissionTable.user, accessEntity.user ?: "")
+                    set(PermissionTable.serverId, serverId)
+                }
+            }
+        }
+    }
+
+    suspend fun revokePermission(
+        db: DBContext,
+        serverId: String,
+        accessEntity: AccessEntity
+    ) {
+        db.withSession { session ->
+            if (accessEntity.user.isNullOrBlank()) {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("serverID", serverId)
+                            setParameter("project", accessEntity.project)
+                            setParameter("group", accessEntity.group)
+                        },
+                        """
+                        DELETE FROM permissions
+                        WHERE (server_id = ?serverID) AND
+                            (project = ?project) AND
+                            (project_group =? ?group)
+                    """.trimIndent()
+                    )
+            } else {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("serverID", serverId)
+                            setParameter("user", accessEntity.user)
+                        },
+                        """
+                        DELETE FROM permissions
+                        WHERE (server_id = ?serverID) AND (username = ?user)
+                    """.trimIndent()
+                    )
+            }
+        }
+    }
+
+    suspend fun revokeAllServerPermissions(
+        db: DBContext,
+        serverId: String
+    ) {
+        db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("serverID", serverId)
+                    },
+                    """
+                        DELETE FROM permissions
+                        WHERE server_id = ?serverID
+                    """.trimIndent()
+                )
+        }
+    }
+
+    suspend fun listAcl(
+        db: DBContext,
+        serverId: String
+    ): List<AccessEntityWithPermission> {
+        return db.withSession { session ->
+            session
+                .sendPreparedStatement(
+                    {
+                        setParameter("serverID", serverId)
+                    },
+                    """
+                        SELECT *
+                        FROM permissions
+                        WHERE server_id = ?serverID
+                    """.trimIndent()
+                ).rows
+        }.map { it.toAccessWithPermission() }
+    }
+
+    suspend fun revokePermissionsFromEntity(db: DBContext, accessEntity: AccessEntity) {
+        db.withSession { session ->
+            if (accessEntity.user.isNullOrBlank()) {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("project", accessEntity.project)
+                            setParameter("group", accessEntity.group)
+                        },
+                        """
+                        DELETE FROM permissions
+                        WHERE (project = ?project) AND (project_group = group)
+                    """.trimIndent()
+                    )
+            } else {
+                session
+                    .sendPreparedStatement(
+                        {
+                            setParameter("user", accessEntity.user)
+                        },
+                        """
+                        DELETE FROM permissions
+                        WHERE username = ?user
+                    """.trimIndent()
+                    )
+            }
+        }
+    }
+
+    fun RowData.toAccessWithPermission(): AccessEntityWithPermission {
+        return AccessEntityWithPermission(
+            AccessEntity(
+                getField(PermissionTable.user),
+                getField(PermissionTable.project),
+                getField(PermissionTable.group)
+            ),
+            ServerAccessRight.valueOf(getField(PermissionTable.permission))
+        )
+    }
+}
