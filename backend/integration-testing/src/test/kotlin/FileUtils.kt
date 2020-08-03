6e742ffeec95f400fbe4898f784b8671bfed6f91
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-08-03 15:59:32 +0200

    Refactor one test

--- backend/integration-testing/src/test/kotlin/FileUtils.kt
+++ backend/integration-testing/src/test/kotlin/FileUtils.kt
@@ -1,14 +1,18 @@
 package dk.sdu.cloud.integration
 
+import dk.sdu.cloud.calls.client.AuthenticatedClient
 import dk.sdu.cloud.calls.client.call
 import dk.sdu.cloud.file.api.CreateDirectoryRequest
 import dk.sdu.cloud.file.api.FileDescriptions
-import dk.sdu.cloud.integration.UCloudLauncher.serviceClient
 import dk.sdu.cloud.integration.e2e.await
 import dk.sdu.cloud.integration.e2e.awaitElement
 import dk.sdu.cloud.integration.e2e.awaitElements
+import dk.sdu.cloud.integration.e2e.awaitNoElements
+import dk.sdu.cloud.service.test.retrySection
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import java.lang.Error
 
 fun uploadFile(homeFolder: String, fileName: String) {
     //MultiPartUploadDescriptions.simpleUpload.call(
@@ -21,31 +25,29 @@ fun uploadFile(homeFolder: String, fileName: String) {
 }
 
 suspend fun WebDriver.createFolder(folderName: String) {
-    awaitElements(By.tagName("button")).find {
-        it.findElement(By.tagName("span")).text == "New Folder"
-    }?.click()
-
-    val inputs = awaitElements(By.tagName("input"))
-
-    inputs.filter { it.isEnabled && it.isDisplayed }.forEach {
-        it.sendKeys(folderName)
-    }
-
-    awaitElements(By.tagName("button")).find {
-        it.text == "Create"
-    }?.click()
-
-    await {
-        findElements(By.tagName("div")).any {
-            it.text == folderName
-        }
-    }
+    findElement(By.xpath("//button/span[text()='New Folder']")).click()
+    awaitElement(By.xpath("//input[@id='default']")).sendKeys(folderName)
+    awaitElement(By.xpath("//button[text()='Create']")).click()
+    awaitElement(By.xpath("//div[text()='$folderName']")) ?: throw IllegalStateException("Element not found")
 }
 
 suspend fun WebDriver.deleteFolder(folderName: String) {
     // awaitElements()
 }
 
-suspend fun createDir(directoryPath: String) {
-    FileDescriptions.createDirectory.call(CreateDirectoryRequest(directoryPath), serviceClient)
+suspend fun WebDriver.selectFolder(folderName: String) {
+    await {
+        findElements(By.tagName("a")).find {
+            it.text == folderName
+        } != null
+    }
+
+    findElements(By.tagName("a")).find {
+        it.text == folderName && it.isDisplayed
+    }?.click()
+}
+
+suspend fun createDir(directoryPath: String, client: AuthenticatedClient) {
+    val foo = FileDescriptions.createDirectory.call(CreateDirectoryRequest(directoryPath), client)
+    println(foo.statusCode)
 }
