1e5514ae203e3bacbd8fb4433fbd4642442375d7
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-08-04 16:17:32 +0200

    Add e2e tests. Update packages. Add Datatags.

--- backend/integration-testing/src/test/kotlin/FileUtils.kt
+++ backend/integration-testing/src/test/kotlin/FileUtils.kt
@@ -1,9 +1,10 @@
 package dk.sdu.cloud.integration
 
-import dk.sdu.cloud.calls.client.AuthenticatedClient
 import dk.sdu.cloud.calls.client.call
 import dk.sdu.cloud.file.api.CreateDirectoryRequest
 import dk.sdu.cloud.file.api.FileDescriptions
+import dk.sdu.cloud.integration.backend.CreatedUser
+
 import dk.sdu.cloud.integration.e2e.await
 import dk.sdu.cloud.integration.e2e.awaitElement
 import dk.sdu.cloud.integration.e2e.awaitElements
@@ -11,8 +12,7 @@ import dk.sdu.cloud.integration.e2e.awaitNoElements
 import dk.sdu.cloud.service.test.retrySection
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
-import java.lang.Error
+import java.io.File
 
 fun uploadFile(homeFolder: String, fileName: String) {
     //MultiPartUploadDescriptions.simpleUpload.call(
@@ -25,14 +25,40 @@ fun uploadFile(homeFolder: String, fileName: String) {
 }
 
 suspend fun WebDriver.createFolder(folderName: String) {
-    findElement(By.xpath("//button/span[text()='New Folder']")).click()
+    retrySection {
+        awaitElement(By.xpath("//div[@data-tag='sidebar']/div/div/button[@data-tag='New Folder-action']/span")).click()
+    }
     awaitElement(By.xpath("//input[@id='default']")).sendKeys(folderName)
     awaitElement(By.xpath("//button[text()='Create']")).click()
-    awaitElement(By.xpath("//div[text()='$folderName']")) ?: throw IllegalStateException("Element not found")
+    awaitElement(By.xpath("//div[text()='$folderName']"))
+}
+
+suspend fun WebDriver.openFileDropdown(folderName: String) {
+    awaitElement(By.xpath("//*[contains(@data-tag, '$folderName-dropdown')]/../..")).click()
 }
 
 suspend fun WebDriver.deleteFolder(folderName: String) {
-    // awaitElements()
+    openFileDropdown(folderName)
+    awaitElement(By.xpath("//div[@data-tag='Move to Trash-action']")).click()
+    awaitElement(By.xpath("//button[text()='Move files']")).click()
+    awaitNoElements(By.xpath("//div[text()='$folderName']"))
+}
+
+suspend fun WebDriver.uploadFile(fileName: String) {
+    retrySection {
+        // This is for some reason prone to being stale.
+        awaitElement(By.xpath("//div[@data-tag='sidebar']/div/div/button[@data-tag='Upload Files-action']/span")).click()
+    }
+    val file = File(fileName)
+    file.writeText("FooBarBaz")
+    // TODO: find better selector criteria
+    awaitElement(By.xpath("/html/body/div[4]/div/div/div/div[2]/div/input")).sendKeys(file.absolutePath)
+    println(awaitElement(By.xpath("//button[text()='Upload']")).size)
+    awaitElement(By.xpath("//button[text()='Upload']")).click()
+    awaitElement(By.xpath("//button[text()='Clear finished uploads']"))
+    awaitElement(By.xpath("//*[@data-tag='modalCloseButton']")).click()
+    awaitNoElements(By.xpath("//button[text()='Clear finished uploads']"))
+    awaitElement(By.xpath("//div[text()='$fileName']"))
 }
 
 suspend fun WebDriver.selectFolder(folderName: String) {
@@ -47,7 +73,6 @@ suspend fun WebDriver.selectFolder(folderName: String) {
     }?.click()
 }
 
-suspend fun createDir(directoryPath: String, client: AuthenticatedClient) {
-    val foo = FileDescriptions.createDirectory.call(CreateDirectoryRequest(directoryPath), client)
-    println(foo.statusCode)
+suspend fun createDir(directoryPath: String, user: CreatedUser) {
+    println(FileDescriptions.createDirectory.call(CreateDirectoryRequest(directoryPath, user.username), user.client))
 }
--- backend/integration-testing/src/test/kotlin/e2e/FilesTest.kt
+++ backend/integration-testing/src/test/kotlin/e2e/FilesTest.kt
@@ -1,13 +1,11 @@
 package dk.sdu.cloud.integration.e2e
 
-import dk.sdu.cloud.app.license.services.LicenseServerTable.address
-import dk.sdu.cloud.file.api.CreateDirectoryRequest
-import dk.sdu.cloud.file.api.FileDescriptions
-import dk.sdu.cloud.integration.backend.createUser
+import dk.sdu.cloud.file.api.FileDescriptions.transferQuota
+import dk.sdu.cloud.integration.backend.*
 import dk.sdu.cloud.integration.createDir
 import dk.sdu.cloud.integration.createFolder
 import dk.sdu.cloud.integration.deleteFolder
-import dk.sdu.cloud.integration.selectFolder
+import dk.sdu.cloud.integration.uploadFile
 import org.junit.Test
 import org.openqa.selenium.By
 
@@ -19,21 +17,40 @@ class FilesTest : EndToEndTest() {
         driver.get("$address/app")
         driver.login(user.username, user.password)
         driver.goToFiles()
-        driver.createFolder("foobar")
+        driver.createFolder("foo")
     }
 
     @Test
     fun `Delete folder`() = e2e {
         val user = createUser()
-        val folderName = "foobar"
-        createDir("/home/${user.username}/$folderName", user.client)
+        val folderName = "bar"
+        createDir("/home/${user.username}/$folderName", user)
         driver.get("$address/app")
         driver.login(user.username, user.password)
         driver.goToFiles()
+        driver.deleteFolder(folderName)
+    }
 
-        val element = driver.awaitElement(By.xpath("//*[contains(@data-tag,'$folderName-dropdown')]"))
-        println(element.tagName)
-        element.click()
-        // driver.deleteFolder(folderName)
+    @Test
+    fun `Rename folder`() = e2e {
+        val user = createUser()
+        val folderName = "baz"
+        createDir("/home/${user.username}/$folderName", user)
+        driver.get("$address/app")
+        driver.login(user.username, user.password)
+        driver.goToFiles()
+    }
+
+    @Test
+    fun `Upload file`() = e2e {
+        val user = createUser()
+        val rootProject = initializeRootProject()
+        addFundsToPersonalProject(rootProject, user.username, sampleStorage.category)
+        setProjectQuota(rootProject, 1024 * 1024 * 1024 * 1024L)
+        setPersonalQuota(rootProject, user.username, 50_000_000)
+        driver.get("$address/app")
+        driver.login(user.username, user.password)
+        driver.goToFiles()
+        driver.uploadFile("foo.txt")
     }
 }
