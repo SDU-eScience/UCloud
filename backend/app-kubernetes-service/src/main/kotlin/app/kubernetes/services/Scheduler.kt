ed92ec01327c786bf8f2d768734ad70db6e3f2b7
Dan Sebastian Thrane <dthrane@gmail.com>
2020-09-09 09:40:24 +0200

    Some notes and thoughts about scheduling

---
+++ backend/app-kubernetes-service/src/main/kotlin/app/kubernetes/services/Scheduler.kt
@@ -0,0 +1,389 @@
+package dk.sdu.cloud.app.kubernetes.services
+
+import dk.sdu.cloud.app.store.api.*
+import dk.sdu.cloud.micro.*
+import dk.sdu.cloud.service.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import kotlinx.coroutines.selects.*
+import java.util.*
+import java.util.concurrent.atomic.*
+import kotlin.collections.HashMap
+import kotlin.math.*
+import kotlin.random.Random
+
+enum class ScheduledJobState {
+    AWAITING_SCHEDULE,
+    SCHEDULED,
+    RUNNING,
+    TERMINATED
+}
+
+data class SchedulingConstraints(
+    val weekdays: List<Int>,
+    val hourOfDayStart: Int,
+    val hourOfDayEnd: Int
+)
+
+data class Node(
+    val id: String,
+    val vcpu: Int,
+    val ram: Int,
+    val freeList: FreeList = FreeList(vcpu, ram)
+)
+
+private data class FreeSlot(
+    var start: Long,
+    var end: Long,
+    var vCpuAvailable: Int,
+    var ramAvailable: Int,
+    var nextSlot: FreeSlot? = null,
+    var previousSlot: FreeSlot? = null,
+    val jobs: ArrayList<String> = ArrayList()
+)
+
+class FreeList(private val totalCpu: Int, private val totalRam: Int) {
+    private val firstSlot = FreeSlot(0, Long.MAX_VALUE, totalCpu, totalRam)
+
+    fun findJobsAt(timestamp: Long): List<String> {
+        var currentSlot: FreeSlot? = firstSlot
+        while (currentSlot != null) {
+            if (timestamp in currentSlot.start..currentSlot.end) {
+                return ArrayList(currentSlot.jobs)
+            }
+            currentSlot = currentSlot.nextSlot
+        }
+        return emptyList()
+    }
+
+    fun hasSpaceAt(start: Long, end: Long, vCpuReservation: Int, ramReservation: Int): Boolean {
+        var currentSlot: FreeSlot? = firstSlot
+        var combinedTime = 0L
+        var startFound = false
+        val totalTime = end - start
+
+        while (currentSlot != null) {
+            if (start >= currentSlot.start && start <= currentSlot.end) {
+                startFound = true
+            } else if (startFound) {
+                return false
+            }
+
+            val hasSpace = currentSlot.vCpuAvailable >= vCpuReservation &&
+                    currentSlot.ramAvailable >= ramReservation
+            when {
+                startFound && hasSpace -> {
+                    combinedTime += currentSlot.end - max(currentSlot.start, start)
+                }
+
+                startFound && !hasSpace -> {
+                    return false
+                }
+            }
+
+            if (combinedTime >= totalTime) {
+                return true
+            }
+
+            currentSlot = currentSlot.nextSlot
+        }
+
+        return false
+    }
+
+    fun findFreeSlot(vCpuReservation: Int, ramReservation: Int, duration: Long): Long? {
+        var currentSlot: FreeSlot? = firstSlot
+
+        while (currentSlot != null) {
+            if (currentSlot.vCpuAvailable >= vCpuReservation && currentSlot.ramAvailable >= ramReservation) {
+                var combinedTime = currentSlot.end - currentSlot.start
+                if (combinedTime >= duration) {
+                    return currentSlot.start
+                }
+
+                // Not enough time in a single slot, we need to start searching
+                var lastSlot: FreeSlot? = currentSlot.nextSlot
+                while (lastSlot != null && combinedTime < duration) {
+                    if (lastSlot.vCpuAvailable >= vCpuReservation && lastSlot.ramAvailable >= ramReservation) {
+                        combinedTime += lastSlot.end - lastSlot.start
+                        lastSlot = lastSlot.nextSlot
+                    } else {
+                        break
+                    }
+                }
+
+                if (combinedTime >= duration) {
+                    return currentSlot.start
+                }
+            }
+
+            currentSlot = currentSlot.nextSlot
+        }
+
+        return null
+    }
+
+    fun addReservation(start: Long, end: Long, vCpuReservation: Int, ramReservation: Int, jobId: String) {
+        // Find all slots we overlap with
+        // If we are fully contained within (i.e. larger or same size) the slot then we can simply adjust the values
+        // If we are smaller then split the slot
+
+        var currentSlot: FreeSlot? = firstSlot
+        while (true) {
+            if (currentSlot == null) break
+            // Slot: 0 100
+            // Res : 10 50
+
+            // Slot: 0 100
+            // Res : 99 200
+
+            if (start >= currentSlot.start && start <= currentSlot.end) {
+                // We are contained within the slot
+                currentSlot.vCpuAvailable -= vCpuReservation
+                currentSlot.ramAvailable -= ramReservation
+
+                assert(currentSlot.vCpuAvailable >= 0)
+                assert(currentSlot.ramAvailable >= 0)
+
+                if (end < currentSlot.end) {
+                    // We are smaller than this slot, we need to split it
+                    val newSlot = currentSlot.copy(
+                        start = end,
+                        vCpuAvailable = currentSlot.vCpuAvailable + vCpuReservation,
+                        ramAvailable = currentSlot.ramAvailable + ramReservation,
+                        previousSlot = currentSlot,
+                        jobs = ArrayList(currentSlot.jobs)
+                    )
+                    currentSlot.nextSlot = newSlot
+                    currentSlot.end = end
+                    currentSlot.jobs.add(jobId)
+                }
+            }
+
+            if (end <= currentSlot.start) {
+                break // No need to search any further
+            }
+
+            currentSlot = currentSlot.nextSlot
+        }
+    }
+
+    fun releaseReservation(start: Long, end: Long, vCpuReservation: Int, ramReservation: Int) {
+        TODO("Do the reverse of addReservation but this time we should attempt to combine slots together")
+    }
+}
+
+
+data class Schedule(
+    val nodes: List<String>,
+    val startOfJob: Long,
+    val endOfJob: Long
+)
+
+data class JobToBeScheduled(
+    val timeRequired: SimpleDuration,
+    val nodesRequired: Int,
+    val vCpuReservation: Int,
+    val ramReservation: Int,
+    val currentState: ScheduledJobState = ScheduledJobState.AWAITING_SCHEDULE,
+    val constraints: SchedulingConstraints = SchedulingConstraints((1..7).toList(), 0, 24),
+    val schedule: Schedule? = null,
+    val id: String = "",
+)
+
+sealed class SchedulerEvent {
+    data class JobTerminated(val id: String) : SchedulerEvent()
+    data class NodeOffline(val id: String) : SchedulerEvent()
+    data class NodeOnline(val node: Node) : SchedulerEvent()
+}
+
+interface SchedulerCallbacks {
+    fun scheduleJob(jobId: String, nodeId: String)
+    fun terminateJob(jobId: String, nodeId: String)
+    fun notifyJobUpdate(job: JobToBeScheduled)
+}
+
+class Scheduler(
+    private val callbacks: SchedulerCallbacks,
+    private val scope: BackgroundScope,
+    private val numberOfSamples: Int = 5
+) {
+    private val nodes = HashMap<String, Node>()
+    private val nodeIds = ArrayList<String>()
+
+    private val jobQueue = Channel<JobToBeScheduled>()
+    private val eventQueue = Channel<SchedulerEvent>()
+
+    suspend fun addToQueue(job: JobToBeScheduled): String {
+        val newId = "${idPrefix}_${idCounter.getAndIncrement()}"
+        jobQueue.send(job.copy(id = newId))
+        return newId
+    }
+
+    suspend fun notifyEvent(event: SchedulerEvent) {
+        eventQueue.send(event)
+    }
+
+    fun start(): Job {
+        return scope.launch {
+            while (isActive) {
+                select<Unit> {
+                    jobQueue.onReceive { job ->
+                        schedule(job)
+                    }
+
+                    eventQueue.onReceive { ev ->
+                        when (ev) {
+                            is SchedulerEvent.JobTerminated -> {
+
+                            }
+
+                            is SchedulerEvent.NodeOffline -> {
+                                val node = nodes[ev.id] ?: run {
+                                    log.warn("Unknown node '${ev.id}' has gone offline")
+                                    return@onReceive
+                                }
+
+                                val currentJobs = node.freeList.findJobsAt(Time.now())
+                                for (jobId in currentJobs) {
+                                    // TODO Notify that job is dead
+                                }
+
+                                nodes.remove(ev.id)
+                                nodeIds.remove(ev.id)
+                            }
+
+                            is SchedulerEvent.NodeOnline -> {
+                                nodes[ev.node.id] = ev.node
+                                nodeIds.add(ev.node.id)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun schedule(job: JobToBeScheduled) {
+        coroutineScope {
+            while (true) {
+                val solutions = (0 until numberOfSamples).map {
+                    async {
+                        val startingPoint = nodes[nodeIds[Random.nextInt(nodes.size)]]!!
+                        val proposedTime = startingPoint.freeList.findFreeSlot(
+                            job.vCpuReservation,
+                            job.ramReservation,
+                            job.timeRequired.toMillis()
+                        ) ?: return@async null
+                        val end = proposedTime + job.timeRequired.toMillis()
+
+                        val nodesFound = ArrayList<String>()
+                        nodesFound.add(startingPoint.id)
+
+                        if (job.nodesRequired > 1) {
+                            val startNode = Random.nextInt(nodes.size)
+                            for (attempt in nodeIds.indices) {
+                                if (nodesFound.size == job.nodesRequired) break
+                                val node = nodes[nodeIds[(startNode + attempt) % nodeIds.size]]!!
+                                if (node.id == startingPoint.id) continue
+
+                                val hasSpace = node.freeList.hasSpaceAt(
+                                    proposedTime,
+                                    end,
+                                    job.vCpuReservation,
+                                    job.ramReservation
+                                )
+
+                                if (hasSpace) nodesFound.add(node.id)
+                            }
+                        }
+
+                        if (nodesFound.size == job.nodesRequired) Schedule(nodesFound, proposedTime, end)
+                        else null
+                    }
+                }.awaitAll()
+
+                val solution = solutions.minByOrNull { it?.startOfJob ?: Long.MAX_VALUE } ?: continue
+                solution.nodes.forEach { node ->
+                    nodes[node]!!.freeList.addReservation(
+                        solution.startOfJob,
+                        solution.endOfJob,
+                        job.vCpuReservation,
+                        job.ramReservation,
+                        job.id
+                    )
+                }
+
+                callbacks.notifyJobUpdate(
+                    job.copy(
+                        schedule = solution,
+                        currentState = ScheduledJobState.SCHEDULED
+                    )
+                )
+                break
+            }
+        }
+    }
+
+    private suspend fun reoderQueue() {
+
+    }
+
+    companion object : Loggable {
+        override val log = logger()
+        private val idCounter = AtomicLong(0)
+        private val idPrefix = UUID.randomUUID().toString()
+    }
+}
+
+fun main() {
+    val scope = BackgroundScope()
+    scope.init()
+    val scheduler = Scheduler(
+        object : SchedulerCallbacks {
+            val histogram = HashMap<Long, Int>()
+            override fun scheduleJob(jobId: String, nodeId: String) {
+                println("scheduleJob($jobId, $nodeId)")
+            }
+
+            override fun terminateJob(jobId: String, nodeId: String) {
+                println("terminateJob($jobId, $nodeId)")
+            }
+
+            override fun notifyJobUpdate(job: JobToBeScheduled) {
+                println(job.id)
+                println(job.schedule)
+                histogram[job.schedule!!.startOfJob] = (histogram[job.schedule.startOfJob] ?: 0) + 1
+                println(histogram.keys.sorted().joinToString(", ") { "$it = ${histogram[it]}"})
+            }
+        },
+        scope
+    )
+
+    val job = scheduler.start()
+    runBlocking {
+        repeat(100) {
+            scheduler.notifyEvent(
+                SchedulerEvent.NodeOnline(Node("n-$it", 30, 30))
+            )
+        }
+
+        // We can run 10 jobs in parallel, which means we should be able to fit this in 200 cycles (201_000 largest endOfJob)
+        repeat(2000) {
+            scheduler.addToQueue(
+                JobToBeScheduled(
+                    SimpleDuration(0, 0, 1),
+                    10,
+                    30,
+                    30,
+                )
+            )
+        }
+
+        println("Terminating in 5 seconds")
+        delay(5000)
+        println("Goodbye")
+    }
+    job.cancel()
+    scope.stop()
+}
---
+++ backend/app-kubernetes-service/wiki/scheduling.md
@@ -0,0 +1,66 @@
+# Scheduling
+
+## Issues to be Resolved
+
+1. No feedback given to users about when their job will run. This is crucial 
+for interactive jobs which probably accounts for the majority of our jobs.
+
+2. The handling of jobs which cannot run immediately is undefined
+
+3. Unable to run exclusively on a full node
+
+4. No guarantee that all 'nodes' of a job is scheduled together
+
+5. Java client for Kubernetes is _bad_. Jobs are not being closed when timer
+runs out.
+
+6. Handling of jobs on nodes which have restarted is undefined
+
+7. Partial failure of some nodes is undefined
+
+8. Scheduled downtime and maintenance
+
+## Solution Proposals
+
+### kube-batch
+
+Basically a dead project appears to be superseded by volcano-sh.
+
+### volcano-sh
+
+- Poorly documented
+- Unclear who funds this project
+- Very early stage project of CNCF
+- Fairly easy to install
+- Supports gang-scheduling and backfill
+- Should take care of the following points:
+  - Fixed: 2, 4
+  - Unclear but potentially fixed: 5, 6, 7
+
+### We do it
+
+- We could probably replace the buggy kubernetes client
+  - Example, watch pods in namespace:
+    `curl "https://rancher-dev.cloud.sdu.dk/k8s/clusters/c-xxxyyy/api/v1/namespaces/default/pods?watch" -u $dt -v`
+  - Where `$dt` is username + password (rancher token)
+  - Output will be one JSON event per line
+- Run pods directly on Kubernetes
+  - We manually specify the node
+  - We read node information from Kubernetes
+  - We manually terminate jobs if they run for too long
+- Pod creation by scheduler micro-service
+- Two queues, one for interactive one for batch
+  - TODO How to determine this?
+- Interactive queue
+  - First fit
+  - No reordering of jobs. This will allow us to concretely tell the user when
+  their interactive job will run.
+  - Issue: We have extremely unreliable estimates for how long the job will run
+  - Issue: Not reordering makes unreliable estimates very bad. Maybe we could
+  retrieve user input which states in which period the user wants the job to
+  be scheduled.
+- Batch queue
+  - Reordering of jobs should be on, apart from that first fit might still work
+  - Might need to add some kind of priority to this job
+  - At this point we would probably also want reservations and a whole bunch
+  of other features (which no one has implemented for K8s)
