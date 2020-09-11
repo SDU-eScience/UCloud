6aeb65cdc3d2235c4af047dffb10130a8d28285b
Dan Sebastian Thrane <dthrane@gmail.com>
2020-09-11 13:44:50 +0200

    Adds public ip index.ts

---
+++ frontend-web/webclient/app/Applications/IPAddresses/index.ts
@@ -0,0 +1,185 @@
+import {buildQueryString} from "Utilities/URIUtilities";
+
+export interface ApplyForAddressRequest {
+    application: string,
+}
+export interface ApplyForAddressResponse {
+    id: number,
+}
+export function applyForAddress(
+    request: ApplyForAddressRequest
+): APICallParameters<ApplyForAddressRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/apply",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface ApproveAddressRequest {
+    id: number,
+}
+export function approveAddress(
+    request: ApproveAddressRequest
+): APICallParameters<ApproveAddressRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/approve",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface RejectAddressRequest {
+    id: number,
+}
+export function rejectAddress(
+    request: RejectAddressRequest
+): APICallParameters<RejectAddressRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/reject",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface ReleaseAddressRequest {
+    id: number,
+}
+export function releaseAddress(
+    request: ReleaseAddressRequest
+): APICallParameters<ReleaseAddressRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/release",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface UpdatePortsRequest {
+    id: number,
+    newPortList: PortAndProtocol[],
+}
+export interface PortAndProtocol {
+    port: number,
+    protocol: InternetProtocol,
+}
+export enum InternetProtocol {
+    TCP = "TCP",
+    UDP = "UDP",
+}
+export function updatePorts(
+    request: UpdatePortsRequest
+): APICallParameters<UpdatePortsRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/update-ports",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface AddToPoolRequest {
+    addresses: string[],
+    exceptions: string[],
+}
+export function addToPool(
+    request: AddToPoolRequest
+): APICallParameters<AddToPoolRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/add-to-pool",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface RemoveFromPoolRequest {
+    addresses: string[],
+    exceptions: string[],
+}
+export function removeFromPool(
+    request: RemoveFromPoolRequest
+): APICallParameters<RemoveFromPoolRequest> {
+    return {
+        method: "POST",
+        path: "/hpc/ip/remove-from-pool",
+        parameters: request,
+        reloadId: Math.random(),
+        payload: request
+    };
+}
+export interface ListAssignedAddressesRequest {
+    itemsPerPage?: number,
+    page?: number,
+}
+export interface PublicIP {
+    entityType: WalletOwnerType,
+    id: number,
+    ipAddress: string,
+    openPorts: PortAndProtocol[],
+    ownerEntity: string,
+}
+export enum WalletOwnerType {
+    USER = "USER",
+    PROJECT = "PROJECT",
+}
+export interface PortAndProtocol {
+    port: number,
+    protocol: InternetProtocol,
+}
+export function listAssignedAddresses(
+    request: ListAssignedAddressesRequest
+): APICallParameters<ListAssignedAddressesRequest> {
+    return {
+        method: "GET",
+        path: buildQueryString("/hpc/ip/list-assigned", request),
+        parameters: request,
+        reloadId: Math.random(),
+        payload: undefined
+    };
+}
+export interface ListMyAddressesRequest {
+    itemsPerPage?: number,
+    page?: number,
+}
+export interface PublicIP {
+    entityType: WalletOwnerType,
+    id: number,
+    ipAddress: string,
+    openPorts: PortAndProtocol[],
+    ownerEntity: string,
+}
+export function listMyAddresses(
+    request: ListMyAddressesRequest
+): APICallParameters<ListMyAddressesRequest> {
+    return {
+        method: "GET",
+        path: buildQueryString("/hpc/ip/list", request),
+        parameters: request,
+        reloadId: Math.random(),
+        payload: undefined
+    };
+}
+export interface ListAddressApplicationsRequest {
+    itemsPerPage?: number,
+    page?: number,
+}
+export interface AddressApplication {
+    application: string,
+    id: number,
+}
+export function listAddressApplications(
+    request: ListAddressApplicationsRequest
+): APICallParameters<ListAddressApplicationsRequest> {
+    return {
+        method: "GET",
+        path: buildQueryString("/hpc/ip/list-applications", request),
+        parameters: request,
+        reloadId: Math.random(),
+        payload: undefined
+    };
+}
