74477632655f9d21ffe09683b760142328bc1472
Dan Sebastian Thrane <dthrane@gmail.com>
2020-09-24 15:31:15 +0200

    Decrease frequency of scans

--- backend/app-orchestrator-service/src/main/kotlin/app/orchestrator/services/JobMonitoringService.kt
+++ backend/app-orchestrator-service/src/main/kotlin/app/orchestrator/services/JobMonitoringService.kt
@@ -103,6 +103,6 @@ class JobMonitoringService(
 
     companion object : Loggable {
         override val log = logger()
-        private const val TIME_BETWEEN_SCANS = 10_000
+        private const val TIME_BETWEEN_SCANS = 1_000 * 30
     }
 }
