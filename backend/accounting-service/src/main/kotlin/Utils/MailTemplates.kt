178bbfe2e7a2a7c78b2573d523d74ee4efd71820
Claudio Pica <pica@cp3.sdu.dk>
2020-11-05 17:50:02 +0100

    Changed text of email notifications

--- backend/accounting-service/src/main/kotlin/Utils/MailTemplates.kt
+++ backend/accounting-service/src/main/kotlin/Utils/MailTemplates.kt
@@ -4,8 +4,8 @@ import dk.sdu.cloud.service.escapeHtml
 
 const val CREDITS_NOTIFY_LIMIT = 5000000
 const val LOW_FUNDS_SUBJECT = "Project low on resource"
-const val NO_MAIL_TEMPLATE = """<p>If you do not want to receive these notifications per mail, 
-    you can unsubscribe to non-crucial emails in your personal settings on UCloud</p>"""
+const val NO_MAIL_TEMPLATE = """<p>If you do not want to receive these email notifications, 
+    you can unsubscribe to non-critical emails in your personal settings on UCloud</p>"""
 
 fun stillLowResources (
     recipient: String,
@@ -15,9 +15,9 @@ fun stillLowResources (
 ) = """
     <p>Dear ${escapeHtml(recipient)}</p>
     <p>
-        We write to you to inform you that the project: ${escapeHtml(projectTitle)} still is low on the 
-        ${escapeHtml(catagory)} resource from ${escapeHtml(provider)} after allocation of new resources. <br>
-        If this is intentional, then ignore this message.
+        the project ${escapeHtml(projectTitle)} is still low on the ${escapeHtml(catagory)} resource 
+        from ${escapeHtml(provider)} after new resources were allocated. <br>
+        If this was intentional, please ignore this message.
     </p>
     $NO_MAIL_TEMPLATE
     """.trimIndent()
@@ -30,8 +30,9 @@ fun lowResourcesTemplate(
 ) = """
     <p>Dear ${escapeHtml(recipient)}</p>
     <p>
-        We write to you to inform you that the project: ${escapeHtml(projectTitle)} is running low on the 
-        ${escapeHtml(catagory)} resource from ${escapeHtml(provider)}.
+        the project ${escapeHtml(projectTitle)} is running low on the ${escapeHtml(catagory)} resource 
+        from ${escapeHtml(provider)}. <br>
+        If needed, you can request additional resources from the project's resource page.
     </p>
     $NO_MAIL_TEMPLATE
     """.trimIndent()
--- backend/grant-service/src/main/kotlin/grant/utils/Templates.kt
+++ backend/grant-service/src/main/kotlin/grant/utils/Templates.kt
@@ -7,14 +7,20 @@ import dk.sdu.cloud.service.escapeHtml
 fun newCommentTemplate(receiver: String, sender: String, projectTitle: String) =
     """
         <p>Dear ${escapeHtml(receiver)}</p>
-        <p>You have received a new comment from ${escapeHtml(sender)} in ${escapeHtml(projectTitle)}.</p>
+        <p>
+            You have received a new comment from ${escapeHtml(sender)} in an application to the project 
+            ${escapeHtml(projectTitle)}.
+        </p>
         $NO_NOTIFICATIONS_DISCLAIMER
     """.trimIndent()
 
 fun newIngoingApplicationTemplate(receiver: String, sender: String, projectTitle: String) =
     """
         <p>Dear ${escapeHtml(receiver)}</p>
-        <p>You have received a new application from ${escapeHtml(sender)} in ${escapeHtml(projectTitle)}.</p>
+        <p>
+            You have received a new application from ${escapeHtml(sender)} in the project 
+            ${escapeHtml(projectTitle)}.
+        </p>
         $NO_NOTIFICATIONS_DISCLAIMER
     """.trimIndent()
 
@@ -22,8 +28,8 @@ fun autoApproveTemplate(receiver: String, sender: String, projectTitle: String) 
     """
         <p>Dear ${escapeHtml(receiver)}</p>
         <p>
-            You have received a new application from ${escapeHtml(sender)} in ${escapeHtml(projectTitle)} 
-            which has been automatically approved by your project policy. 
+            You have received a new application from ${escapeHtml(sender)} in the project 
+            ${escapeHtml(projectTitle)}, which has been automatically approved by your project policy. 
         </p>
         $NO_NOTIFICATIONS_DISCLAIMER
     """.trimIndent()
@@ -45,31 +51,33 @@ fun responseTemplate(status: ApplicationStatus, receiver: String, sender: String
 private fun approved(projectTitle: String) =
     """
         <p>
-            We are happy to tell you that your application for resources from 
-            ${escapeHtml(projectTitle)} has been approved
+            We are glad to inform you that your application for resources from 
+            ${escapeHtml(projectTitle)} has been approved.
         </p>
     """.trimIndent()
 
 private fun rejected(projectTitle: String) =
     """
-        <p>We are sorry, but your application for resources from ${escapeHtml(projectTitle)} has been rejected</p>
+        <p>
+            We regret to inform you that your application for resources from ${escapeHtml(projectTitle)} 
+            has been rejected.
+        </p>
     """.trimIndent()
 
 private fun closed(projectTitle: String, sender: String) =
     """
-        <p>An application for ${escapeHtml(projectTitle)} has been closed by ${escapeHtml(sender)}</p>
+        <p>An application for ${escapeHtml(projectTitle)} has been withdrawn by ${escapeHtml(sender)}.</p>
     """.trimIndent()
 
 fun updatedTemplate(projectTitle: String, receiver: String, sender: String) =
     """
         <p>Dear ${escapeHtml(receiver)}</p>
         <p>
-            We write to inform you that your applications for resources in project ${escapeHtml(projectTitle)} 
-            have been changed by ${escapeHtml(sender)}. You might want to check the changes to see if the 
-            application still fit your needs.
+            your application for resources in project ${escapeHtml(projectTitle)} has been changed by 
+            ${escapeHtml(sender)}. Please review the changes to the application.
         </p>
         $NO_NOTIFICATIONS_DISCLAIMER
     """.trimIndent()
 
-const val NO_NOTIFICATIONS_DISCLAIMER = "<p>If you do not want to receive these notifications per mail, " +
-        "you can unsubscribe to non-crucial emails in your personal settings on UCloud</p>"
+const val NO_NOTIFICATIONS_DISCLAIMER = """<p>If you do not want to receive these email notifications, 
+    you can unsubscribe to non-critical emails in your personal settings on UCloud</p>"""
