9b16669a7ffa203d2382a45cee39deb24c38e936
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-11-13 12:47:59 +0100

    Fixes broken test

--- frontend-web/webclient/__tests__/SimpleSearch/SimpleSearch.test.tsx
+++ frontend-web/webclient/__tests__/SimpleSearch/SimpleSearch.test.tsx
@@ -1,34 +1,36 @@
 import {configure} from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
-import {createBrowserHistory} from "history";
 import * as React from "react";
 import {Provider} from "react-redux";
-import {MemoryRouter} from "react-router";
 import {create} from "react-test-renderer";
 import {ThemeProvider} from "styled-components";
 import Search from "../../app/Search/Search";
 import theme from "../../app/ui-components/theme";
 import {store} from "../../app/Utilities/ReduxUtilities";
+import {withRouter} from "react-router";
 
 configure({adapter: new Adapter()});
+
+import {createBrowserHistory} from "history";
+
+jest.mock("react-router", () => ({
+    useHistory: () => createBrowserHistory(),
+    useLocation: () => ({search: ""}),
+    useRouteMatch: () => ({
+        isExact: false,
+        params: {priority: "FILES"},
+        url: "",
+        path: ""
+    }),
+    withRouter: a => a
+}))
 
 test("Search mount", () => {
     expect(
         create(
             <Provider store={store}>
                 <ThemeProvider theme={theme}>
-                    <MemoryRouter>
-                        <Search
-                            match={{
-                                isExact: false,
-                                params: {priority: "FILES"},
-                                url: "",
-                                path: ""
-                            }}
-                            history={createBrowserHistory()}
-                            location={{search: ""}}
-                        />
-                    </MemoryRouter>
+                    <Search />
                 </ThemeProvider>
             </Provider>
         )
