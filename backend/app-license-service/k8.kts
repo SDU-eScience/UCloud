87de265e8e0cff9ebfb267d38b3d73f47e9e14c8
Dan Sebastian Thrane <dthrane@gmail.com>
2021-01-04 07:19:34 +0100

    Version bumps and release notes

--- backend/app-license-service/k8.kts
+++ backend/app-license-service/k8.kts
@@ -3,7 +3,7 @@ package dk.sdu.cloud.k8
 
 bundle {
     name = "app-license"
-    version = "0.2.4"
+    version = "0.2.5"
 
     withAmbassador("/api/app/license") {}
 
--- backend/elastic-management-service/k8.kts
+++ backend/elastic-management-service/k8.kts
@@ -3,7 +3,7 @@ package dk.sdu.cloud.k8
 
 bundle {
     name = "elastic-management"
-    version = "1.1.3"
+    version = "1.1.4"
 
     val deployment = withDeployment {
         injectSecret("elasticsearch-logging-cluster-credentials")
