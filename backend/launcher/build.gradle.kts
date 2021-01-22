ea9d97948ea82d9d9b5851ad3cb7fc88bf7244f7
Dan Sebastian Thrane <dthrane@gmail.com>
2021-01-22 09:09:56 +0100

    Another batch of documentation

--- backend/launcher/build.gradle.kts
+++ backend/launcher/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation(project(":service-common"))
     implementation("io.swagger.core.v3:swagger-models:2.1.5")
     implementation("io.swagger.core.v3:swagger-core:2.1.5")
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.4.0")
 
     rootProject.childProjects.values
         .filter { it.name.endsWith("-service") }
