36e28b17893b9c59b76a1e44b3e10941cefbb7ba
Dan Sebastian Thrane <dthrane@gmail.com>
2021-02-26 09:16:24 +0100

    Minor change to public ip ui

--- frontend-web/webclient/app/Applications/NetworkIP/Browse.tsx
+++ frontend-web/webclient/app/Applications/NetworkIP/Browse.tsx
@@ -178,7 +178,6 @@ export const Browse: React.FunctionComponent<{
                         <Text>
                             Public IP addresses allow you to exposes the services of your application via the public
                             Internet. You can apply to use a public IP when you apply for resources.
-                            <Link to={"/project/grants-landing"}><Button fullWidth>Request an IP address</Button></Link>
                         </Text>
                     </NoResultsCardBody>
                 </> : null
