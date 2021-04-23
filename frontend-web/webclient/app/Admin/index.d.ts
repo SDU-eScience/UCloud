6f47bc14e87f86a97a41c9f3197715e141de0502
Henrik Schulz <henrikschulz@hotmail.com>
2021-04-23 12:01:53 +0200

    Fixes #2376

--- frontend-web/webclient/app/Admin/index.d.ts
+++ frontend-web/webclient/app/Admin/index.d.ts
@@ -3,7 +3,11 @@ export interface UserCreationState {
     password: string
     repeatedPassword: string
     email: string
+    firstnames: string
+    lastname: string
     usernameError: boolean
     passwordError: boolean
     emailError: boolean
+    firstnamesError: boolean
+    lastnameError: boolean
 }
