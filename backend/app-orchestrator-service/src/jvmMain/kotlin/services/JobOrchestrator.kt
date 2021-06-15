4c183051e1ba169391c63456100895feb655fef4
Dan Sebastian Thrane <dthrane@gmail.com>
2021-06-15 14:46:13 +0200

    Log output has returned

--- backend/app-orchestrator-service/src/jvmMain/kotlin/services/JobOrchestrator.kt
+++ backend/app-orchestrator-service/src/jvmMain/kotlin/services/JobOrchestrator.kt
@@ -371,6 +371,8 @@ class JobOrchestrator(
                                 }
                             )
                         }
+
+                        delay(100)
                     }
                 }
 
