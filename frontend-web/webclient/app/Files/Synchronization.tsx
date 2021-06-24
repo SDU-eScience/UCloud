d3eec3b39d5c5922b987707aedafd953cbf22a96
Brian Alberg <brian@alberg.org>
2021-06-24 16:48:35 +0200

    Added front-end for synchronization

---
+++ frontend-web/webclient/app/Files/Synchronization.tsx
@@ -0,0 +1,165 @@
+import { useCloudAPI, useCloudCommand } from "Authentication/DataHook";
+import {snackbarStore} from "Snackbar/SnackbarStore";
+import React, { useEffect, useRef, useState } from "react";
+import { Box, Button, Checkbox, Flex, Icon, Input, Label, SelectableText, SelectableTextWrapper } from "ui-components";
+import List, { ListRow } from "ui-components/List";
+import { Text } from "ui-components";
+import {file, PageV2} from "UCloud";
+import synchronizationApi = file.orchestrator.synchronization;
+import orchestrator = file.orchestrator;
+import { emptyPageV2 } from "DefaultObjects";
+import { TextSpan } from "ui-components/Text";
+import { copyToClipboard } from "UtilityFunctions";
+import { pathComponents } from "Utilities/FileUtilities";
+
+const Tab: React.FunctionComponent<{ selected: boolean, onClick: () => void }> = props => {
+    return <SelectableText
+        mr="1em"
+        cursor="pointer"
+        selected={props.selected}
+        fontSize={3}
+        onClick={props.onClick}
+    >
+        {props.children}
+    </SelectableText>
+};
+
+
+export const SynchronizationSettings: React.FunctionComponent<{
+    path: string;
+}> = ({path}) => {
+    const provider = pathComponents(path)[0];
+
+    const [manageDevices, setManageDevices] = useState(false);
+    const [devices, fetchDevices] = useCloudAPI<PageV2<orchestrator.SynchronizationDevice>>(
+        synchronizationApi.browseDevices({ provider }),
+        emptyPageV2
+    );
+    const [folder, fetchFolder] = useCloudAPI<orchestrator.SynchronizedFolder|undefined>(
+        synchronizationApi.retrieveFolder({ path: path, provider }),
+        undefined
+    );
+
+
+    const deviceIdRef = useRef<HTMLInputElement>(null);
+    const [loading, invokeCommand] = useCloudCommand();
+    const [synchronizedFolder, setSynchronizedFolder] = useState<orchestrator.SynchronizedFolder|undefined>(undefined);
+    const [ucloudDeviceId, setUcloudDeviceId] = useState<string|undefined>(undefined);
+
+    const addDevice = async e => {
+        e.preventDefault();
+        if (deviceIdRef.current && deviceIdRef.current.value.length > 0) {
+            await invokeCommand(synchronizationApi.addDevice({ id: deviceIdRef.current.value, provider }));
+            fetchDevices(synchronizationApi.browseDevices({ provider }), true);
+            deviceIdRef.current.value = "";
+            snackbarStore.addSuccess("Added device", false);
+        } else {
+            snackbarStore.addFailure("Device ID cannot be empty", false);
+        }
+    };
+
+    const removeDevice = async (id: string) => {
+        await invokeCommand(synchronizationApi.removeDevice({ id, provider }));
+        fetchDevices(synchronizationApi.browseDevices({ provider }), true);
+        snackbarStore.addSuccess("Removed device", false);
+    };
+
+
+    useEffect(() => {
+        console.log(synchronizedFolder);
+        if (synchronizedFolder) {
+            setUcloudDeviceId(synchronizedFolder.device_id);
+        }
+    }, [synchronizedFolder]);
+
+    useEffect(() => {
+        if (folder.data) {
+            setSynchronizedFolder(folder.data);
+        }
+    }, [folder]);
+
+
+    const toggleSynchronizeFolder = async () => {
+        if (!synchronizedFolder) {
+            await invokeCommand(synchronizationApi.addFolder({ path, provider } ));
+            fetchFolder(synchronizationApi.retrieveFolder({ path, provider }), true);
+        } else {
+            if (folder.data) {
+                await invokeCommand(synchronizationApi.removeFolder({ id: folder.data.id, provider }));
+                setUcloudDeviceId(undefined);
+                setSynchronizedFolder(undefined);
+            }
+        }
+    };
+
+
+    return <>
+        <SelectableTextWrapper>
+            <Tab selected={!manageDevices} onClick={() => setManageDevices(false)}>Synchronize folder</Tab>
+            <Tab selected={manageDevices} onClick={() => setManageDevices(true)}>Manage devices</Tab>
+        </SelectableTextWrapper>
+        {manageDevices ? (
+            <>
+                <form onSubmit={addDevice}>
+                    <Flex mt={10} mb={10}>
+                        <Input ref={deviceIdRef} placeholder="Device ID" />
+                        <Button color="green" width="160px">Add device</Button>
+                    </Flex>
+                </form>
+                <List>
+                    {devices.data.items.map(d => {
+                        return (
+                            <ListRow
+                                key={d.id}
+                                left={
+                                    <>
+                                        {d.id}
+                                    </>
+                                } 
+                                right={
+                                    <>
+                                        <Button color="red" onClick={() => removeDevice(d.id)}>
+                                            <Icon name="trash" size="16px"/>
+                                        </Button>
+                                    </>
+                                }
+                            />
+                        )
+                    })}
+                </List>
+            </>
+        ) : (
+            <>
+                <Text mt="30px">To use the synchronization feature you have to set up a local instance of <a href="https://syncthing.net">Syncthing</a> on your device and add your device ID in Manage devices.</Text>
+                <Box mt="30px" mb="30px">
+                    <Label>
+                        <Checkbox
+                            checked={synchronizedFolder !== undefined}
+                            onChange={() => toggleSynchronizeFolder()}
+                        />
+                        <TextSpan fontSize={2}>Add {path} to synchronization</TextSpan>
+                    </Label>
+
+                </Box>
+
+                {ucloudDeviceId ? (
+                    <>
+                        <Text mb="20px">
+                            The folder is being synchronized with your devices from Device ID:
+                        </Text>
+                        <Flex>
+                        <Input readOnly={true} value={ucloudDeviceId} />
+                        <Button onClick={() => copyToClipboard({
+                            value: ucloudDeviceId,
+                            message: "Copied " + ucloudDeviceId + " to clipboard"
+                        })}>Copy</Button>
+                        </Flex>
+                        <Text mt="20px">
+                            Add this as a remote device to your local instance of Syncthing to begin synchronization.
+                        </Text>
+                    </>
+                ) : (<></>)}
+            </>
+        )}
+    </>
+};
