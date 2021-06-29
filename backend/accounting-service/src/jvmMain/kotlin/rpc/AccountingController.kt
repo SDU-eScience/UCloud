be0d68e43bf022762778286a9e8759b145d6b978
Henrik Schulz <henrikschulz@hotmail.com>
2021-06-29 10:31:24 +0200

    merge

--- backend/accounting-service/src/jvmMain/kotlin/rpc/AccountingController.kt
+++ backend/accounting-service/src/jvmMain/kotlin/rpc/AccountingController.kt
@@ -1,15 +1,10 @@
 package dk.sdu.cloud.accounting.rpc
 
-import dk.sdu.cloud.Actor
 import dk.sdu.cloud.accounting.api.*
 import dk.sdu.cloud.accounting.services.BalanceService
 import dk.sdu.cloud.calls.server.RpcServer
-import dk.sdu.cloud.calls.server.project
-import dk.sdu.cloud.calls.server.securityPrincipal
 import dk.sdu.cloud.service.Controller
 import dk.sdu.cloud.service.db.async.DBContext
-import dk.sdu.cloud.service.db.async.withSession
-import dk.sdu.cloud.toActor
 
 class AccountingController(
     private val db: DBContext,
@@ -17,7 +12,6 @@ class AccountingController(
 ) : Controller {
     override fun configure(rpcServer: RpcServer) = with(rpcServer) {
         implement(Accounting.charge) {
-
         }
 
         implement(Accounting.deposit) {
