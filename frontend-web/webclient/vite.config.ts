1399b72bd0e7b98491d96b9c1335f42823f77f8c
Jonas Malte Hinchely <jonas@hinchely.dk>
2021-07-02 12:32:08 +0200

    Move index file. Add plugin

--- frontend-web/webclient/vite.config.ts
+++ frontend-web/webclient/vite.config.ts
@@ -1,10 +1,8 @@
-import {defineConfig} from 'vite'
-import path from "path"
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import {defineConfig} from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
-    root: "./app",
-    resolve: {alias: [{find: '@', replacement: path.resolve('app')}]},
+    plugins: [reactRefresh(), tsconfigPaths()]
 })
