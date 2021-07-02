9d5497896a04a675b8386eb87005943a8e58522d
Jonas Malte Hinchely <jonas@hinchely.dk>
2021-07-02 11:08:52 +0200

    Add vite

---
+++ frontend-web/webclient/vite.config.ts
@@ -0,0 +1,10 @@
+import {defineConfig} from 'vite'
+import path from "path"
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [reactRefresh()],
+    root: "./app",
+    resolve: {alias: [{find: '@', replacement: path.resolve('app')}]},
+})
