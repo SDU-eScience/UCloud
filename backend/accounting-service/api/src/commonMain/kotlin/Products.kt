1811eefa24bc7e28c58f29f2b91ee251d26aad9a
Dan Sebastian Thrane <dthrane@gmail.com>
2021-10-08 12:21:32 +0200

    Product documentation

--- backend/accounting-service/api/src/commonMain/kotlin/Products.kt
+++ backend/accounting-service/api/src/commonMain/kotlin/Products.kt
@@ -123,68 +123,14 @@ sealed class PaymentModel {
 
 @UCloudApiOwnedBy(Products::class)
 enum class ProductPriceUnit {
-    @UCloudApiDoc("""
-        Used for resources which either: are charged once for the entire life-time (`ChargeType.ABSOLUTE`) or
-        used to enforce a quota (`ChargeType.DIFFERENTIAL_QUOTA`).
-        
-        When used in combination with `ChargeType.ABSOLUTE` then this is typically used for resources such as:
-        licenses, public IPs and links.
-        
-        When used in combination with `ChargeType.DIFFERENTIAL_QUOTA` it is used to enforce a quota. At any point in
-        time, a user should never be allowed to use more units than specified in their current `balance`. For example,
-        if `balance = 100` and `ProductType = ProductType.COMPUTE` then no more than 100 jobs should be allowed to run
-        at any given point in time. Similarly, this can be used to enforce a storage quota.
-    """)
     PER_UNIT,
 
-    @UCloudApiDoc("""
-        Used for resources which are charged periodically in a pre-defined currency.
-        
-        This `ProductPriceUnit` can only be used in combination with `ChargeType.ABSOLUTE`.
-        
-        The pre-defined currency is decided between the UCloud/Core and the provider out-of-band. UCloud only supports
-        a single currency for the entire system.
-        
-        The accounting system only stores balances as an integer, for precision reasons. As a result, a charge using
-        `CREDITS_PER_X` will always refer to one-millionth of the currency (for example: 1 Credit = 0.000001 DKK).
-        
-        This price unit comes in several variants: `MINUTE`, `HOUR`, `DAY`. This period is used to define the `units`
-        of a `charge`. For example, if a `charge` is made on a product with `CREDITS_PER_MINUTE` then the `units`
-        property refer to the number of minutes which have elapsed. The provider is not required to perform `charge`
-        operations this often, it only serves to define the true meaning of `units` in the `charge` operation.
-        
-        The period used SHOULD always refer to monotonically increasing time. In practice, this means that a user should
-        not be charged differently because of summer/winter time.
-    """)
     CREDITS_PER_MINUTE,
-    @UCloudApiDoc("See `CREDITS_PER_MINUTE`")
     CREDITS_PER_HOUR,
-    @UCloudApiDoc("See `CREDITS_PER_MINUTE`")
     CREDITS_PER_DAY,
 
-    @UCloudApiDoc("""
-        Used for resources which are charged periodically.
-        
-        This `ProductPriceUnit` can only be used in combination with `ChargeType.ABSOLUTE`.
-        
-        All allocations granted to a product of `UNITS_PER_X` specify the amount of units of the recipient can use.
-        Some examples include:
-        
-          - Core hours
-          - Public IP hours
-          
-        This price unit comes in several variants: `MINUTE`, `HOUR`, `DAY`. This period is used to define the `units`
-        of a `charge`. For example, if a `charge` is made on a product with `CREDITS_PER_MINUTE` then the `units`
-        property refer to the number of minutes which have elapsed. The provider is not required to perform `charge`
-        operations this often, it only serves to define the true meaning of `units` in the `charge` operation.
-        
-        The period used SHOULD always refer to monotonically increasing time. In practice, this means that a user should
-        not be charged differently because of summer/winter time.
-    """)
     UNITS_PER_MINUTE,
-    @UCloudApiDoc("See `UNITS_PER_MINUTE`")
     UNITS_PER_HOUR,
-    @UCloudApiDoc("See `UNITS_PER_MINUTE`")
     UNITS_PER_DAY,
 }
 
