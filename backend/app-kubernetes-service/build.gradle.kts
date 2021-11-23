570b8cfed31bcdeda6788873eb14dc596059f061
Dan Sebastian Thrane <dthrane@gmail.com>
2021-11-23 13:49:44 +0100

    Bumping some versions

--- backend/app-kubernetes-service/build.gradle.kts
+++ backend/app-kubernetes-service/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "2021.3.0-alpha24"
+version = "2021.3.0-alpha25"
 
 application {
     mainClassName = "dk.sdu.cloud.app.kubernetes.MainKt"
--- backend/file-orchestrator-service/k8.kts
+++ backend/file-orchestrator-service/k8.kts
@@ -3,7 +3,7 @@ package dk.sdu.cloud.k8
 
 bundle {
     name = "file-orchestrator"
-    version = "2021.3.0-alpha7"
+    version = "2021.3.0-alpha8"
     
     withAmbassador(null) {
         addSimpleMapping("/api/files")
