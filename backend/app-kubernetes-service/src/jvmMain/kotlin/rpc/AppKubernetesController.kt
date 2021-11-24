cc7a96af39656ddfa6ab31264b6370b1ddf44239
Dan Sebastian Thrane <dthrane@gmail.com>
2021-11-24 08:20:24 +0100

    Fixes #2958

--- backend/app-kubernetes-service/src/jvmMain/kotlin/rpc/AppKubernetesController.kt
+++ backend/app-kubernetes-service/src/jvmMain/kotlin/rpc/AppKubernetesController.kt
@@ -1,6 +1,7 @@
 package dk.sdu.cloud.app.kubernetes.rpc
 
 import dk.sdu.cloud.app.kubernetes.api.KubernetesCompute
+import dk.sdu.cloud.app.kubernetes.api.KubernetesNetworkIP
 import dk.sdu.cloud.app.kubernetes.services.JobAndRank
 import dk.sdu.cloud.app.kubernetes.services.JobManagement
 import dk.sdu.cloud.app.kubernetes.services.K8LogService
@@ -92,6 +93,10 @@ class AppKubernetesController(
 
         implement(KubernetesCompute.retrieveProducts) {
             ok(jobManagement.retrieveProductsTemporary())
+        }
+
+        implement(KubernetesCompute.updateAcl) {
+            ok(BulkResponse(request.items.map {  }))
         }
 
         implement(KubernetesCompute.follow) {
