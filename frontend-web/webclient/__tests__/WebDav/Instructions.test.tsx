88c58166ddb50fd07f92ce01887b9af90c4a8570
Dan Sebastian Thrane <dthrane@gmail.com>
2021-12-09 14:31:04 +0100

    Delete broken tests

--- frontend-web/webclient/__tests__/WebDav/Instructions.test.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import {Instructions} from "../../app/WebDav/Instructions";
-import {create} from "react-test-renderer";
-
-test("Mount instructions", () => {
-    expect(create(<Instructions token="foo" />)).toMatchSnapshot();
-});
