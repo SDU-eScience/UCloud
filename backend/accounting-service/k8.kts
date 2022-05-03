c949f41d7963b071cb1d6b4d9e90335e01fc37e5
Dan Sebastian Thrane <dthrane@gmail.com>
2022-05-03 15:48:11 +0200

    Version bumps and renames some files to fix migrations

--- backend/accounting-service/k8.kts
+++ backend/accounting-service/k8.kts
@@ -3,7 +3,7 @@ package dk.sdu.cloud.k8
 
 bundle {
     name = "accounting"
-    version = "2022.1.45"
+    version = "2022.1.48-devel-hippo"
 
     withAmbassador("/api/accounting") {
         addSimpleMapping("/api/products")
