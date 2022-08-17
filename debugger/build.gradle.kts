8fab64a39eaca09acb6f2e7b094f58b21f966efd
Dan Sebastian Thrane <dthrane@gmail.com>
2022-08-17 10:01:29 +0200

    Bumps some version numbers in the debugger

--- debugger/build.gradle.kts
+++ debugger/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("multiplatform") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
     application
 }
 
