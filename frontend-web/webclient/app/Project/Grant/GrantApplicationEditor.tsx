cd2aace2996057ee24b042f18f344340d5cc499a
Jonas Malte Hinchely <jonas@hinchely.dk>
2022-09-27 14:17:13 +0200

    Add file.

--- frontend-web/webclient/app/Project/Grant/GrantApplicationEditor.tsx
+++ frontend-web/webclient/app/Project/Grant/GrantApplicationEditor.tsx
@@ -1370,29 +1370,29 @@ export const GrantApplicationEditor: (target: RequestTarget) =>
                     {grantApplication.status.revisions.length === 0 ? null : <Accordion title="Revisions">
                         <Table>
                             <TableHeader>
-                                <TableHeaderCell>
+                                <TableHeaderCell width="200px">
+                                    Updated at
+                                </TableHeaderCell>
+                                <TableHeaderCell width="150px">
                                     Updated by
                                 </TableHeaderCell>
                                 <TableHeaderCell>
                                     Revision comment
                                 </TableHeaderCell>
-                                <TableHeaderCell>
-                                    Updated at
-                                </TableHeaderCell>
                             </TableHeader>
                             <tbody>
                                 {grantApplication.status.revisions.map(rev =>
                                     <TableRow key={rev.revisionNumber}>
-                                        <TableCell>
-                                            {rev.revisionNumber} {rev.updatedBy}
+                                        <TableCell textAlign="center">
+                                            {format(rev.createdAt, "dd/MM/yyyy HH:mm:ss")}
+                                        </TableCell>
+                                        <TableCell textAlign="center">
+                                            {rev.updatedBy}
                                         </TableCell>
                                         <TableCell>
                                             <pre style={{overflowX: "scroll"}}>
                                                 {rev.document.revisionComment}
                                             </pre>
-                                        </TableCell>
-                                        <TableCell>
-                                            {format(rev.createdAt, "dd/MM/yyyy HH:mm:ss")}
                                         </TableCell>
                                     </TableRow>
                                 )}
