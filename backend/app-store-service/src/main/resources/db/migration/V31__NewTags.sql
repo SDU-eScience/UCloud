e932a9c197c2206be07ed360e45b94c11b2fd543
xiroV <brian@xirov.com>
2022-10-13 14:19:39 +0200

    Added migration script for new tag system

---
+++ backend/app-store-service/src/main/resources/db/migration/V31__NewTags.sql
@@ -0,0 +1,26 @@
+create table app_store.tags(
+    id serial primary key,
+    tag text not null
+);
+
+create unique index tag_unique on app_store.tags (lower(tag));
+
+insert into app_store.tags (id, tag) values (0, 'temporary');
+
+insert into app_store.tags (tag)
+    select distinct tag from app_store.application_tags
+    on conflict do nothing
+;
+
+
+alter table app_store.application_tags
+    add column tag_id int references app_store.tags(id) not null default 0;
+
+update app_store.application_tags set tag_id = (
+    select id from app_store.tags where lower(tag) = lower(application_tags.tag) limit 1
+);
+
+delete from app_store.tags where id = 0;
+
+alter table app_store.application_tags
+    drop column tag;
