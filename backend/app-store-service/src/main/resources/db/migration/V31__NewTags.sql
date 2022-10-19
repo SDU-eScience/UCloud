f77d8dd0f8ef4d40e548bf8b0ded5c9aadecc3d5
xiroV <brian@xirov.com>
2022-10-19 15:20:24 +0200

    Manual sorting in migration script, added rows and columns

--- backend/app-store-service/src/main/resources/db/migration/V31__NewTags.sql
+++ backend/app-store-service/src/main/resources/db/migration/V31__NewTags.sql
@@ -12,7 +12,6 @@ insert into app_store.tags (tag)
     on conflict do nothing
 ;
 
-
 alter table app_store.application_tags
     add column tag_id int references app_store.tags(id) not null default 0;
 
@@ -35,7 +34,9 @@ alter table app_store.application_tags drop column id;
 create table app_store.overview (
     reference_type text not null,
     reference_id text not null,
-    order_id int not null
+    order_id int not null,
+    rows int default 1,
+    columns int default 7
 );
 
 create sequence app_store.overview_order_sequence start 1 increment 1;
@@ -54,6 +55,44 @@ insert into app_store.overview (reference_type, reference_id, order_id)
         'Health Science',
         'Bioinformatics'
     );
+
+update app_store.overview set rows = 3 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Featured');
+
+update app_store.tags set tag = 'Development' where tag = 'development';
+
+update app_store.overview set order_id = 1 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Featured');
+
+update app_store.overview set order_id = 2 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Engineering');
+
+update app_store.overview set order_id = 3 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Data Analytics');
+
+update app_store.overview set order_id = 4 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Social Science');
+
+update app_store.overview set order_id = 5 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Applied Science');
+
+update app_store.overview set order_id = 6 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Natural Science');
+
+update app_store.overview set order_id = 7 where reference_type = 'TAG' and
+    cast(reference_id as int) = (select id from tags where tag = 'Development');
+
+update app_store.overview set order_id = 8 where reference_type = 'TAG' and
+        cast(reference_id as int) = (select id from tags where tag = 'Virtual Machines');
+
+update app_store.overview set order_id = 9 where reference_type = 'TAG' and
+        cast(reference_id as int) = (select id from tags where tag = 'Digital Humanities');
+
+update app_store.overview set order_id = 10 where reference_type = 'TAG' and
+        cast(reference_id as int) = (select id from tags where tag = 'Health Science');
+
+update app_store.overview set order_id = 11 where reference_type = 'TAG' and
+        cast(reference_id as int) = (select id from tags where tag = 'Bioinformatics');
 
 insert into app_store.overview (reference_type, reference_id, order_id) values
     ('TOOL', 'BEDTools', nextval('overview_order_sequence')),
