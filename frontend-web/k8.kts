98d4241f812ccb392713d514f6b685216a3934ff
Dan Sebastian Thrane <dthrane@gmail.com>
2022-10-25 16:02:46 +0200

    Tweaks for provider overview

--- frontend-web/k8.kts
+++ frontend-web/k8.kts
@@ -3,7 +3,7 @@ package dk.sdu.cloud.k8
 
 bundle {
     name = "webclient"
-    version = "2022.2.31"
+    version = "2022.2.32"
 
     withAmbassador(null) {
         addSimpleMapping("/api/auth-callback")
