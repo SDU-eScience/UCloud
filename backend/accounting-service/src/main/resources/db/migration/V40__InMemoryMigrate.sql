30acad449ef61c534a31014d1cdb0f555fea3e43
Dan Sebastian Thrane <dthrane@gmail.com>
2022-12-14 10:11:07 +0100

    Remove destructive part of devel-accounting migration

--- backend/accounting-service/src/main/resources/db/migration/V40__InMemoryMigrate.sql
+++ backend/accounting-service/src/main/resources/db/migration/V40__InMemoryMigrate.sql
@@ -1,40 +1,40 @@
 -- Moved to internal
-drop function if exists "grant".approve_application(
-    approved_by text,
-    application_id_in bigint
-);
-
-drop function if exists accounting.charge(
-    requests accounting.charge_request[]
-);
-
-drop function if exists accounting.process_charge_requests(
-    requests accounting.charge_request[]
-);
-
-drop function if exists accounting.transfer(
-    requests accounting.transfer_request[]
-);
-
-drop function if exists accounting.process_transfer_requests(
-    requests accounting.transfer_request[]
-);
-
-drop function if exists accounting.credit_check(
-    requests accounting.charge_request[]
-);
-
-drop function if exists accounting.credit_check(
-    requests accounting.transfer_request[]
-);
-
-drop function if exists accounting.deposit(
-    requests accounting.deposit_request[]
-);
-
-drop function if exists accounting.update_allocations(
-    request accounting.allocation_update_request[]
-);
+-- drop function if exists "grant".approve_application(
+--     approved_by text,
+--     application_id_in bigint
+-- );
+--
+-- drop function if exists accounting.charge(
+--     requests accounting.charge_request[]
+-- );
+--
+-- drop function if exists accounting.process_charge_requests(
+--     requests accounting.charge_request[]
+-- );
+--
+-- drop function if exists accounting.transfer(
+--     requests accounting.transfer_request[]
+-- );
+--
+-- drop function if exists accounting.process_transfer_requests(
+--     requests accounting.transfer_request[]
+-- );
+--
+-- drop function if exists accounting.credit_check(
+--     requests accounting.charge_request[]
+-- );
+--
+-- drop function if exists accounting.credit_check(
+--     requests accounting.transfer_request[]
+-- );
+--
+-- drop function if exists accounting.deposit(
+--     requests accounting.deposit_request[]
+-- );
+--
+-- drop function if exists accounting.update_allocations(
+--     request accounting.allocation_update_request[]
+-- );
 
 alter table "grant".applications add column synchronized boolean default false;
 
