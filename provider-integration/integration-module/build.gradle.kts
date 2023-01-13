42edac34410548fa3c92bb5cd66af7edb13eb8cd
Dan Sebastian Thrane <dthrane@gmail.com>
2023-01-13 10:36:16 +0100

    Version bump for syncthing changes

--- provider-integration/integration-module/build.gradle.kts
+++ provider-integration/integration-module/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     run {
-        val version = "2022.2.70"
+        val version = "2022.3.1-sync0"
         fun ucloud(module: String) = implementation("dk.sdu.cloud:$module:$version")
 
         ucloud("file-orchestrator-service-api")
