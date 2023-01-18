18dc5691a5e8a89fdbad4c8540846880dd053ce4
Dan Sebastian Thrane <dthrane@gmail.com>
2023-01-18 14:25:12 +0100

    Version update

--- provider-integration/integration-module/build.gradle.kts
+++ provider-integration/integration-module/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     run {
-        val version = "2022.3.3"
+        val version = "2022.3.6"
         fun ucloud(module: String) = implementation("dk.sdu.cloud:$module:$version")
 
         ucloud("file-orchestrator-service-api")
