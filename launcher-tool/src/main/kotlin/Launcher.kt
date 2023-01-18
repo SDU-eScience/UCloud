6a7b7216602945c70d323565291870aff2aef79b
Dan Sebastian Thrane <dthrane@gmail.com>
2023-01-18 13:44:41 +0100

    Attempt to use headless for LoadingIndicator

--- launcher-tool/src/main/kotlin/Launcher.kt
+++ launcher-tool/src/main/kotlin/Launcher.kt
@@ -32,6 +32,8 @@ fun cliHint(invocation: String) {
         .bold().render("./launcher $invocation").boldOff().render("'"))
 }
 
+var isHeadless: Boolean = false
+
 fun main(args: Array<String>) {
     try {
         val postExecPath = args.getOrNull(0) ?: error("Bad invocation")
@@ -68,6 +70,7 @@ fun main(args: Array<String>) {
         fileFactory = LocalFileFactory()
 
         val shouldInitializeTestEnvironment = args.contains("init") && args.contains("--all-providers")
+        isHeadless = !shouldInitializeTestEnvironment
 
         // NOTE(Dan): initCurrentEnvironment() now initializes an environment which is ready. It returns true if the
         // environment is new. This method will override several "environment" global variables, such as the
