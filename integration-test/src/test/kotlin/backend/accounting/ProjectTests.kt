b39f0568f723ff0ddb2c5606c626f8ff0fb7ee93
xiroV <brian@xirov.com>
2023-02-06 15:43:28 +0100

    Updated tests for project invite links

--- integration-test/src/test/kotlin/backend/accounting/ProjectTests.kt
+++ integration-test/src/test/kotlin/backend/accounting/ProjectTests.kt
@@ -6,158 +6,120 @@ import dk.sdu.cloud.integration.IntegrationTest
 import dk.sdu.cloud.integration.adminClient
 import dk.sdu.cloud.integration.utils.*
 import dk.sdu.cloud.project.api.v2.*
+import kotlin.test.assertFails
 import kotlin.test.assertNotEquals
 
 class ProjectTests : IntegrationTest() {
+
+    // Note(Brian): Utility function to get the client of a user with the specified role within the project.
+    //  - If role is null, a client for a user which isn't a member of the project is returned,
+    //  - if role is PI, a client for the pi of the project is returned,
+    //  - otherwise a client for a new user with the specified project role is returned.
+    suspend fun clientWithProjectRole(project: NormalProjectInitialization, role: ProjectRole?): AuthenticatedClient {
+        return when (role) {
+            null -> {
+                createUser().client
+            }
+
+            ProjectRole.USER -> {
+                val (userClient, username) = createUser()
+                addMemberToProject(
+                    project.projectId,
+                    project.piClient,
+                    userClient,
+                    username,
+                    ProjectRole.USER
+                )
+                userClient
+            }
+
+            ProjectRole.ADMIN -> {
+                val (userClient, username) = createUser()
+                addMemberToProject(
+                    project.projectId,
+                    project.piClient,
+                    userClient,
+                    username,
+                    ProjectRole.ADMIN
+                )
+                userClient
+            }
+
+            else -> {
+                project.piClient
+            }
+        }
+    }
+
+
     override fun defineTests() {
         run {
-            class In()
+            class In(
+                val requestFromRole: ProjectRole?
+            )
 
             class Out(
                 val createStatus: HttpStatusCode,
-                val browseStatus: HttpStatusCode,
                 val browseResponse: List<ProjectInviteLink>
             )
 
-            test<In, Out>("Project invite link create and browse (PI)") {
+            test<In, Out>("Project invite link create and browse") {
                 execute {
                     val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
                     val project = initializeNormalProject(root)
 
+                    val client = clientWithProjectRole(project, input.requestFromRole)
+
                     val create = Projects.createInviteLink.call(
                         Unit,
-                        project.piClient.withProject(project.projectId)
+                        client.withProject(project.projectId)
                     ).statusCode
 
                     val browse = Projects.browseInviteLinks.call(
                         ProjectsBrowseInviteLinksRequest(),
-                        project.piClient.withProject(project.projectId)
-                    )
+                        client.withProject(project.projectId)
+                    ).orThrow()
 
                     Out(
                         create,
-                        browse.statusCode,
-                        browse.orThrow().items
+                        browse.items
                     )
                 }
 
-                case("PI create and browse") {
-                    input(In())
+                case("by PI (success)") {
+                    input(In(ProjectRole.PI))
                     check {
                         assert(output.createStatus == HttpStatusCode.OK)
-                        assert(output.browseStatus == HttpStatusCode.OK)
                         assert(output.browseResponse.size == 1)
                     }
                 }
-            }
 
-            test<In, Out>("Project invite link create and browse (Admin)") {
-                execute {
-                    val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
-                    val project = initializeNormalProject(root)
-
-                    val (projectAdminClient, projectAdminUsername) = createUser()
-                    addMemberToProject(project.projectId, project.piClient, projectAdminClient, projectAdminUsername, ProjectRole.ADMIN)
-
-                    val create = Projects.createInviteLink.call(
-                        Unit,
-                        projectAdminClient.withProject(project.projectId)
-                    ).statusCode
-
-                    val browse = Projects.browseInviteLinks.call(
-                        ProjectsBrowseInviteLinksRequest(),
-                        projectAdminClient.withProject(project.projectId)
-                    )
-
-                    Out(
-                        create,
-                        browse.statusCode,
-                        browse.orThrow().items
-                    )
-                }
-
-                case("Admin create and browse") {
-                    input(In())
+                case("by project admin (success)") {
+                    input(In(ProjectRole.ADMIN))
                     check {
                         assert(output.createStatus == HttpStatusCode.OK)
-                        assert(output.browseStatus == HttpStatusCode.OK)
                         assert(output.browseResponse.size == 1)
                     }
                 }
-            }
 
-            test<In, Out>("Project invite link create and browse failure (Member)") {
-                execute {
-                    val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
-                    val project = initializeNormalProject(root)
-
-                    val (memberClient, memberUsername) = createUser()
-                    addMemberToProject(project.projectId, project.piClient, memberClient, memberUsername)
-
-                    val create = Projects.createInviteLink.call(
-                        Unit,
-                        memberClient.withProject(project.projectId)
-                    ).statusCode
-
-                    val browse = Projects.browseInviteLinks.call(
-                        ProjectsBrowseInviteLinksRequest(),
-                        memberClient.withProject(project.projectId)
-                    )
-
-                    Out(
-                        create,
-                        browse.statusCode,
-                        emptyList()
-                    )
+                case("by project member (failure)") {
+                    input(In(ProjectRole.USER))
+                    expectFailure()
                 }
 
-                case("Member create and browse fail") {
-                    input(In())
-                    check {
-                        assertNotEquals(output.createStatus, HttpStatusCode.OK)
-                        assertNotEquals(output.browseStatus, HttpStatusCode.OK)
-                    }
-                }
-            }
-
-            test<In, Out>("Project invite link create and browse failure (Other user)") {
-               execute {
-                   val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
-                   val project = initializeNormalProject(root)
-
-                   val (otherUserClient, _) = createUser()
-
-                   val create = Projects.createInviteLink.call(
-                       Unit,
-                       otherUserClient.withProject(project.projectId)
-                   ).statusCode
-
-                   val browse = Projects.browseInviteLinks.call(
-                       ProjectsBrowseInviteLinksRequest(),
-                       otherUserClient.withProject(project.projectId)
-                   )
-
-                   Out(
-                       create,
-                       browse.statusCode,
-                       emptyList()
-                   )
-               }
-
-                case("Other user create and browse fail") {
-                    input(In())
-                    check {
-                        assertNotEquals(output.createStatus, HttpStatusCode.OK)
-                        assertNotEquals(output.browseStatus, HttpStatusCode.OK)
-                    }
+                case("by other user (failure)") {
+                    input(In(null))
+                    expectFailure()
                 }
             }
         }
 
         run {
             class In(
-                val role: ProjectRole
+                val requestFromRole: ProjectRole?,
+                val updateAssignmentRole: ProjectRole
             )
+
             class Out(
                 val token: String,
                 val browse: List<ProjectInviteLink>
@@ -168,19 +130,21 @@ class ProjectTests : IntegrationTest() {
                     val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
                     val project = initializeNormalProject(root)
 
+                    val client = clientWithProjectRole(project, input.requestFromRole)
+
                     val token = Projects.createInviteLink.call(
                         Unit,
-                        project.piClient.withProject(project.projectId)
+                        client.withProject(project.projectId)
                     ).orThrow().token
 
                     Projects.updateInviteLinkRoleAssignment.call(
-                        ProjectsUpdateInviteLinkRoleAssignmentRequest(token, input.role),
-                        project.piClient.withProject(project.projectId)
+                        ProjectsUpdateInviteLinkRoleAssignmentRequest(token, input.updateAssignmentRole),
+                        client.withProject(project.projectId)
                     )
 
                     val browse = Projects.browseInviteLinks.call(
                         ProjectsBrowseInviteLinksRequest(),
-                        project.piClient.withProject(project.projectId)
+                        client.withProject(project.projectId)
                     ).orThrow()
 
                     Out(
@@ -189,10 +153,145 @@ class ProjectTests : IntegrationTest() {
                     )
                 }
 
-                case("successful role assignment") {
+                case("by PI (success)") {
+                    input(In(ProjectRole.PI, ProjectRole.ADMIN))
+                    check {
+                        assert(output.browse.first { it.token == output.token }.roleAssignment == input.updateAssignmentRole)
+                    }
+                }
+
+                case("by project admin (success)") {
+                    input(In(ProjectRole.ADMIN, ProjectRole.ADMIN))
+                    check {
+                        assert(output.browse.first { it.token == output.token }.roleAssignment == input.updateAssignmentRole)
+                    }
+                }
+
+                case("by project member (failure)") {
+                    input(In(ProjectRole.USER, ProjectRole.ADMIN))
+                    expectFailure()
+                }
+
+                case("by other user (failure)") {
+                    input(In(null, ProjectRole.ADMIN))
+                    expectFailure()
+                }
+            }
+        }
+        run {
+            class In(
+                val requestFromRole: ProjectRole?,
+            )
+
+            class Out(
+                val token: String,
+                val browse: List<ProjectInviteLink>
+            )
+
+            test<In, Out>("Project invite link update group assignment") {
+                execute {
+                    val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
+                    val project = initializeNormalProject(root)
+
+                    val group1 = createGroup(project)
+                    val group2 = createGroup(project)
+
+                    val client = clientWithProjectRole(project, input.requestFromRole)
+
+                    val token = Projects.createInviteLink.call(
+                        Unit,
+                        client.withProject(project.projectId)
+                    ).orThrow().token
+
+                    Projects.updateInviteLinkGroupAssignment.call(
+                        ProjectsUpdateInviteLinkGroupAssignmentRequest(token, listOf(group1.groupId, group2.groupId)),
+                        client.withProject(project.projectId)
+                    )
+
+                    val browse = Projects.browseInviteLinks.call(
+                        ProjectsBrowseInviteLinksRequest(),
+                        client.withProject(project.projectId)
+                    ).orThrow()
+
+                    Out(
+                        token,
+                        browse.items
+                    )
+                }
+
+                case("by PI (success)") {
+                    input(In(ProjectRole.PI))
+                    check {
+                        assert(output.browse.first { it.token == output.token }.groupAssignment.size == 2)
+                    }
+                }
+
+                case("by project admin (success)") {
                     input(In(ProjectRole.ADMIN))
                     check {
-                        assert(output.browse.first { it.token == output.token }.roleAssignment == input.role)
+                        assert(output.browse.first { it.token == output.token }.groupAssignment.size == 2)
+                    }
+                }
+
+                case("by project member (failure)") {
+                    input(In(ProjectRole.USER))
+                    expectFailure()
+                }
+
+                case("by other user (failure)") {
+                    input(In(null))
+                    expectFailure()
+                }
+            }
+        }
+        run {
+            class In()
+
+            class Out(
+                val username: String,
+                val members: List<ProjectMember>?
+            )
+
+            test<In, Out>("Project invite link accept") {
+                execute {
+                    val root = initializeRootProject(setOf(UCLOUD_PROVIDER))
+                    val project = initializeNormalProject(root)
+
+                    val group1 = createGroup(project)
+                    val group2 = createGroup(project)
+
+                    val user = createUser()
+
+                    val token = Projects.createInviteLink.call(
+                        Unit,
+                        project.piClient.withProject(project.projectId)
+                    ).orThrow().token
+
+                    Projects.updateInviteLinkGroupAssignment.call(
+                        ProjectsUpdateInviteLinkGroupAssignmentRequest(token, listOf(group1.groupId, group2.groupId)),
+                        project.piClient.withProject(project.projectId)
+                    )
+
+                    Projects.acceptInviteLink.call(
+                        ProjectsAcceptInviteLinkRequest(token),
+                        user.client
+                    ).orThrow()
+
+                    val projectMembers = Projects.retrieve.call(
+                        ProjectsRetrieveRequest(project.projectId, includeMembers = true),
+                        project.piClient.withProject(project.projectId)
+                    ).orThrow()
+
+                    Out(
+                        user.username,
+                        projectMembers.status.members
+                    )
+                }
+
+                case("user added to project") {
+                    input(In())
+                    check {
+                        assert(output.members?.map { it.username }?.contains(output.username) ?: false)
                     }
                 }
             }
