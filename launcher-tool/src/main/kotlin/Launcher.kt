367d17c61592953bcb33ac8e120484478035f057
Henrik Schulz <henrikschulz@hotmail.com>
2023-02-10 10:28:15 +0100

    Update Launcher.kt
    Parenthesis problem. 
    Do to missing parenthesis headless mode required a string of format t[0-9] before it would be true. Should be fixed with this addition. Do not know why this worked on initial branch.

--- launcher-tool/src/main/kotlin/Launcher.kt
+++ launcher-tool/src/main/kotlin/Launcher.kt
@@ -72,7 +72,7 @@ fun main(args: Array<String>) {
         val shouldInitializeTestEnvironment = (args.contains("init") && args.contains("--all-providers"))
 
         isHeadless = shouldInitializeTestEnvironment || (args.contains("env") && args.contains("delete")) ||
-             args.contains("snapshot") && args.find { it.contains(Regex("^[t][0-9]+\$")) } != null
+             (args.contains("snapshot") && args.find { it.contains(Regex("^[t][0-9]+\$")) } != null)
 
         // NOTE(Dan): initCurrentEnvironment() now initializes an environment which is ready. It returns true if the
         // environment is new. This method will override several "environment" global variables, such as the
