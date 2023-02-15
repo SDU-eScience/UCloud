31160452c211acc7ae9e47152fcf75e542f25f64
xiroV <brian@xirov.com>
2023-02-15 14:35:01 +0100

    Added sharesSupported to FSFileSupport

--- frontend-web/webclient/app/UCloud/FilesApi.tsx
+++ frontend-web/webclient/app/UCloud/FilesApi.tsx
@@ -600,7 +600,7 @@ class FilesApi extends ResourceApi<UFile, ProductStorage, UFileSpecification,
 
                     const support = cb.collection?.status.resolvedSupport?.support;
                     if (!support) return false;
-                    if (!(support as FileCollectionSupport).files.sharesSupported) return false;
+                    if ((support as FileCollectionSupport).files.sharesSupported === false) return false;
 
                     const isMissingPermissions = selected.some(it => !it.permissions.myself.some(p => p === "ADMIN"));
                     const hasNonDirectories = selected.some(it => it.status.type != "DIRECTORY");
