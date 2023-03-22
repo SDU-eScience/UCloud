21474e5e1269240d491e9ffaee379cb668bfea3c
Jonas Malte Hinchely <jonas@hinchely.dk>
2023-03-22 16:00:10 +0100

    Remove styled components from Provider overview

--- frontend-web/webclient/app/Providers/Overview.tsx
+++ frontend-web/webclient/app/Providers/Overview.tsx
@@ -4,14 +4,13 @@ import {useTitle} from "@/Navigation/Redux/StatusActions";
 import {Box, Link, theme} from "@/ui-components";
 import {GridCardGroup} from "@/ui-components/Grid";
 import * as React from "react";
-import styled from "styled-components";
 import {NonAuthenticatedHeader} from "@/Navigation/Header";
 import {Client} from "@/Authentication/HttpClientInstance";
 import {ProviderLogo} from "@/Providers/ProviderLogo";
 import {ProviderTitle} from "@/Providers/ProviderTitle";
 import Providers from "@/Assets/provider_info.json";
-import * as Heading from "@/ui-components/Heading";
 import {Feature, hasFeature} from "@/Features";
+import {injectStyle} from "@/Unstyled";
 
 interface ProviderType {
     id: string;
@@ -21,28 +20,31 @@ interface ProviderType {
     description: string;
 }
 
+const ProviderLogoClass = "provider-log";
+const ProviderContentClass = "provider-content"
+
 export function ProviderEntry(props: {provider: ProviderType}): React.ReactElement {
     return (
         <Link to={`/providers/detailed/${props.provider.id}`}>
-            <ProviderCard>
+            <div className={ProviderCard}>
                 <AbsoluteNoPointerEvents left={0} top={0}
                     cursor="inherit"
                     height="10px"
                     width="100%"
                     background={theme.colors.headerBg} />
 
-                <div className={"provider-logo"}>
+                <div className={ProviderLogoClass}>
                     <ProviderLogo providerId={props.provider.id} size={150} />
                 </div>
 
-                <Heading.h3>
+                <h3>
                     <ProviderTitle providerId={props.provider.id} />
-                </Heading.h3>
+                </h3>
 
-                <div className={"provider-content"}>
+                <div className={ProviderContentClass}>
                     {props.provider.shortDescription}
                 </div>
-            </ProviderCard>
+            </div>
         </Link>
     );
 }
@@ -69,33 +71,33 @@ export default function ProviderOverview() {
     return (<MainContainer main={main} />);
 }
 
-const ProviderCard = styled.div`
-  padding: 10px 10px 10px 10px;
-  width: 250px;
-  height: 412px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  border-radius: ${theme.radius};
-  position: relative;
-  overflow: hidden;
-  box-shadow: ${theme.shadows.sm};
-  background-color: var(--appCard, #f00);
-  cursor: pointer;
-  color: var(--black);
+const ProviderCard = injectStyle("provider-card", k => `
+    ${k} {
+        padding: 10px 10px 10px 10px;
+        width: 250px;
+        height: 412px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        border-radius: ${theme.radius};
+        position: relative;
+        overflow: hidden;
+        box-shadow: ${theme.shadows.sm};
+        background-color: var(--appCard, #f00);
+        cursor: pointer;
+        color: var(--black);
 
-  transition: transform ${theme.timingFunctions.easeIn} ${theme.duration.fastest} ${theme.transitionDelays.xsmall};
-  will-change: transform;
+        transition: transform ${theme.timingFunctions.easeIn} ${theme.duration.fastest} ${theme.transitionDelays.xsmall};
+        will-change: transform;
+    }
 
-  &:hover {
+  ${k}:hover {
     transition: transform ${theme.timingFunctions.easeOut} ${theme.duration.fastest} ${theme.transitionDelays.xsmall};
-      // box-shadow: ${theme.shadows.md};
     box-shadow: 0px 3px  5px -1px rgba(0, 106, 255, 0.2), 0px 6px 10px 0px rgba(0, 106, 255, .14), 0px 1px 18px 0px rgba(0, 106, 255, .12);
     transform: translateY(-2px);
   }
 
-  // Background
-  &:before {
+  ${k}::before {
     pointer-events: none;
     content: "";
     position: absolute;
@@ -107,11 +109,11 @@ const ProviderCard = styled.div`
     background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxwYXR0ZXJuIHZpZXdCb3g9IjAgMCBhdXRvIGF1dG8iIHg9IjAiIHk9IjAiIGlkPSJwMSIgd2lkdGg9IjU2IiBwYXR0ZXJuVHJhbnNmb3JtPSJyb3RhdGUoMTUpIHNjYWxlKDAuNSAwLjUpIiBoZWlnaHQ9IjEwMCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTTI4IDY2TDAgNTBMMCAxNkwyOCAwTDU2IDE2TDU2IDUwTDI4IDY2TDI4IDEwMCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjYzlkM2RmNDQiIHN0cm9rZS13aWR0aD0iMS41Ij48L3BhdGg+PHBhdGggZD0iTTI4IDBMMjggMzRMMCA1MEwwIDg0TDI4IDEwMEw1NiA4NEw1NiA1MEwyOCAzNCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjYzlkM2RmNDQiIHN0cm9rZS13aWR0aD0iNCI+PC9wYXRoPjwvcGF0dGVybj48L2RlZnM+PHJlY3QgZmlsbD0idXJsKCNwMSkiIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiPjwvcmVjdD48L3N2Zz4=");
   }
 
-  &:hover:after {
+  ${k}:hover::after {
     opacity: 1;
   }
 
-  .provider-logo {
+  ${k} > .${ProviderLogoClass} {
     margin-left: auto;
     margin-right: auto;
     margin-top: 10px;
@@ -121,14 +123,17 @@ const ProviderCard = styled.div`
     object-fit: scale-down;
   }
   
-  .provider-content {
+  ${k} > .${ProviderContentClass} {
     height: 300px;
     width: 100%;
     margin-top: 4px;
   }
-  
-  h3 {
-    width: 100%;
+
+  ${k} > h3 {
+    font-size: 24px; 
     text-align: center;
+    font-weight: 400;
+    width: 100%;
+    margin: 0 0 0 0;
   }
-`;
+`);
