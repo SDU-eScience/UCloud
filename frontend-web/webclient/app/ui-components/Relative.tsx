6051a4e4512f26e8407afd51763303327b0b6a82
Jonas Malte Hinchely <jonas@hinchely.dk>
2023-03-24 09:56:44 +0100

    Whitespace

--- frontend-web/webclient/app/ui-components/Relative.tsx
+++ frontend-web/webclient/app/ui-components/Relative.tsx
@@ -4,23 +4,22 @@ import {CSSProperties} from "react";
 import {extractSize} from "@/Unstyled";
 
 const Relative: React.FunctionComponent<BoxProps & {
-  top?: number | string;
-  left?: number | string;
-  bottom?: number | string;
-  right?: number | string;
-  children?: React.ReactNode;
+    top?: number | string;
+    left?: number | string;
+    bottom?: number | string;
+    right?: number | string;
+    children?: React.ReactNode;
 }> = props => {
-  const style: CSSProperties = {position: "relative"};
-  if (props.top !== undefined) style.top = extractSize(props.top);
-  if (props.left !== undefined) style.left = extractSize(props.left);
-  if (props.right !== undefined) style.right = extractSize(props.right);
-  if (props.bottom !== undefined) style.bottom = extractSize(props.bottom);
-
-  return <Box
-      style={style}
-      children={props.children}
-      {...props}
-  />;
+    const style: CSSProperties = {position: "relative"};
+    if (props.top !== undefined) style.top = extractSize(props.top);
+    if (props.left !== undefined) style.left = extractSize(props.left);
+    if (props.right !== undefined) style.right = extractSize(props.right);
+    if (props.bottom !== undefined) style.bottom = extractSize(props.bottom);
+    return <Box
+        style={style}
+        children={props.children}
+        {...props}
+    />;
 };
 
 Relative.displayName = "Relative";
