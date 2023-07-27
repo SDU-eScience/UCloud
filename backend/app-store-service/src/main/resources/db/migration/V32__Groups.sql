0e894d25dab22db8af22de6865f072e0b6fd4455
Brian Ravn <brian@xirov.com>
2023-07-27 15:59:33 +0200

    Migration script for new app store

---
+++ backend/app-store-service/src/main/resources/db/migration/V32__Groups.sql
@@ -0,0 +1,38 @@
+create table if not exists app_store.application_groups (
+    id serial primary key,
+    title text not null,
+    logo bytea,
+    description text
+);
+
+alter table app_store.applications add column group_id int references app_store.application_groups(id);
+alter table app_store.applications add column flavor_name text;
+
+create table if not exists app_store.group_tags (
+    group_id int references application_groups(id),
+    tag_id int references tags(id),
+    primary key (group_id, tag_id)
+);
+
+create table if not exists app_store.sections (
+    id serial primary key,
+    title text not null,
+    order_index int not null,
+    page text not null
+);
+
+create table if not exists app_store.section_featured_items (
+    section_id int references sections(id),
+    group_id int references app_store.application_groups(id),
+    order_index int not null,
+    primary key (section_id, group_id)
+);
+
+create table if not exists app_store.section_tags (
+    section_id int references sections(id),
+    tag_id int references application_groups(id),
+    primary key (section_id, tag_id)
+);
+
+drop table app_store.overview;
+drop table app_store.application_tags;
