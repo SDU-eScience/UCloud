f2e0d9756c652ba03b5311a8961152cfed1aedba
Jonas Malte Hinchely <jonas@hinchely.dk>
2023-09-14 10:43:02 +0200

    Update packages. Fix #4051

--- frontend-web/webclient/app/Products/Products.tsx
+++ frontend-web/webclient/app/Products/Products.tsx
@@ -71,7 +71,7 @@ const DetailedView = injectStyle("detailed-view", k => `
 
 export const MachineView: React.FunctionComponent<{area: ProductType, provider: string; color?: string}> = ({area, provider, color = "var(--blue, #f00)"}) => {
     const [machines, refetch] = useCloudAPI<UCloud.PageV2<Product>>(
-        UCloud.accounting.products.browse({filterArea: area, filterProvider: provider, filterUsable: true, itemsPerPage: 10}),
+        {...UCloud.accounting.products.browse({filterArea: area, filterProvider: provider, filterUsable: true, itemsPerPage: 10}), unauthenticated: !Client.isLoggedIn},
         emptyPage
     );
 
@@ -106,9 +106,9 @@ export const MachineView: React.FunctionComponent<{area: ProductType, provider: 
                     <ListV2
                         page={machines.data}
                         loading={machines.loading}
-                        onLoadMore={() => refetch(UCloud.accounting.products.browse({
+                        onLoadMore={() => refetch({...UCloud.accounting.products.browse({
                             filterArea: area, filterProvider: provider, filterUsable: true, next: machines.data.next
-                        }))}
+                        }), unauthenticated: !Client.isLoggedIn})}
                         pageRenderer={items => (
                             <div className={MachineTypesWrapper}>
                                 <Table>
