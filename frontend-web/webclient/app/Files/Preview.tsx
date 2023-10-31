d71dbc80893008332d9bab79cb0690cdc7de697e
Jonas Malte Hinchely <jonas@hinchely.dk>
2023-10-31 13:34:11 +0100

    Fixes #4071

--- frontend-web/webclient/app/Files/Preview.tsx
+++ frontend-web/webclient/app/Files/Preview.tsx
@@ -8,16 +8,56 @@ import {useEffect, useState} from "react";
 import {fileName} from "@/Utilities/FileUtilities";
 import {bulkRequestOf} from "@/DefaultObjects";
 import {BulkResponse} from "@/UCloud";
-import {injectStyle} from "@/Unstyled";
+import {classConcat, injectStyle, injectStyleSimple} from "@/Unstyled";
 import fileType from "magic-bytes.js";
-import Editor from "@monaco-editor/react";
 import {PREVIEW_MAX_SIZE} from "../../site.config.json";
+import {AsyncCache} from "@/Utilities/AsyncCache";
+import {useSelector} from "react-redux";
+
+const monacoCache = new AsyncCache<any>();
+
+async function getMonaco(): Promise<any> {
+    return monacoCache.retrieve("", async () => {
+        const monaco = await (import("monaco-editor"));
+        self.MonacoEnvironment = {
+            getWorker: function (workerId, label) {
+                switch (label) {
+                    case 'json':
+                        return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
+                    case 'css':
+                    case 'scss':
+                    case 'less':
+                        return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
+                    case 'html':
+                    case 'handlebars':
+                    case 'razor':
+                        return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
+                    case 'typescript':
+                    case 'javascript':
+                        return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
+                    default:
+                        return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
+                }
+
+                function getWorkerModule(moduleUrl, label) {
+                    return new Worker(self.MonacoEnvironment!.getWorkerUrl!(moduleUrl, label), {
+                        name: label,
+                        type: 'module'
+                    });
+                };
+            }
+        };
+        return monaco;
+    })
+}
 
 export const MAX_PREVIEW_SIZE_IN_BYTES = PREVIEW_MAX_SIZE;
 
-export const FilePreview: React.FunctionComponent<{file: UFile}> = ({file}) => {
+export function FilePreview({file}: {file: UFile}): React.ReactNode {
     const [type, setType] = useState<ExtensionType>(null);
     const [loading, invokeCommand] = useCloudCommand();
+
+    const codePreview = React.useRef<HTMLDivElement>(null);
 
     const [data, setData] = useState("");
     const [error, setError] = useState<string | null>(null);
@@ -58,7 +98,7 @@ export const FilePreview: React.FunctionComponent<{file: UFile}> = ({file}) => {
                         case "audio":
                         case "video":
                         case "pdf":
-                            setData(URL.createObjectURL(new Blob([contentBlob], {type: foundFileType[0].mime}))); 
+                            setData(URL.createObjectURL(new Blob([contentBlob], {type: foundFileType[0].mime})));
                             setError(null);
                             break;
                         case "code":
@@ -91,6 +131,36 @@ export const FilePreview: React.FunctionComponent<{file: UFile}> = ({file}) => {
         fetchData();
     }, [file]);
 
+    const lightTheme = useSelector((red: ReduxObject) => red.sidebar.theme)
+    React.useEffect(() => {
+        if (type === "text" || type === "code") {
+            const theme = lightTheme === "light" ? "light" : "vs-dark";
+            getMonaco().then(monaco => monaco.editor.setTheme(theme));
+        }
+    }, [lightTheme, type]);
+
+    React.useLayoutEffect(() => {
+        const node = codePreview.current;
+        if (!node) return;
+        if (type === "text" || type === "code") {
+            getMonaco().then(monaco => {
+                const count = monaco.editor.getEditors().length;
+                if (count > 0) return;
+                monaco.editor.create(node, {
+                    value: data,
+                    language: languageFromExtension(extensionFromPath(file.id)),
+                    readOnly: true,
+                    minimap: {enabled: false},
+                    theme: isLightThemeStored() ? "light" : "vs-dark",
+                })
+            });
+            return () => {
+                getMonaco().then(monaco => monaco.editor.getEditors().forEach(it => it.dispose()));
+            }
+        }
+        return () => void 0;
+    }, [type, data]);
+
     if (file.status.type !== "FILE") return null;
     if ((loading || data === "") && !error) return <PredicatedLoadingSpinner loading />
 
@@ -99,18 +169,7 @@ export const FilePreview: React.FunctionComponent<{file: UFile}> = ({file}) => {
     switch (type) {
         case "text":
         case "code":
-            node = <Editor
-                height="500px"
-                width="100%"
-                options={{
-                    readOnly: true,
-                    minimap: {enabled: false},
-                }}
-                theme={isLightThemeStored() ? "light" : "vs-dark"}
-                language={languageFromExtension(extensionFromPath(file.id))}
-                className="fullscreen"
-                value={data}
-            />;
+            node = <div ref={codePreview} className={classConcat(Editor, "fullscreen")} />;
             break;
         case "image":
             node = <img alt={fileName(file.id)} src={data} />
@@ -131,13 +190,18 @@ export const FilePreview: React.FunctionComponent<{file: UFile}> = ({file}) => {
             node = <div />
             break;
     }
-    
+
     if (error !== null) {
         node = <div>{error}</div>;
     }
 
     return <div className={ItemWrapperClass}>{node}</div>;
 }
+
+const Editor = injectStyleSimple("m-editor", `
+    height: 500px;
+    width: 100%;
+`);
 
 const ItemWrapperClass = injectStyle("item-wrapper", k => `
     ${k} {
@@ -155,7 +219,7 @@ const ItemWrapperClass = injectStyle("item-wrapper", k => `
 
 function tryDecodeText(buf: Uint8Array): string | null {
     try {
-        const d = new TextDecoder("utf-8", { fatal: true });
+        const d = new TextDecoder("utf-8", {fatal: true});
         return d.decode(buf);
     } catch (e) {
         return null;
