f916449574256e5250749cb3a53ce81fa2dd15ea
Dan Sebastian Thrane <dthrane@gmail.com>
2023-11-28 12:06:44 +0100

    Minor fixes in prep for 2023.4.0

--- frontend-web/webclient/app/AvataaarLib/hook.ts
+++ frontend-web/webclient/app/AvataaarLib/hook.ts
@@ -4,12 +4,16 @@ import {fetchBulkAvatars, FetchBulkAvatarsResponse} from "@/AvataaarLib/index";
 import {useUState, UState} from "@/Utilities/UState";
 
 class AvatarState extends UState<AvatarState> {
+    private inflight: Record<string, true> = {};
     private cache: Record<string, AvatarType> = {};
 
     updateCache(usernames: string[]): Promise<void> {
-        /*
-        const usernamesToUse = usernames.filter(it => !this.cache.hasOwnProperty(it));
+        const usernamesToUse = usernames.filter(it =>
+            !this.cache.hasOwnProperty(it) &&
+            !this.inflight.hasOwnProperty(it));
+
         if (usernamesToUse.length === 0) return Promise.resolve();
+        for (const username of usernamesToUse) this.inflight[username] = true;
 
         return this.run(async () => {
             const response = await callAPI<FetchBulkAvatarsResponse>(fetchBulkAvatars({usernames: usernamesToUse}));
@@ -17,10 +21,20 @@ class AvatarState extends UState<AvatarState> {
             const newCache = response !== null ? {...this.cache, ...response.avatars} : this.cache;
             if (response !== null) {
                 this.cache = newCache;
+                for (const username of usernamesToUse) {
+                    if (this.cache[username] == null) {
+                        this.cache[username] = defaultAvatar;
+                    }
+                }
             }
         });
-         */
-        return Promise.resolve();
+    }
+
+    invalidateAndUpdate(usernames: string[]): Promise<void> {
+        for (const name of usernames) {
+            delete this.cache[name];
+        }
+        return this.updateCache(usernames);
     }
 
     avatar(username: string): AvatarType {
