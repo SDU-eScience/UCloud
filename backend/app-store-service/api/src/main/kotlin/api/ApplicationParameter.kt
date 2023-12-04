0b77a7ec2e1f12ec5085aeb6005dcf40c51cdd53
Dan Sebastian Thrane <dthrane@gmail.com>
2023-12-04 09:32:34 +0100

    More formatting

--- backend/app-store-service/api/src/main/kotlin/api/ApplicationParameter.kt
+++ backend/app-store-service/api/src/main/kotlin/api/ApplicationParameter.kt
@@ -224,7 +224,7 @@ sealed class ApplicationParameter {
         override var name: String = "",
         override val title: String = "",
         override val description: String = "",
-        override val optional: Boolean = false
+        override val optional: Boolean = false,
     ) : ApplicationParameter() {
         override val defaultValue: JsonElement? = null
     }
