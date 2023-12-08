6a44d2ea419f3346e718367ebf0067e1ca694206
Henrik Schulz <henrikschulz@hotmail.com>
2023-12-08 12:24:57 +0100

    remvoed from build.gradle

--- backend/file-orchestrator-service/api/build.gradle.kts
+++ backend/file-orchestrator-service/api/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
    api(project(":accounting-service:api"))
-    implementation(project(mapOf("path" to ":service-lib-server")))
     // api("com.github.java-json-tools:json-schema-validator:2.2.14")
 }
