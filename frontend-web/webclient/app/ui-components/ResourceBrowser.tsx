d6477104f9c277d37c82eb5c4575de795c0af63f
Jonas Malte Hinchely <jonas@hinchely.dk>
2024-04-11 16:21:00 +0200

    #4141 Ellipses are added, even when no folder is present. (Only in one folder in the drive, in the image)

--- frontend-web/webclient/app/ui-components/ResourceBrowser.tsx
+++ frontend-web/webclient/app/ui-components/ResourceBrowser.tsx
@@ -1223,7 +1223,7 @@ export class ResourceBrowser<T> {
             }
         }
 
-        const canKeepMiddle = combinedLengthAfterTruncation <= targetPathLength;
+        const canKeepMiddle = crumbs.length === 2 || combinedLengthAfterTruncation <= targetPathLength;
         const canKeepParent = combinedLengthWithParent <= targetPathLength;
 
         const fragment = document.createDocumentFragment();
@@ -1237,7 +1237,7 @@ export class ResourceBrowser<T> {
                 idx === crumbs.length - 1 ||
                 (idx === crumbs.length - 2 && canKeepParent);
 
-            if (idx === 1 && !canKeepMiddle && idx !== crumbs.length - 2) {
+            if (idx === 1 && !canKeepMiddle) {
                 const listItem = document.createElement("li");
                 listItem.innerText = "...";
                 fragment.append(listItem);
