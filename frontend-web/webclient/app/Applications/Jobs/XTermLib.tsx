552f34cadbf5b5c41c27428686e0a1f20498a04d
Jonas Malte Hinchely <jonas@hinchely.dk>
2024-04-29 10:36:20 +0200

    #4177 Terminal theme colors don't match background colors anymore

--- frontend-web/webclient/app/Applications/Jobs/XTermLib.tsx
+++ frontend-web/webclient/app/Applications/Jobs/XTermLib.tsx
@@ -84,7 +84,7 @@ export function appendToXterm(term: Terminal, textToAppend: string): void {
 function getTheme(): ITheme {
     if (!isLightThemeStored()) {
         return {
-            background: "#282c2f",
+            background: "#2a313b",
             selectionForeground: "#44475a",
             foreground: "#f8f8f2",
             cyan: "#8be9fd",
@@ -106,7 +106,7 @@ function getTheme(): ITheme {
         };
     } else {
         return {
-            background: "#f8f8f9",
+            background: "#fff",
             selectionForeground: "#d6d6d6",
             foreground: "#4d4d4c",
             red: "#c82829",
