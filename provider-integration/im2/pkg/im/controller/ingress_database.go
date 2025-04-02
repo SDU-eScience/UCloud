d4b0520ab1a3bab41ba6e1a17214951d0e1611e9
Brian Ravn <brian@xirov.com>
2025-04-02 08:45:42 +0200

    Started work on ingress database

---
+++ provider-integration/im2/pkg/im/controller/ingress_database.go
@@ -0,0 +1,49 @@
+package controller
+
+import (
+	"sync"
+
+	"ucloud.dk/pkg/log"
+	orc "ucloud.dk/pkg/orchestrators"
+)
+
+var ingresses = map[string]*orc.Ingress{}
+
+var ingressesMutex = sync.Mutex{}
+
+func initIngressDatabase() {
+	if !RunsServerCode() {
+		return
+	}
+
+	ingressesMutex.Lock()
+	defer ingressesMutex.Unlock()
+	fetchAllIngresses()
+}
+
+func fetchAllIngresses() {
+	next := ""
+
+	for {
+		page, err := orc.BrowseIngresses(next, orc.BrowseIngressesFlags{
+			IncludeProduct: false,
+			IncludeUpdates: true,
+		})
+
+		if err != nil {
+			log.Warn("Failed to fetch ingresses: %v", err)
+			break
+		}
+
+		for i := 0; i < len(page.Items); i++ {
+			ingress := &page.Items[i]
+			ingresses[ingress.Id] = ingress
+		}
+
+		if !page.Next.IsSet() {
+			break
+		} else {
+			next = page.Next.Get()
+		}
+	}
+}
