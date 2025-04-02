defdb95867cd2011e16e42de239956beb7d2f744
Brian Ravn <brian@xirov.com>
2025-04-02 08:44:46 +0200

    Updated ingress data types

--- provider-integration/im2/pkg/orchestrators/ingresses.go
+++ provider-integration/im2/pkg/orchestrators/ingresses.go
@@ -2,6 +2,8 @@ package orchestrators
 
 import (
 	"ucloud.dk/pkg/apm"
+	c "ucloud.dk/pkg/client"
+	fnd "ucloud.dk/pkg/foundation"
 )
 
 type Ingress struct {
@@ -9,6 +11,10 @@ type Ingress struct {
 	Specification IngressSpecification `json:"specification"`
 	Status        IngressStatus        `json:"status"`
 	Updates       []IngressUpdate      `json:"updates"`
+}
+
+type IngressSupport struct {
+	Product apm.ProductReference `json:"product"`
 }
 
 type IngressSpecification struct {
@@ -34,3 +40,23 @@ const (
 	IngressStateReady       IngressState = "READY"
 	IngressStateUnavailable IngressState = "UNAVAILABLE"
 )
+
+// API
+// =====================================================================================================================
+
+const ingressesCtrlNamespace = "ingresses.control."
+const ingressesCtrlContext = "/api/ingresses/control/"
+
+type BrowseIngressesFlags struct {
+	IncludeProduct bool `json:"includeProduct"`
+	IncludeUpdates bool `json:"includeUpdates"`
+}
+
+func BrowseIngresses(next string, flags BrowseIngressesFlags) (fnd.PageV2[Ingress], error) {
+	return c.ApiBrowse[fnd.PageV2[Ingress]](
+		ingressesCtrlNamespace+"browse",
+		ingressesCtrlContext,
+		"",
+		append([]string{"next", next}, c.StructToParameters(flags)...),
+	)
+}
