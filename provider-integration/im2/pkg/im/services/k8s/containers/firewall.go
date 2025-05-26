802dc9238e3f46217ba5169f0f66e012de63fd06
Dan Sebastian Thrane <dthrane@gmail.com>
2025-05-26 10:26:05 +0200

    IM2/K8s: Remove redundant network policy rules

--- provider-integration/im2/pkg/im/services/k8s/containers/firewall.go
+++ provider-integration/im2/pkg/im/services/k8s/containers/firewall.go
@@ -30,17 +30,6 @@ func prepareFirewallOnJobCreate(
 		pod.ObjectMeta.Labels["ucloud.dk/firewallSensitive"] = "true"
 	}
 
-	// NOTE(Dan): Kubernetes will insert null instead of an empty list if we pass an empty list
-	// The JSON patch below will only work if the list is present and we cannot insert an empty list
-	// if it is not already present via JSON patch. As a result, we will insert a dummy entry which
-	// (hopefully) shouldn't have any effect.
-
-	// NOTE(Dan): The IP listed below is reserved for documentation (TEST-NET-1,
-	// see https://tools.ietf.org/html/rfc5737). Let's hope no one gets the bright idea to actually
-	// use this subnet in practice.
-	allowNetworkFromSubnet(firewall, invalidSubnet)
-	allowNetworkToSubnet(firewall, invalidSubnet)
-
 	var peers []orc.AppParameterValue
 
 	for _, v := range job.Specification.Parameters {
@@ -135,5 +124,3 @@ type networkPolicyEgressPatch struct {
 	Path  string                             `json:"path"`
 	Value networking.NetworkPolicyEgressRule `json:"value"`
 }
-
-const invalidSubnet = "192.0.2.100/32"
