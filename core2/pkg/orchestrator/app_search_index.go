e13713d0924b6b8ebc03c31c56315817bae58f8a
Dan Sebastian Thrane <dthrane@gmail.com>
2025-08-04 08:09:15 +0200

    Core2: App catalog search tweak

--- core2/pkg/orchestrator/app_search_index.go
+++ core2/pkg/orchestrator/app_search_index.go
@@ -3,6 +3,7 @@ package orchestrator
 import (
 	"fmt"
 	"github.com/blevesearch/bleve"
+	"strings"
 	orcapi "ucloud.dk/shared/pkg/orc2"
 )
 
@@ -23,16 +24,17 @@ func appAddToIndex(id AppGroupId, group orcapi.ApplicationGroup) {
 	var toIndex struct {
 		Title       string
 		Description string
-		Flavor      []string
+		Flavor      string
 	}
 
 	toIndex.Title = group.Specification.Title
 	toIndex.Description = group.Specification.Description
+	flavors := strings.Builder{}
 	for _, app := range group.Status.Applications {
-		if app.Metadata.FlavorName != "" {
-			toIndex.Flavor = append(toIndex.Flavor, app.Metadata.FlavorName)
-		}
+		flavors.WriteString(app.Metadata.FlavorName)
+		flavors.WriteString(" ")
 	}
+	toIndex.Flavor = flavors.String()
 	err := appIndex.Index(fmt.Sprint(id), toIndex)
 	if err != nil {
 		panic(err)
