95af7c12844dc91c5a678fea98c2cd95962cd444
Jonas Malte Hinchely <jonas@hinchely.dk>
2025-08-12 13:26:21 +0200

    Fixes #4932 test -- Run with terminal output test-ui -- Run with UI test-headed -- Run with browser showing actions test-results -- Show result of testing (if any)
    Also:
    refresh-icons -- Runs all three icon related actions

--- frontend-web/webclient/tests/test-1.spec.ts
+++ frontend-web/webclient/tests/test-1.spec.ts
@@ -7,12 +7,13 @@ test.beforeEach(async ({page}) => {
 
 test('Create and delete folder (with available resources)', async ({page}) => {
   await page.getByRole('link', {name: 'Go to Files'}).click();
-  await page.getByRole('link', {name: 'Logo for K8s Dev Provider Home'}).click();
+  await page.getByText("Home").click();
   await page.getByText('Create folder⌥ F').click();
   await page.getByRole('textbox').nth(1).fill('FOOBAR');
   await page.getByRole('textbox').nth(1).press('Enter');
   await page.getByText('FOOBAR').click();
   await page.locator('div').filter({hasText: /^FOOBAR$/}).nth(1).dblclick();
   await page.goBack();
+  // TODO(Jonas): I don't think this link is considered idiomatic
   expect(page.locator('div').filter({hasText: "Could not find directory"})).toBeTruthy();
 });
