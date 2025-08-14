5c0b141bbae7300697baf4df884830d465bb29b8
Dan Sebastian Thrane <dthrane@gmail.com>
2025-08-14 09:58:47 +0200

    IM1: Delete code related to IM1

--- provider-integration/integration-module/docker/dev-base/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-version=`cat ../../../../backend/version.txt`
-docker buildx build \
-    --push \
-    --tag dreg.cloud.sdu.dk/ucloud-dev/integration-module:${version} \
-    --platform linux/arm64/v8,linux/amd64 \
-    .
--- provider-integration/integration-module/src/main/kotlin/sql/RawSql.kt
+++
@@ -1,4 +0,0 @@
-package dk.sdu.cloud.sql
-
-@JvmInline
-value class RawSql(val sql: String)
