f5b2f0911957eebe6b16cd801614922530aa8053
Jonas Malte Hinchely <jonas@hinchely.dk>
2025-08-27 13:09:07 +0200

    More testing

--- frontend-web/webclient/tests/file.spec.ts
+++ frontend-web/webclient/tests/file.spec.ts
@@ -1,5 +1,5 @@
 import {test, expect} from '@playwright/test';
-import {Drive, Folder, login} from "./shared";
+import {Components, Drive, Folder, login, Rows} from "./shared";
 
 const Drives: Record<string, string> = {};
 
@@ -15,20 +15,18 @@ test.beforeEach(async ({page, userAgent}) => {
 test('Create and delete folder (with available resources)', async ({page, userAgent}) => {
     const driveName = Drives[userAgent!];
     const folderName = Folder.newFolderName();
-    await Drive.goToDrives(page);
-    await page.getByText(driveName).dblclick();
+    await Drive.openDrive(page, driveName);
     await Folder.create(page, folderName);
     await expect(page.locator('div > span', {hasText: folderName})).toHaveCount(1);
     await Folder.delete(page, folderName);
     await expect(page.locator('div > span', {hasText: folderName})).toHaveCount(0);
 });
 
-test('Rename (with available resources)', async ({page}) => {
+test('Rename (with available resources)', async ({page, userAgent}) => {
+    const driveName = Drives[userAgent!];
     const folderName = Folder.newFolderName();
     const newFolderName = Folder.newFolderName();
-    const driveName = Drive.newDriveName();
-    await Drive.create(page, driveName);
-    await page.locator('span').filter({hasText: driveName}).dblclick();
+    await Drive.openDrive(page, driveName);
     await Folder.create(page, folderName);
     await page.locator('div > span').filter({hasText: folderName}).click();
     await page.getByText('Rename').click();
@@ -36,10 +34,37 @@ test('Rename (with available resources)', async ({page}) => {
     await page.keyboard.press('Enter');
     await page.getByText(newFolderName).dblclick();
     await expect(page.getByText('This folder is empty')).toHaveCount(1);
+});
 
-    // Cleanup
-    await page.goBack();
-    await Folder.delete(page, newFolderName);
+
+test('Change sensitivity (with available resources)', async ({page, userAgent}) => {
+    const driveName = Drives[userAgent!];
+    await Drive.openDrive(page, driveName);
+
+    const folderName = Folder.newFolderName();
+    await Folder.create(page, folderName);
+
+    /* TODO(Jonas): One should be enough! */
+    await Components.clickRefreshAndWait(page);
+
+    await Components.clickRefreshAndWait(page);
+
+    await Components.clickRefreshAndWait(page);
+
+    await Components.clickRefreshAndWait(page);
+    /* TODO(Jonas): One should be enough! */
+
+    await Rows.actionByRowTitle(page, folderName, "click");
+    await page.locator('div:nth-child(6)').first().click();
+    await page.getByText('Change sensitivity').click();
+    // TODO(Jonas): Ensure NO confidential is present (or ensure that specific one has? If they happen simultaniously, more than one could be)
+    await page.locator('#sensitivityDialogValue').selectOption('CONFIDENTIAL');
+    await page.getByRole('textbox', {name: 'Reason for sensitivity change'}).fill('Content');
+    await page.getByRole('button', {name: 'Update'}).click();
+    // TODO(Jonas): Ensure 1 confidential is present (or ensure that specific one has?)
+
+    await expect(page.getByText("C", {exact: true})).toHaveCount(1);
+    // Cleanup    
 });
 
 test.afterEach(async ({page, userAgent}) => {
--- frontend-web/webclient/tests/shared.ts
+++ frontend-web/webclient/tests/shared.ts
@@ -1,6 +1,6 @@
-import {expect, Locator, type Page} from '@playwright/test';
+import {expect, Locator, type Page} from "@playwright/test";
 
-// Note(Jonas): If it complains that it doesn't exist, create it.
+// Note(Jonas): If it complains that it doesn"t exist, create it.
 import {default as data} from "./test_data.json" with {type: "json"};
 
 const user = data.users.with_resources;
@@ -8,8 +8,8 @@ const user = data.users.with_resources;
 export async function login(page: Page) {
     if (!user) throw Error("No username or password provided");
     await page.goto(ucloudUrl("login"));
-    await page.getByText('Other login options →').click();
-    await page.getByRole('textbox', {name: 'Username'}).fill(user.username);
+    await page.getByText("Other login options →").click();
+    await page.getByRole("textbox", {name: "Username"}).fill(user.username);
     await page.getByRole('textbox', {name: 'Password'}).fill(user.password);
     await page.getByRole('button', {name: 'Login'}).click();
     await stickSidebar(page);
@@ -31,26 +31,25 @@ export const Folder = {
 
     async create(page: Page, name: string) {
         expect(page.url().includes("/files?path=")).toBeTruthy();
-        await page.getByText('Create folder').click();
-        await page.getByRole('textbox').nth(1).fill(name);
-        await page.getByRole('textbox').nth(1).press('Enter');
+        await page.getByText("Create folder").click();
+        await page.getByRole("textbox").nth(1).fill(name);
+        await page.getByRole("textbox").nth(1).press("Enter");
     },
 
     async delete(page: Page, name: string) {
         await Rows.actionByRowTitle(page, name, "click");
-        await page.locator('.operation.button6.in-header:nth-child(6)').click(); // Ellipses
-        await page.getByRole('button', {name: 'Move to trash'}).click({delay: 1000 + 200});
+        await page.locator(".operation.button6.in-header:nth-child(6)").click(); // Ellipses
+        await page.getByRole("button", {name: "Move to trash"}).click({delay: 1000 + 200});
     }
 }
 
-const Rows = {
+export const Rows = {
     async actionByRowTitle(page: Page, name: string, action: "click" | "dblclick" | "hover", retries = 5) {
         // If not found, wait 3 seconds. If already waited, find height of table and scroll by height. If at bottom, fail.
         try {
             await page.waitForTimeout(200);
-            const loc = page.locator('div > span', {hasText: name});
+            const loc = page.locator("div > span", {hasText: name});
             if (loc) {
-                console.log(loc);
                 await loc[action]();
             }
         } catch (e) {
@@ -59,7 +58,6 @@ const Rows = {
             }
             await page.locator(".scrolling").hover();
             page.mouse.wheel(0, 400);
-            console.log("scrolling")
             Rows.actionByRowTitle(page, name, action, retries - 1);
         }
     },
@@ -71,51 +69,54 @@ export const Drive = {
     },
 
     async goToDrives(page: Page) {
-        await page.getByRole('link', {name: 'Go to Files'}).click();
-        await Components.projectSwitcher(page, "HOVER");
+        await page.getByRole("link", {name: "Go to Files"}).click();
+        await Components.projectSwitcher(page, "hover");
+    },
+
+    async openDrive(page: Page, name: string) {
+        if (!page.url().includes("/app/drives")) {
+            await this.goToDrives(page);
+        }
+        await page.getByText(name).dblclick();
     },
 
     async create(page: Page, name: string) {
-        await Drive.goToDrives(page);
+        await this.goToDrives(page);
         await page.waitForTimeout(200);
-        await page.locator("div.operation").filter({hasText: 'Create drive'}).click();
-        await page.getByRole('textbox', {name: 'Choose a name*'}).fill(name);
-        await page.getByRole('button', {name: 'Create', disabled: false}).click();
+        await page.locator("div.operation").filter({hasText: "Create drive"}).click();
+        await page.getByRole("textbox", {name: "Choose a name*"}).fill(name);
+        await page.getByRole("button", {name: "Create", disabled: false}).click();
     },
 
     async delete(page: Page, name: string) {
-        await Drive.goToDrives(page);
+        await this.goToDrives(page);
         await Rows.actionByRowTitle(page, name, "click");
-        await page.getByText('Delete').click();
-        await page.locator('#collectionName').fill(name);
-        await page.getByRole('button', {name: 'I understand what I am doing', disabled: false}).click();
+        await page.getByText("Delete").click();
+        await page.locator("#collectionName").fill(name);
+        await page.getByRole("button", {name: "I understand what I am doing", disabled: false}).click();
     },
 
     async rename(page: Page, oldName: string, newName: string) {
         await Rows.actionByRowTitle(page, oldName, "click");
-        await page.getByText('Rename').click();
-        await page.getByRole('textbox').nth(1).fill(newName);
-        await page.getByRole('textbox').nth(1).press('Enter');
+        await page.getByText("Rename").click();
+        await page.getByRole("textbox").nth(1).fill(newName);
+        await page.getByRole("textbox").nth(1).press("Enter");
     },
 
     async properties(page: Page, name: string) {
         await Rows.actionByRowTitle(page, name, "click");
-        await page.getByText('Properties').click();
+        await page.getByText("Properties").click();
     }
 };
 
 export const Components = {
-    async projectSwitcher(page: Page, action: "CLICK" | "HOVER") {
+    async projectSwitcher(page: Page, action: "click" | "hover") {
         const loc = page.locator("div.project-switcher").first();
-        switch (action) {
-            case "CLICK": {
-                await loc.click();
-                break;
-            }
-            case "HOVER": {
-                await loc.hover();
-                break;
-            }
-        }
+        await loc[action]();
+    },
+
+    async clickRefreshAndWait(page: Page, waitForMs: number = 500) {
+        await page.locator(".refresh-icon").click();
+        if (waitForMs > 0) await page.waitForTimeout(waitForMs);
     }
 }
