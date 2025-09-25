2e7e39eb30ed620fecacb38eb32bdffa6751bf81
Jonas Malte Hinchely <jonas@hinchely.dk>
2025-09-25 16:53:28 +0200

    Wild test

--- frontend-web/webclient/tests/application.spec.ts
+++ frontend-web/webclient/tests/application.spec.ts
@@ -1,5 +1,5 @@
-import {expect, test} from '@playwright/test';
-import {Applications, Components, login, Runs} from './shared';
+import {expect, test} from "@playwright/test";
+import {Applications, Components, login, Runs, File, Drive} from "./shared";
 
 test.beforeEach(async ({page}) => {
     login(page);
@@ -8,19 +8,19 @@ test.beforeEach(async ({page}) => {
 test("Run job with jobname, extend time, stop job, validate jobname in runs", async ({page}) => {
     test.setTimeout(240_000);
     await Applications.gotoApplications(page);
-    await page.getByRole('button', {name: 'Open application'}).click();
-    await page.getByRole('textbox', {name: 'Job name'}).click();
+    await page.getByRole("button", {name: "Open application"}).click();
+    await page.getByRole("textbox", {name: "Job name"}).click();
     const jobName = Runs.newJobName();
-    await page.getByRole('textbox', {name: 'Job name'}).fill(jobName);
+    await page.getByRole("textbox", {name: "Job name"}).fill(jobName);
     await Components.selectAvailableMachineType(page);
-    await page.getByRole('button', {name: '+1'}).click();
-    await page.getByRole('button', {name: 'Submit'}).click();
+    await page.getByRole("button", {name: "+1"}).click();
+    await page.getByRole("button", {name: "Submit"}).click();
 
     while (!await page.getByText("Time remaining: 01").isVisible()) {
         await page.waitForTimeout(1000);
     }
 
-    await page.getByRole('button', {name: '+1'}).click();
+    await page.getByRole("button", {name: "+1"}).click();
 
     while (!await page.getByText("Time remaining: 02").isVisible()) {
         await page.waitForTimeout(1000);
@@ -38,7 +38,7 @@ test("Run job with jobname, extend time, stop job, validate jobname in runs", as
 
 test("Favorite app, unfavorite app", async ({page}) => {
     // Note(Jonas): Stuff like finding the favorite icon is pretty difficult through selectors,
-    // so maybe consider adding test ids? I don't know what makes the most sense.
+    // so maybe consider adding test ids? I don"t know what makes the most sense.
     throw Error("Not implemented");
 });
 
@@ -47,12 +47,12 @@ test("Start app and stop app from runs page. Start it from runs page, testing pa
     const jobName = Runs.newJobName();
 
     await Applications.gotoApplications(page);
-    await page.getByRole('button', {name: 'Open application'}).click();
-    await page.getByRole('textbox', {name: 'Job name'}).click();
-    await page.getByRole('textbox', {name: 'Job name'}).fill(jobName);
+    await page.getByRole("button", {name: "Open application"}).click();
+    await page.getByRole("textbox", {name: "Job name"}).click();
+    await page.getByRole("textbox", {name: "Job name"}).fill(jobName);
     await Components.selectAvailableMachineType(page);
-    await page.getByRole('button', {name: '+1'}).click();
-    await page.getByRole('button', {name: 'Submit'}).click();
+    await page.getByRole("button", {name: "+1"}).click();
+    await page.getByRole("button", {name: "Submit"}).click();
 
     while (!await page.getByText("Time remaining: 01").isVisible()) {
         await page.waitForTimeout(1000);
@@ -81,5 +81,53 @@ test("Start app and stop app from runs page. Start it from runs page, testing pa
         await page.waitForTimeout(1000);
     }
     await expect(page.getByText("Run application again")).toHaveCount(1);
+
+});
+
+test("Mount folder with file in job, and cat inside contents", async ({page}) => {
+    const driveName = Drive.newDriveName();
+    await Drive.create(page, driveName);
+    await Drive.openDrive(page, driveName);
+    const folderName = File.newFolderName();
+    await File.create(page, folderName);
+    await File.open(page, folderName);
+    const [uploadedFileName, contents] = ["UploadedFile.txt", "Am I not invisible???"];
+    await File.uploadFiles(page, [{name: uploadedFileName, contents: contents}]);
+    await Applications.gotoApplications(page);
+    await page.getByRole("button", {name: "Open application"}).click();
+    await Components.selectAvailableMachineType(page);
+
+    await page.getByRole("button", {name: "Add folder"}).click();
+    await page.getByRole("textbox", {name: "No directory selected"}).click();
+    await page.getByRole("dialog").getByRole("button", {name: "Use"}).click();
+
+    await page.getByRole("button", {name: "Submit"}).click();
+
+    while (!await page.getByRole('button', {name: 'Open terminal'}).isVisible()) {
+        await page.waitForTimeout(500);
+    }
+
+    const terminalPagePromise = page.waitForEvent('popup');
+
+    await page.getByRole('button', {name: 'Open terminal'}).click();
+
+    const terminalPage = await terminalPagePromise;
+
+    await terminalPage.waitForTimeout(1000);
+    await terminalPage.getByText("ucloud@").click();
+
+    for (const key of "cat") {
+        await terminalPage.keyboard.press(key);
+    }
+    
+    terminalPage.keyboard.press("Space");
+
+    for (const key of (folderName + "/" + uploadedFileName)) {
+        await terminalPage.keyboard.press(key);    
+    }
+
+    await terminalPage.keyboard.press("Enter");
+
+    await expect(terminalPage.getByText(contents)).toHaveCount(1);
 
 });
