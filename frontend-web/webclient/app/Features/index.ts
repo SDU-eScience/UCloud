4cb36a100b6b5ed55a13d16a0367e16bc2177593
Jonas Malte Hinchely <jonas@hinchely.dk>
2025-10-23 13:16:29 +0200

    Remove unused Feature flags

--- frontend-web/webclient/app/Features/index.ts
+++ frontend-web/webclient/app/Features/index.ts
@@ -2,7 +2,6 @@ import {inDevEnvironment, onDevSite} from "@/UtilityFunctions";
 
 export enum Feature {
     PROVIDER_CONNECTION,
-    INLINE_TERMINAL,
     NEW_IDPS,
     COMPONENT_STORED_CUT_COPY,
     TRANSFER_TO,
@@ -11,16 +10,9 @@ export enum Feature {
     // current policies.
     ADDITIONAL_USER_INFO,
 
-    JOB_RENAME,
-
-    APP_CATALOG_FILTER,
-
-    NEW_TASKS,
-    COMMAND_PALETTE,
 
     PROVIDER_CONDITION,
 
-    REORDER_APP_GROUP,
 
     ALTERNATIVE_USAGE_SELECTOR,
     NEW_SYNCTHING_UI,
@@ -28,6 +20,10 @@ export enum Feature {
     HIDE_PROJECTS,
 
     CORE2,
+
+    // MISSING BACKEND SUPPORT
+    JOB_RENAME,
+    REORDER_APP_GROUP,
 }
 
 enum Environment {
@@ -77,36 +73,14 @@ const featureMap: Record<string, FeatureConfig> = {
         showWithFlag: allEnvironments,
     },
 
-    "inline-terminal": {
-        feature: Feature.INLINE_TERMINAL,
-        showWithoutFlag: allEnvironments,
-    },
-
     "component-stored-cut-copy": {
         feature: Feature.COMPONENT_STORED_CUT_COPY,
         showWithoutFlag: allEnvironments,
     },
 
-    "app-catalog-filter": {
-        feature: Feature.APP_CATALOG_FILTER,
-        showWithoutFlag: allEnvironments,
-        showWithFlag: allEnvironments
-    },
-
     "transfer-to": {
         feature: Feature.TRANSFER_TO,
         showWithoutFlag: allEnvironments,
-    },
-
-    "new-tasks": {
-        feature: Feature.NEW_TASKS,
-        showWithoutFlag: allEnvironments,
-    },
-
-    "command-palette": {
-        feature: Feature.COMMAND_PALETTE,
-        showWithoutFlag: allEnvironments,
-        showWithFlag: allEnvironments,
     },
 
     "provider-condition": {
