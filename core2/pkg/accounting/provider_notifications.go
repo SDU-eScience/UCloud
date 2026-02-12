a5f9da4a4dabad1286b51cadbd64ba340a019960
Dan Sebastian Thrane <dthrane@gmail.com>
2026-02-12 08:44:01 +0100

    Core2: Don't notify providers about empty wallets

--- core2/pkg/accounting/provider_notifications.go
+++ core2/pkg/accounting/provider_notifications.go
@@ -90,7 +90,7 @@ func initProviderNotifications() {
 				}
 			} else if walletOk {
 				wallet, ok := internalRetrieveWallet(time.Now(), walletId, false)
-				if ok && !wallet.PaysFor.FreeToUse {
+				if ok && !wallet.PaysFor.FreeToUse && len(wallet.AllocationGroups) != 0 {
 					var allChannels []chan *accapi.WalletV2
 
 					providerNotifications.Mu.Lock()
--- wiki/release-notes.md
+++ wiki/release-notes.md
@@ -4,6 +4,12 @@ This page tracks a high-level overview of changes to the version of UCloud
 hosted at cloud.sdu.dk. This page is intended for internal
 use.
 
+## 12-02-26
+
+```
+UCloud/Core @ 2026.1.43
+```
+
 ## 05-02-26
 
 ```
