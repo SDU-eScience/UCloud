b13ad15ba4704a89348f05945e56a11ae213f8cd
Jonas Malte Hinchely <jonas@hinchely.dk>
2026-03-04 17:04:06 +0100

    Fix missing unit in org details Fix wrong early startdate for products

--- launcher-tool-go/pkg/launcher/00_module.go
+++ launcher-tool-go/pkg/launcher/00_module.go
@@ -530,7 +530,7 @@ func TestsRun(adminUser, adminPass string) {
 					missingRootAllocations = append(missingRootAllocations, accapi.RootAllocateRequest{
 						Category: product.Category.ToId(),
 						Quota:    quota,
-						Start:    fndapi.Timestamp(time.Now()),
+						Start:    fndapi.Timestamp(time.Now().AddDate(0, -1, 0)),
 						End:      fndapi.Timestamp(time.Now().AddDate(0, 0, 7)),
 					})
 				}
@@ -636,6 +636,7 @@ func TestsRun(adminUser, adminPass string) {
 			_, err := fndapi.UsersUpdateOptionalInfo.Invoke(fndapi.OptionalUserInfo{
 				OrganizationFullName: util.OptValue[string]("Test org"),
 				Department:           util.OptValue[string]("Test department"),
+				Unit:                 util.OptValue("Test unit"),
 				ResearchField:        util.OptValue[string]("Other"),
 				Position:             util.OptValue[string]("Robot"),
 				Gender:               util.OptValue[string]("Other"),
