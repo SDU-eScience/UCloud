2b7db7ff1742e7f54bbf81a17f39c6993f411c18
Jonas Malte Hinchely <jonas@hinchely.dk>
2026-05-19 16:10:39 +0200

    Fixes #5541

--- frontend-web/webclient/app/ui-components/Sidebar.tsx
+++ frontend-web/webclient/app/ui-components/Sidebar.tsx
@@ -580,6 +580,11 @@ export interface SidebarDialog {
 
 const fileTypeCache: Record<string, FileType | "DELETED"> = {}
 
+
+type DriveChange = void;
+export type DriveChangeEvent = CustomEvent<DriveChange>;
+export const DriveChange = "DriveChangeCustomEvent";
+
 function useSidebarFilesPage(): [
     APICallState<PageV2<FileCollection>>,
     FileMetadataAttached[]
@@ -647,6 +652,17 @@ function useSidebarFilesPage(): [
     React.useEffect(() => {
         fetchDrives(FileCollectionsApi.browse({itemsPerPage: 250/* , filterMemberFiles: "all" */}))
     }, [projectId]);
+
+    React.useEffect(() => {
+        function driveChangeListener() {
+            fetchDrives(FileCollectionsApi.browse({itemsPerPage: 250/* , filterMemberFiles: "all" */}))
+        }
+
+        window.addEventListener(DriveChange, driveChangeListener);
+        return () => {
+            window.removeEventListener(DriveChange, driveChangeListener);
+        }
+    }, []);
 
     return [
         drives,
