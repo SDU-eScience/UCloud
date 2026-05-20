06c153445830a43011b9bd4e6cd7b1f7c3acb05a
Jonas Malte Hinchely <jonas@hinchely.dk>
2026-05-20 11:11:04 +0200

    Indentation fixes

--- frontend-web/webclient/app/Applications/Jobs/Resources/PrivateNetworks.tsx
+++ frontend-web/webclient/app/Applications/Jobs/Resources/PrivateNetworks.tsx
@@ -10,17 +10,7 @@ import {doNothing} from "@/UtilityFunctions";
 import {peerResourceAllowed} from "@/Applications/Jobs/Resources/Peers";
 import {Feature, hasFeature} from "@/Features";
 
-export const PrivateNetworkResource: React.FunctionComponent<{
-    application: Application;
-    params: ApplicationParameter[];
-    errors: Record<string, string>;
-    setErrors: React.Dispatch<React.SetStateAction<Record<string, string>>>
-    onAdd: () => void;
-    onRemove: (id: string) => void;
-    provider?: string;
-    dnsHostname: string;
-    onDnsHostnameChange: (ev: React.SyntheticEvent) => void;
-}> = ({
+export function PrivateNetworkResource({
     application,
     params,
     errors,
@@ -30,73 +20,83 @@ export const PrivateNetworkResource: React.FunctionComponent<{
     provider,
     dnsHostname,
     onDnsHostnameChange,
-}) => {
-        if (!peerResourceAllowed(application) || !hasFeature(Feature.NEW_VM_UI)) return null;
+}: {
+    application: Application;
+    params: ApplicationParameter[];
+    errors: Record<string, string>;
+    setErrors: React.Dispatch<React.SetStateAction<Record<string, string>>>
+    onAdd: () => void;
+    onRemove: (id: string) => void;
+    provider?: string;
+    dnsHostname: string;
+    onDnsHostnameChange: (ev: React.SyntheticEvent) => void;
+}) {
+    if (!peerResourceAllowed(application) || !hasFeature(Feature.NEW_VM_UI)) return null;
 
-        return (
-            <Card>
-                <Box>
-                    <Flex alignItems="center">
-                        <Box flexGrow={1}>
-                            <Heading.h4>Connect to other jobs</Heading.h4>
-                        </Box>
-                        <Button type={"button"} ml={"5px"} lineHeight={"16px"} onClick={onAdd}>Connect network</Button>
-                    </Flex>
-
-                    <Box mb={8} mt={8}>
-                        {params.length !== 0 ? (
-                            <Box mb={16}>
-                                <Label>
-                                    Hostname
-                                    <Input
-                                        mt={"8px"}
-                                        style={{minWidth: "220px"}}
-                                        value={dnsHostname}
-                                        onChange={onDnsHostnameChange}
-                                    />
-                                </Label>
-                                <div style={{color: "var(--textSecondary)", fontStyle: "italic", marginTop: "8px"}}>
-                                    Your job will be identified by this name within the network.
-                                </div>
-                            </Box>
-                        ) : (
-                            <>
-                                If you need to connect this job to a network of other jobs then click {" "}
-                                <BaseLink
-                                    href="#"
-                                    onClick={e => {
-                                        e.preventDefault();
-                                        onAdd();
-                                    }}
-                                >
-                                    &quot;Connect network&quot;
-                                </BaseLink>
-                                {" "}
-                                to select one. You can manage networks in {" "}
-                                <Link to={AppRoutes.resources.privateNetworks()} target="_blank">
-                                    private networks
-                                </Link>
-                                .
-                            </>
-                        )}
+    return (
+        <Card>
+            <Box>
+                <Flex alignItems="center">
+                    <Box flexGrow={1}>
+                        <Heading.h4>Connect to other jobs</Heading.h4>
                     </Box>
+                    <Button type={"button"} ml={"5px"} lineHeight={"16px"} onClick={onAdd}>Connect network</Button>
+                </Flex>
 
-                    {params.map(entry => (
-                        <Box key={entry.name} mb={"7px"}>
-                            <Widget
-                                provider={provider}
-                                parameter={entry}
-                                errors={errors}
-                                setErrors={setErrors}
-                                application={application}
-                                injectWorkflowParameters={doNothing}
-                                onRemove={() => {
-                                    onRemove(entry.name);
-                                }}
-                            />
+                <Box mb={8} mt={8}>
+                    {params.length !== 0 ? (
+                        <Box mb={16}>
+                            <Label>
+                                Hostname
+                                <Input
+                                    mt={"8px"}
+                                    style={{minWidth: "220px"}}
+                                    value={dnsHostname}
+                                    onChange={onDnsHostnameChange}
+                                />
+                            </Label>
+                            <div style={{color: "var(--textSecondary)", fontStyle: "italic", marginTop: "8px"}}>
+                                Your job will be identified by this name within the network.
+                            </div>
                         </Box>
-                    ))}
+                    ) : (
+                        <>
+                            If you need to connect this job to a network of other jobs then click {" "}
+                            <BaseLink
+                                href="#"
+                                onClick={e => {
+                                    e.preventDefault();
+                                    onAdd();
+                                }}
+                            >
+                                &quot;Connect network&quot;
+                            </BaseLink>
+                            {" "}
+                            to select one. You can manage networks in {" "}
+                            <Link to={AppRoutes.resources.privateNetworks()} target="_blank">
+                                private networks
+                            </Link>
+                            .
+                        </>
+                    )}
                 </Box>
-            </Card>
-        );
-    };
+
+                {params.map(entry => (
+                    <Box key={entry.name} mb={"7px"}>
+                        <Widget
+                            provider={provider}
+                            parameter={entry}
+                            errors={errors}
+                            setErrors={setErrors}
+                            application={application}
+                            injectWorkflowParameters={doNothing}
+                            onRemove={() => {
+                                onRemove(entry.name);
+                            }}
+                        />
+                    </Box>
+                ))}
+            </Box>
+        </Card>
+    );
+};
