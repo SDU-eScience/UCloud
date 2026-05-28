ab9a810ac1b1e5fdb8090c47788303836cf1477c
Jonas Malte Hinchely <jonas@hinchely.dk>
2026-05-28 12:46:02 +0200

    Fixes redirection issue Update packages

--- frontend-web/webclient/app/Applications/Jobs/JobsBrowse.tsx
+++ frontend-web/webclient/app/Applications/Jobs/JobsBrowse.tsx
@@ -441,12 +441,15 @@ function JobBrowse({opts}: {opts?: ResourceBrowserOpts<Job> & {omitBreadcrumbs?:
         }
     }, []);
 
-    const setLocalProject = opts?.isModal ? async (projectId?: string) => {
+    const setLocalProject = opts?.isModal ? (projectId?: string) => {
         const b = browserRef.current;
         if (b) {
-            b.canConsumeResources = await checkCanConsumeResources(projectId ?? null, {api: JobsApi});
-            activeProject.current = projectId;
-            b.refresh();
+            b.canConsumeResources = false;
+            checkCanConsumeResources(projectId ?? null, {api: JobsApi}).then(canConsume => {
+                b.canConsumeResources = canConsume;
+                activeProject.current = projectId;
+                b.refresh();
+            });
         }
     } : undefined;
 
